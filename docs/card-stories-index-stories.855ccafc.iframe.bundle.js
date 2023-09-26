64674f67d306d250fc0da14b13f6d3db3e7618cf
IanRamosC <IanRamosC@users.noreply.github.com>
2023-09-26 20:08:58 +0000

    Add Stats section that will be used in My Jetpack (#33160)
    * Create reusable Card component
    
    * Abstract Status to its own component
    
    * Use Status component in ProductCard
    
    * Fix Card title spacing
    
    * Add Stats section component
    
    * changelog
    
    * Fix components usage
    
    * Fix project versions
    
    * Remove unnecessary onClick
    
    * Refactor helper functions
    
    * Update SCSS variables
    
    * Fix count title size
    
    * Refactor Status style classNames
    
    * Fix mobile styles
    
    * Remove unnecessary character
    
    * Rename StatsCards folder
    
    * Remove extra space
    
    * Handle null scenarios
    
    * Reuse differenceMagnitude variable
    
    * Bump versions
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/6317407542

---
+++ docs/card-stories-index-stories.855ccafc.iframe.bundle.js
@@ -0,0 +1,72 @@
+(()=>{(self.webpackChunk_automattic_jetpack_storybook=self.webpackChunk_automattic_jetpack_storybook||[]).push([[1471],{"../../packages/my-jetpack/_inc/components/card/stories/index.stories.jsx":(l,a,e)=>{var _,v,h;"use strict";e.r(a),e.d(a,{Default:()=>d,__namedExportsOrder:()=>f,default:()=>u});var r=e("../../../node_modules/.pnpm/react@18.2.0/node_modules/react/index.js"),c=e("../../../node_modules/.pnpm/react-router-dom@6.6.2_react-dom@18.2.0_react@18.2.0/node_modules/react-router-dom/dist/index.js"),i=e("../../../node_modules/.pnpm/react-router@6.6.2_react@18.2.0/node_modules/react-router/dist/index.js"),n=e("../../packages/my-jetpack/_inc/components/card/index.jsx"),t=e("../../../node_modules/.pnpm/react@18.2.0/node_modules/react/jsx-runtime.js"),p=`import React from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Card from '..';
+
+export default {
+	title: 'Packages/My Jetpack/Card',
+	component: Card,
+};
+
+const DefaultArgs = {
+	title: 'Stats',
+	headerRightContent: <div>Right content</div>,
+	children: (
+		<p>
+			Lorem ipsum dolor <b>sit amet</b>, consectetur adipiscing elit. Cras rutrum neque odio, vel
+			viverra lectus vulputate et.
+		</p>
+	),
+};
+
+const Template = args => (
+	<HashRouter>
+		<Routes>
+			<Route path="/" element={ <Card { ...args } /> } />
+		</Routes>
+	</HashRouter>
+);
+
+export const Default = Template.bind( {} );
+Default.parameters = {};
+Default.args = DefaultArgs;
+`,s={Default:{startLoc:{col:17,line:16},endLoc:{col:17,line:20},startBody:{col:17,line:16},endBody:{col:17,line:20}}};const u={parameters:{storySource:{source:`import React from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import Card from '..';
+export default {
+  title: 'Packages/My Jetpack/Card',
+  component: Card
+};
+const DefaultArgs = {
+  title: 'Stats',
+  headerRightContent: <div>Right content</div>,
+  children: <p>
+            Lorem ipsum dolor <b>sit amet</b>, consectetur adipiscing elit. Cras rutrum neque odio, vel
+            viverra lectus vulputate et.
+        </p>
+};
+const Template = args => <HashRouter>
+        <Routes>
+            <Route path="/" element={<Card {...args} />} />
+        </Routes>
+    </HashRouter>;
+export const Default = Template.bind({});
+Default.parameters = {};
+Default.args = DefaultArgs;
+Default.parameters = {
+  ...Default.parameters,
+  docs: {
+    ...Default.parameters?.docs,
+    source: {
+      originalSource: "args => <HashRouter>\\n        <Routes>\\n            <Route path=\\"/\\" element={<Card {...args} />} />\\n        </Routes>\\n    </HashRouter>",
+      ...Default.parameters?.docs?.source
+    }
+  }
+};`,locationsMap:{default:{startLoc:{col:17,line:16},endLoc:{col:17,line:20},startBody:{col:17,line:16},endBody:{col:17,line:20}}}}},title:"Packages/My Jetpack/Card",component:n.Z},m={title:"Stats",headerRightContent:(0,t.jsx)("div",{children:"Right content"}),children:(0,t.jsxs)("p",{children:["Lorem ipsum dolor ",(0,t.jsx)("b",{children:"sit amet"}),", consectetur adipiscing elit. Cras rutrum neque odio, vel viverra lectus vulputate et."]})},o=j=>(0,t.jsx)(c.UT,{children:(0,t.jsx)(i.Z5,{children:(0,t.jsx)(i.AW,{path:"/",element:(0,t.jsx)(n.Z,{...j})})})});o.displayName="Template";const d=o.bind({});d.parameters={},d.args=m,d.parameters={...d.parameters,docs:{...(_=d.parameters)==null?void 0:_.docs,source:{originalSource:`args => <HashRouter>
+        <Routes>
+            <Route path="/" element={<Card {...args} />} />
+        </Routes>
+    </HashRouter>`,...(h=(v=d.parameters)==null?void 0:v.docs)==null?void 0:h.source}}};const f=["Default"]},"../../packages/my-jetpack/_inc/components/card/index.jsx":(l,a,e)=>{"use strict";e.d(a,{Z:()=>h});var r=e("../components/components/text/index.tsx"),c=e("../../../node_modules/.pnpm/classnames@2.3.2/node_modules/classnames/index.js"),i=e.n(c),n=e("../../../node_modules/.pnpm/prop-types@15.8.1/node_modules/prop-types/index.js"),t=e.n(n),p=e("../../../node_modules/.pnpm/react@18.2.0/node_modules/react/index.js"),s=e("../../../node_modules/.pnpm/style-loader@2.0.0_webpack@5.76.0/node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js"),u=e.n(s),m=e("../../../node_modules/.pnpm/css-loader@6.5.1_webpack@5.76.0/node_modules/css-loader/dist/cjs.js!../../../node_modules/.pnpm/postcss-loader@6.2.0_postcss@8.4.21_webpack@5.76.0/node_modules/postcss-loader/dist/cjs.js??ruleSet[1].rules[14].use[2]!../../../node_modules/.pnpm/sass-loader@12.4.0_sass@1.64.1_webpack@5.76.0/node_modules/sass-loader/dist/cjs.js!../../packages/my-jetpack/_inc/components/card/style.module.scss"),o={};o.insert="head",o.singleton=!1;var d=u()(m.Z,o);const f=m.Z.locals||{};var _=e("../../../node_modules/.pnpm/react@18.2.0/node_modules/react/jsx-runtime.js");const v=j=>{const{title:g,headerRightContent:x,className:R,children:E}=j,y=i()(f.container,R);return(0,_.jsxs)("div",{className:y,children:[(0,_.jsxs)("div",{className:f.title,children:[(0,_.jsx)("div",{className:f.name,children:(0,_.jsx)(r.ZP,{variant:"title-medium",children:g})}),x]}),E]})};v.displayName="Card",v.propTypes={children:t().node,title:t().string.isRequired,className:t().string,headerRightContent:t().node},v.__docgenInfo={description:"",methods:[],displayName:"Card",props:{children:{description:"",type:{name:"node"},required:!1},title:{description:"",type:{name:"string"},required:!0},className:{description:"",type:{name:"string"},required:!1},headerRightContent:{description:"",type:{name:"node"},required:!1}}};const h=v},"../../../node_modules/.pnpm/classnames@2.3.2/node_modules/classnames/index.js":(l,a)=>{var e,r;/*!
+	Copyright (c) 2018 Jed Watson.
+	Licensed under the MIT License (MIT), see
+	http://jedwatson.github.io/classnames
+*/(function(){"use strict";var c={}.hasOwnProperty,i="[native code]";function n(){for(var t=[],p=0;p<arguments.length;p++){var s=arguments[p];if(s){var u=typeof s;if(u==="string"||u==="number")t.push(s);else if(Array.isArray(s)){if(s.length){var m=n.apply(null,s);m&&t.push(m)}}else if(u==="object"){if(s.toString!==Object.prototype.toString&&!s.toString.toString().includes("[native code]")){t.push(s.toString());continue}for(var o in s)c.call(s,o)&&s[o]&&t.push(o)}}}return t.join(" ")}l.exports?(n.default=n,l.exports=n):(e=[],r=function(){return n}.apply(a,e),r!==void 0&&(l.exports=r))})()},"../../../node_modules/.pnpm/css-loader@6.5.1_webpack@5.76.0/node_modules/css-loader/dist/cjs.js!../../../node_modules/.pnpm/postcss-loader@6.2.0_postcss@8.4.21_webpack@5.76.0/node_modules/postcss-loader/dist/cjs.js??ruleSet[1].rules[14].use[2]!../../../node_modules/.pnpm/sass-loader@12.4.0_sass@1.64.1_webpack@5.76.0/node_modules/sass-loader/dist/cjs.js!../../packages/my-jetpack/_inc/components/card/style.module.scss":(l,a,e)=>{"use strict";e.d(a,{Z:()=>p});var r=e("../../../node_modules/.pnpm/css-loader@6.5.1_webpack@5.76.0/node_modules/css-loader/dist/runtime/noSourceMaps.js"),c=e.n(r),i=e("../../../node_modules/.pnpm/css-loader@6.5.1_webpack@5.76.0/node_modules/css-loader/dist/runtime/api.js"),n=e.n(i),t=n()(c());t.push([l.id,".pecv05IGfM82FIHp9Q4E{padding:calc(var(--spacing-base)*3);background:var(--jp-white);border-radius:var(--jp-border-radius);box-shadow:0 0 40px rgba(0,0,0,.08);height:100%;display:flex;flex-direction:column;box-shadow:0 0 0 1px var(--jp-gray-10) inset,0px 0px 40px 0px rgba(0,0,0,.08)}.pecv05IGfM82FIHp9Q4E.jgcGPDlYaLnB6QcRWSWR{background:none;text-decoration:none;background-color:var(--jp-white-off);color:var(--jp-black-80)}.pecv05IGfM82FIHp9Q4E.jgcGPDlYaLnB6QcRWSWR:focus{box-shadow:0 0 0 1.5px var(--jp-black);background-color:var(--jp-white);outline:3px solid rgba(0,0,0,0)}.pecv05IGfM82FIHp9Q4E.Hv659vLLaZ8SaSWyxtAZ{box-shadow:0 0 0 1.5px var(--jp-red-60)}.i1rUritEO7pQO4fsj9PF{width:100%;display:flex;gap:var(--spacing-base);align-items:center;justify-content:space-between;margin-bottom:var(--spacing-base)}.i1rUritEO7pQO4fsj9PF h3{font-weight:700}.YP7299HZXBCUg1n1NUqU{display:flex;height:100%;gap:var(--spacing-base);align-items:center;flex-wrap:wrap-reverse}",""]),t.locals={container:"pecv05IGfM82FIHp9Q4E","is-link":"jgcGPDlYaLnB6QcRWSWR","has-error":"Hv659vLLaZ8SaSWyxtAZ",title:"i1rUritEO7pQO4fsj9PF",name:"YP7299HZXBCUg1n1NUqU"};const p=t}}]);})();
--- docs/pricing-table-stories-index-stories.780b3744.iframe.bundle.js
+++ docs/pricing-table-stories-index-stories.780b3744.iframe.bundle.js
@@ -1,4 +1,4 @@
-"use strict";(()=>{(self.webpackChunk_automattic_jetpack_storybook=self.webpackChunk_automattic_jetpack_storybook||[]).push([[3010],{"../components/components/pricing-table/stories/index.stories.tsx":(T,l,n)=>{var s,u,d;n.r(l),n.d(l,{__namedExportsOrder:()=>P,_default:()=>i,default:()=>m});var o=n("../components/components/button/index.tsx"),a=n("../components/components/product-price/index.tsx"),e=n("../components/components/pricing-table/index.tsx"),t=n("../../../node_modules/.pnpm/react@18.2.0/node_modules/react/jsx-runtime.js"),b=`import Button from '../../button';
+"use strict";(()=>{(self.webpackChunk_automattic_jetpack_storybook=self.webpackChunk_automattic_jetpack_storybook||[]).push([[3367,731],{"../components/components/pricing-table/stories/index.stories.tsx":(P,o,n)=>{var l,a,c;n.r(o),n.d(o,{__namedExportsOrder:()=>f,_default:()=>i,default:()=>g});var m=n("../components/components/button/index.tsx"),s=n("../components/components/product-price/index.tsx"),e=n("../components/components/pricing-table/index.tsx"),t=n("../../../node_modules/.pnpm/react@18.2.0/node_modules/react/jsx-runtime.js"),p=`import Button from '../../button';
 import ProductPrice from '../../product-price';
 import PricingTable, { PricingTableColumn, PricingTableHeader, PricingTableItem } from '../index';
 import type { ComponentStory, ComponentMeta } from '@storybook/react';
@@ -72,7 +72,7 @@ const DefaultArgs = {
 
 export const _default = Template.bind( {} );
 _default.args = DefaultArgs;
-`,g={_default:{startLoc:{col:54,line:14},endLoc:{col:1,line:41},startBody:{col:54,line:14},endBody:{col:1,line:41}}};const m={parameters:{storySource:{source:`import Button from '../../button';
+`,T={_default:{startLoc:{col:54,line:14},endLoc:{col:1,line:41},startBody:{col:54,line:14},endBody:{col:1,line:41}}};const g={parameters:{storySource:{source:`import Button from '../../button';
 import ProductPrice from '../../product-price';
 import PricingTable, { PricingTableColumn, PricingTableHeader, PricingTableItem } from '../index';
 import type { ComponentStory, ComponentMeta } from '@storybook/react';
@@ -145,7 +145,7 @@ _default.parameters = {
       ..._default.parameters?.docs?.source
     }
   }
-};`,locationsMap:{default:{startLoc:{col:54,line:14},endLoc:{col:1,line:41},startBody:{col:54,line:14},endBody:{col:1,line:41}}}}},title:"JS Packages/Components/Pricing Table",component:e.ZP,subcomponents:{PricingTableColumn:e.oK,PricingTableHeader:e.NE,PricingTableItem:e.kF}},c=r=>(0,t.jsxs)(e.ZP,{...r,children:[(0,t.jsxs)(e.oK,{primary:!0,children:[(0,t.jsxs)(e.NE,{children:[(0,t.jsx)(a.Z,{price:9.95,offPrice:4.98,legend:"/month, billed yearly",currency:"USD",hideDiscountLabel:r==null?void 0:r.hideDiscountLabel}),(0,t.jsx)(o.Z,{fullWidth:!0,children:"Get Premium"})]}),(0,t.jsx)(e.kF,{isIncluded:!0,label:(0,t.jsx)("strong",{children:"Up to 1000"})}),(0,t.jsx)(e.kF,{isIncluded:!0,tooltipInfo:"This is an info"}),(0,t.jsx)(e.kF,{isIncluded:!0,tooltipInfo:"This is an info with title",tooltipTitle:"Small title"}),(0,t.jsx)(e.kF,{isIncluded:!0}),(0,t.jsx)(e.kF,{isIncluded:!0})]}),(0,t.jsxs)(e.oK,{children:[(0,t.jsxs)(e.NE,{children:[(0,t.jsx)(a.Z,{price:0,legend:"Free forever",currency:"USD",hidePriceFraction:!0}),(0,t.jsx)(o.Z,{fullWidth:!0,variant:"secondary",children:"Start for free"})]}),(0,t.jsx)(e.kF,{isIncluded:!0,label:"Up to 300"}),(0,t.jsx)(e.kF,{isIncluded:!1,label:"This is not included",tooltipInfo:"This has a tooltip, so its overwrites the default info on small screens"}),(0,t.jsx)(e.kF,{isIncluded:!1}),(0,t.jsx)(e.kF,{isIncluded:!0}),(0,t.jsx)(e.kF,{isIncluded:!0})]})]});c.displayName="Template";const f={title:"Buy premium, or start for free",items:[{name:"Feature A with limit",tooltipInfo:"Default info for Feature A"},{name:"Feature B",tooltipInfo:"Default info for Feature B"},{name:"Feature C with a longer title that will span multiple lines",tooltipInfo:"Default info for Feature C",tooltipTitle:"Title for C"},{name:"Feature D",tooltipInfo:"Default info for Feature D",tooltipTitle:"Title for D"},{name:"Feature E"}],hideDiscountLabel:!1},i=c.bind({});i.args=f,i.parameters={...i.parameters,docs:{...(s=i.parameters)==null?void 0:s.docs,source:{originalSource:`args => {
+};`,locationsMap:{default:{startLoc:{col:54,line:14},endLoc:{col:1,line:41},startBody:{col:54,line:14},endBody:{col:1,line:41}}}}},title:"JS Packages/Components/Pricing Table",component:e.ZP,subcomponents:{PricingTableColumn:e.oK,PricingTableHeader:e.NE,PricingTableItem:e.kF}},u=r=>(0,t.jsxs)(e.ZP,{...r,children:[(0,t.jsxs)(e.oK,{primary:!0,children:[(0,t.jsxs)(e.NE,{children:[(0,t.jsx)(s.Z,{price:9.95,offPrice:4.98,legend:"/month, billed yearly",currency:"USD",hideDiscountLabel:r==null?void 0:r.hideDiscountLabel}),(0,t.jsx)(m.Z,{fullWidth:!0,children:"Get Premium"})]}),(0,t.jsx)(e.kF,{isIncluded:!0,label:(0,t.jsx)("strong",{children:"Up to 1000"})}),(0,t.jsx)(e.kF,{isIncluded:!0,tooltipInfo:"This is an info"}),(0,t.jsx)(e.kF,{isIncluded:!0,tooltipInfo:"This is an info with title",tooltipTitle:"Small title"}),(0,t.jsx)(e.kF,{isIncluded:!0}),(0,t.jsx)(e.kF,{isIncluded:!0})]}),(0,t.jsxs)(e.oK,{children:[(0,t.jsxs)(e.NE,{children:[(0,t.jsx)(s.Z,{price:0,legend:"Free forever",currency:"USD",hidePriceFraction:!0}),(0,t.jsx)(m.Z,{fullWidth:!0,variant:"secondary",children:"Start for free"})]}),(0,t.jsx)(e.kF,{isIncluded:!0,label:"Up to 300"}),(0,t.jsx)(e.kF,{isIncluded:!1,label:"This is not included",tooltipInfo:"This has a tooltip, so its overwrites the default info on small screens"}),(0,t.jsx)(e.kF,{isIncluded:!1}),(0,t.jsx)(e.kF,{isIncluded:!0}),(0,t.jsx)(e.kF,{isIncluded:!0})]})]});u.displayName="Template";const d={title:"Buy premium, or start for free",items:[{name:"Feature A with limit",tooltipInfo:"Default info for Feature A"},{name:"Feature B",tooltipInfo:"Default info for Feature B"},{name:"Feature C with a longer title that will span multiple lines",tooltipInfo:"Default info for Feature C",tooltipTitle:"Title for C"},{name:"Feature D",tooltipInfo:"Default info for Feature D",tooltipTitle:"Title for D"},{name:"Feature E"}],hideDiscountLabel:!1},i=u.bind({});i.args=d,i.parameters={...i.parameters,docs:{...(l=i.parameters)==null?void 0:l.docs,source:{originalSource:`args => {
   return <PricingTable {...args}>
             <PricingTableColumn primary>
                 <PricingTableHeader>
@@ -172,4 +172,79 @@ _default.parameters = {
                 <PricingTableItem isIncluded={true} />
             </PricingTableColumn>
         </PricingTable>;
-}`,...(d=(u=i.parameters)==null?void 0:u.docs)==null?void 0:d.source}}};const P=["_default"]}}]);})();
+}`,...(c=(a=i.parameters)==null?void 0:a.docs)==null?void 0:c.source}}};const f=["_default"]},"../../plugins/protect/src/js/components/pricing-table/stories/index.stories.jsx":(P,o,n)=>{var l,a,c;n.r(o),n.d(o,{Default:()=>i,__namedExportsOrder:()=>f,default:()=>u});var m=n("../../../node_modules/.pnpm/react@18.2.0/node_modules/react/index.js"),s=n("../../plugins/protect/src/js/state/store.js"),e=n("../../plugins/protect/src/js/components/interstitial-page/stories/mock.js"),t=n("../../plugins/protect/src/js/components/pricing-table/index.jsx"),p=n("../../../node_modules/.pnpm/react@18.2.0/node_modules/react/jsx-runtime.js"),T=`/* eslint-disable react/react-in-jsx-scope */
+import React from 'react';
+import { initStore } from '../../../state/store';
+import { jetpackProtectInitialState } from '../../interstitial-page/stories/mock.js';
+import ConnectedPricingTable from '../index.jsx';
+
+window.jetpackProtectInitialState = jetpackProtectInitialState;
+initStore();
+
+export default {
+	title: 'Plugins/Protect/Pricing Table',
+	component: ConnectedPricingTable,
+	parameters: {
+		layout: 'centered',
+		actions: { argTypesRegex: '^on.*' },
+	},
+	argTypes: {
+		isCard: {
+			control: { type: 'boolean' },
+		},
+	},
+};
+
+const DefaultDefaultPricingTable = args => {
+	return <ConnectedPricingTable { ...args } />;
+};
+
+export const Default = DefaultDefaultPricingTable.bind( {} );
+Default.args = {
+	isCard: false,
+	onAdd: () => {},
+};
+`,g={Default:{startLoc:{col:35,line:25},endLoc:{col:1,line:27},startBody:{col:35,line:25},endBody:{col:1,line:27}}};window.jetpackProtectInitialState=e.r,(0,s.N)();const u={title:"Plugins/Protect/Pricing Table",component:t.Z,parameters:{storySource:{source:`
+import React from 'react';
+import { initStore } from '../../../state/store';
+import { jetpackProtectInitialState } from '../../interstitial-page/stories/mock.js';
+import ConnectedPricingTable from '../index.jsx';
+window.jetpackProtectInitialState = jetpackProtectInitialState;
+initStore();
+export default {
+  title: 'Plugins/Protect/Pricing Table',
+  component: ConnectedPricingTable,
+  parameters: {
+    layout: 'centered',
+    actions: {
+      argTypesRegex: '^on.*'
+    }
+  },
+  argTypes: {
+    isCard: {
+      control: {
+        type: 'boolean'
+      }
+    }
+  }
+};
+const DefaultDefaultPricingTable = args => {
+  return <ConnectedPricingTable {...args} />;
+};
+export const Default = DefaultDefaultPricingTable.bind({});
+Default.args = {
+  isCard: false,
+  onAdd: () => {}
+};
+Default.parameters = {
+  ...Default.parameters,
+  docs: {
+    ...Default.parameters?.docs,
+    source: {
+      originalSource: "args => {\\n  return <ConnectedPricingTable {...args} />;\\n}",
+      ...Default.parameters?.docs?.source
+    }
+  }
+};`,locationsMap:{default:{startLoc:{col:35,line:25},endLoc:{col:1,line:27},startBody:{col:35,line:25},endBody:{col:1,line:27}}}},layout:"centered",actions:{argTypesRegex:"^on.*"}},argTypes:{isCard:{control:{type:"boolean"}}}},d=r=>(0,p.jsx)(t.Z,{...r});d.displayName="DefaultDefaultPricingTable";const i=d.bind({});i.args={isCard:!1,onAdd:()=>{}},i.parameters={...i.parameters,docs:{...(l=i.parameters)==null?void 0:l.docs,source:{originalSource:`args => {
+  return <ConnectedPricingTable {...args} />;
+}`,...(c=(a=i.parameters)==null?void 0:a.docs)==null?void 0:c.source}}};const f=["Default"]}}]);})();
