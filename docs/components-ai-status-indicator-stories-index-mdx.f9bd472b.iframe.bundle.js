c3eb6b6c2fe6d86a5b6ded52186c9ef3d8896b4e
retrofox <retrofox@users.noreply.github.com>
2023-11-15 20:08:26 +0000

    AI Client: expose build folder (#34118)
    * add build scripts to composer
    
    * include ./build  in the mirror
    
    * address `exports`, `main`, and `types` props
    
    * changelog
    
    * Update projects/js-packages/ai-client/composer.json
    
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>
    
    * fix composer building scripts
    
    * remove duplicated composer scripts
    
    * add composer watch script
    
    * move main index file to src/
    
    * declare TS window global vars
    
    * introduce clean script
    
    * put compiled files in the build/ folder
    
    * ignore build/ folder
    
    * generate TS def in building process
    
    * add @types/react dep
    
    * def HOC with ReactElement type
    
    * fix types for the AIControl component
    
    * fix types in the data-flow lib
    
    * fix types in Storybook stories
    
    * update packages deps
    
    * add connectionStatus to JPConnectionInitialState
    
    * Update projects/js-packages/ai-client/changelog/update-ai-client-expose-compiles-files
    
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>
    
    * Update projects/js-packages/ai-client/changelog/update-ai-client-expose-compiles-files
    
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>
    
    * bump new pkg version
    
    ---------
    
    Co-authored-by: Brad Jorsch <anomiex@users.noreply.github.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/6882232880

---
+++ docs/components-ai-status-indicator-stories-index-mdx.f9bd472b.iframe.bundle.js
@@ -0,0 +1,13 @@
+"use strict";(()=>{(self.webpackChunk_automattic_jetpack_storybook=self.webpackChunk_automattic_jetpack_storybook||[]).push([[9869],{"../ai-client/src/components/ai-status-indicator/stories/index.mdx":(m,i,o)=>{o.r(i),o.d(i,{default:()=>r});var u=o("../../../node_modules/.pnpm/react@18.2.0/node_modules/react/index.js"),s=o("../../../node_modules/.pnpm/react@18.2.0/node_modules/react/jsx-runtime.js"),_=o("../../../node_modules/.pnpm/@storybook+addon-docs@7.4.6_react-dom@18.2.0_react@18.2.0/node_modules/@storybook/addon-docs/dist/shims/mdx-react-shim.js"),j=o.n(_),n=o("../../../node_modules/.pnpm/@storybook+blocks@7.4.6_@types+react@18.2.33_react-dom@18.2.0_react@18.2.0/node_modules/@storybook/blocks/dist/index.mjs"),l=o("../ai-client/src/components/ai-status-indicator/index.tsx"),a=o("../ai-client/src/components/ai-status-indicator/stories/index.stories.tsx");function d(e){const t=Object.assign({h1:"h1",h2:"h2",h3:"h3"},(0,_.useMDXComponents)(),e.components);return(0,s.jsxs)(s.Fragment,{children:[(0,s.jsx)(n.h_,{of:a}),`
+`,(0,s.jsx)(t.h1,{id:"aistatusindicator",children:"AiStatusIndicator"}),`
+`,(0,s.jsx)(t.h2,{id:"requesting-states",children:"Requesting states"}),`
+`,(0,s.jsx)(t.h3,{id:"init",children:"Init"}),`
+`,(0,s.jsx)(n.oG,{id:"js-packages-ai-client-aistatusindicator--init"}),`
+`,(0,s.jsx)(t.h3,{id:"requesting",children:"Requesting"}),`
+`,(0,s.jsx)(n.oG,{id:"js-packages-ai-client-aistatusindicator--requesting"}),`
+`,(0,s.jsx)(t.h3,{id:"suggesting",children:"Suggesting"}),`
+`,(0,s.jsx)(n.oG,{id:"js-packages-ai-client-aistatusindicator--suggesting"}),`
+`,(0,s.jsx)(t.h3,{id:"done",children:"Done"}),`
+`,(0,s.jsx)(n.oG,{id:"js-packages-ai-client-aistatusindicator--done"}),`
+`,(0,s.jsx)(t.h3,{id:"error",children:"Error"}),`
+`,(0,s.jsx)(n.oG,{id:"js-packages-ai-client-aistatusindicator--error"})]})}function c(e={}){const{wrapper:t}=Object.assign({},(0,_.useMDXComponents)(),e.components);return t?(0,s.jsx)(t,Object.assign({},e,{children:(0,s.jsx)(d,e)})):d(e)}const r=c}}]);})();
