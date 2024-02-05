308ab1ba883c11c877c53d5a986fb94b27dbc56a
dkmyta <dkmyta@users.noreply.github.com>
2024-02-05 23:15:11 +0000

    Protect: Add WAF standalone mode indicator (#34840)
    * Add method for detecting standalone mode and FirewallHeader indication when enabled
    
    * changelog
    
    * Add dummary arg to avoid bad minification of translation strings
    
    * Use js-packages Status component over custom implementation in FirewallHeader
    
    * Fix currentStatus logic
    
    * Add check for unsupported env in currentStatus logic
    
    * Improve naming for better clarity
    
    * Move get_standalone_mode_status method to waf package and include return in Waf_Runner::get_config
    
    * changelog
    
    * Improve WAF package changelogger entry
    
    * Reword entry
    
    * Fix version mismatch
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/7791988050

---
+++ docs/status-stories-index-stories.62d47f45.iframe.bundle.js
@@ -0,0 +1,107 @@
+(()=>{(self.webpackChunk_automattic_jetpack_storybook=self.webpackChunk_automattic_jetpack_storybook||[]).push([[7772],{"../components/components/status/stories/index.stories.tsx":(m,_,t)=>{var f,y,v,g,A,N,b,B,L,E,O,C,I,T,j,h,G,R,Z,P,K;"use strict";t.r(_),t.d(_,{ActionNeeded:()=>o,Active:()=>n,CustomLabel:()=>d,Error:()=>e,Inactive:()=>s,Initializing:()=>l,NoLabel:()=>r,__namedExportsOrder:()=>x,default:()=>c});var a=t("../components/components/status/index.tsx"),i=t("../../../node_modules/.pnpm/react@18.2.0/node_modules/react/jsx-runtime.js"),S=`import Status from '..';
+
+export default {
+	title: 'JS Packages/Components/Status',
+	component: Status,
+	parameters: {
+		layout: 'centered',
+	},
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const Active = args => {
+	return <Status status="active" />;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const Error = args => {
+	return <Status status="error" />;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const ActionNeeded = args => {
+	return <Status status="action" />;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const Initializing = args => {
+	return <Status status="initializing" />;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const Inactive = args => {
+	return <Status status="inactive" />;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const CustomLabel = args => {
+	return <Status label="Code is poetry" />;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const NoLabel = args => {
+	return <Status status="active" label="" />;
+};
+`,u={Active:{startLoc:{col:22,line:12},endLoc:{col:1,line:14},startBody:{col:22,line:12},endBody:{col:1,line:14}},Error:{startLoc:{col:21,line:17},endLoc:{col:1,line:19},startBody:{col:21,line:17},endBody:{col:1,line:19}},ActionNeeded:{startLoc:{col:28,line:22},endLoc:{col:1,line:24},startBody:{col:28,line:22},endBody:{col:1,line:24}},Initializing:{startLoc:{col:28,line:27},endLoc:{col:1,line:29},startBody:{col:28,line:27},endBody:{col:1,line:29}},Inactive:{startLoc:{col:24,line:32},endLoc:{col:1,line:34},startBody:{col:24,line:32},endBody:{col:1,line:34}},CustomLabel:{startLoc:{col:27,line:37},endLoc:{col:1,line:39},startBody:{col:27,line:37},endBody:{col:1,line:39}},NoLabel:{startLoc:{col:23,line:42},endLoc:{col:1,line:44},startBody:{col:23,line:42},endBody:{col:1,line:44}}};const c={title:"JS Packages/Components/Status",component:a.Z,parameters:{storySource:{source:`import Status from '..';
+
+export default {
+	title: 'JS Packages/Components/Status',
+	component: Status,
+	parameters: {
+		layout: 'centered',
+	},
+};
+
+
+export const Active = args => {
+	return <Status status="active" />;
+};
+
+
+export const Error = args => {
+	return <Status status="error" />;
+};
+
+
+export const ActionNeeded = args => {
+	return <Status status="action" />;
+};
+
+
+export const Initializing = args => {
+	return <Status status="initializing" />;
+};
+
+
+export const Inactive = args => {
+	return <Status status="inactive" />;
+};
+
+
+export const CustomLabel = args => {
+	return <Status label="Code is poetry" />;
+};
+
+
+export const NoLabel = args => {
+	return <Status status="active" label="" />;
+};
+`,locationsMap:{active:{startLoc:{col:22,line:12},endLoc:{col:1,line:14},startBody:{col:22,line:12},endBody:{col:1,line:14}},error:{startLoc:{col:21,line:17},endLoc:{col:1,line:19},startBody:{col:21,line:17},endBody:{col:1,line:19}},"action-needed":{startLoc:{col:28,line:22},endLoc:{col:1,line:24},startBody:{col:28,line:22},endBody:{col:1,line:24}},initializing:{startLoc:{col:28,line:27},endLoc:{col:1,line:29},startBody:{col:28,line:27},endBody:{col:1,line:29}},inactive:{startLoc:{col:24,line:32},endLoc:{col:1,line:34},startBody:{col:24,line:32},endBody:{col:1,line:34}},"custom-label":{startLoc:{col:27,line:37},endLoc:{col:1,line:39},startBody:{col:27,line:37},endBody:{col:1,line:39}},"no-label":{startLoc:{col:23,line:42},endLoc:{col:1,line:44},startBody:{col:23,line:42},endBody:{col:1,line:44}}}},layout:"centered"}},n=p=>(0,i.jsx)(a.Z,{status:"active"});n.displayName="Active";const e=p=>(0,i.jsx)(a.Z,{status:"error"});e.displayName="Error";const o=p=>(0,i.jsx)(a.Z,{status:"action"});o.displayName="ActionNeeded";const l=p=>(0,i.jsx)(a.Z,{status:"initializing"});l.displayName="Initializing";const s=p=>(0,i.jsx)(a.Z,{status:"inactive"});s.displayName="Inactive";const d=p=>(0,i.jsx)(a.Z,{label:"Code is poetry"});d.displayName="CustomLabel";const r=p=>(0,i.jsx)(a.Z,{status:"active",label:""});r.displayName="NoLabel",n.parameters={...n.parameters,docs:{...(f=n.parameters)==null?void 0:f.docs,source:{originalSource:`args => {
+  return <Status status="active" />;
+}`,...(v=(y=n.parameters)==null?void 0:y.docs)==null?void 0:v.source}}},e.parameters={...e.parameters,docs:{...(g=e.parameters)==null?void 0:g.docs,source:{originalSource:`args => {
+  return <Status status="error" />;
+}`,...(N=(A=e.parameters)==null?void 0:A.docs)==null?void 0:N.source}}},o.parameters={...o.parameters,docs:{...(b=o.parameters)==null?void 0:b.docs,source:{originalSource:`args => {
+  return <Status status="action" />;
+}`,...(L=(B=o.parameters)==null?void 0:B.docs)==null?void 0:L.source}}},l.parameters={...l.parameters,docs:{...(E=l.parameters)==null?void 0:E.docs,source:{originalSource:`args => {
+  return <Status status="initializing" />;
+}`,...(C=(O=l.parameters)==null?void 0:O.docs)==null?void 0:C.source}}},s.parameters={...s.parameters,docs:{...(I=s.parameters)==null?void 0:I.docs,source:{originalSource:`args => {
+  return <Status status="inactive" />;
+}`,...(j=(T=s.parameters)==null?void 0:T.docs)==null?void 0:j.source}}},d.parameters={...d.parameters,docs:{...(h=d.parameters)==null?void 0:h.docs,source:{originalSource:`args => {
+  return <Status label="Code is poetry" />;
+}`,...(R=(G=d.parameters)==null?void 0:G.docs)==null?void 0:R.source}}},r.parameters={...r.parameters,docs:{...(Z=r.parameters)==null?void 0:Z.docs,source:{originalSource:`args => {
+  return <Status status="active" label="" />;
+}`,...(K=(P=r.parameters)==null?void 0:P.docs)==null?void 0:K.source}}};const x=["Active","Error","ActionNeeded","Initializing","Inactive","CustomLabel","NoLabel"];try{n.displayName="Active",n.__docgenInfo={description:"",displayName:"Active",props:{}},typeof STORYBOOK_REACT_CLASSES!="undefined"&&(STORYBOOK_REACT_CLASSES["../components/components/status/stories/index.stories.tsx#Active"]={docgenInfo:n.__docgenInfo,name:"Active",path:"../components/components/status/stories/index.stories.tsx#Active"})}catch(p){}try{e.displayName="Error",e.__docgenInfo={description:"",displayName:"Error",props:{}},typeof STORYBOOK_REACT_CLASSES!="undefined"&&(STORYBOOK_REACT_CLASSES["../components/components/status/stories/index.stories.tsx#Error"]={docgenInfo:e.__docgenInfo,name:"Error",path:"../components/components/status/stories/index.stories.tsx#Error"})}catch(p){}try{o.displayName="ActionNeeded",o.__docgenInfo={description:"",displayName:"ActionNeeded",props:{}},typeof STORYBOOK_REACT_CLASSES!="undefined"&&(STORYBOOK_REACT_CLASSES["../components/components/status/stories/index.stories.tsx#ActionNeeded"]={docgenInfo:o.__docgenInfo,name:"ActionNeeded",path:"../components/components/status/stories/index.stories.tsx#ActionNeeded"})}catch(p){}try{l.displayName="Initializing",l.__docgenInfo={description:"",displayName:"Initializing",props:{}},typeof STORYBOOK_REACT_CLASSES!="undefined"&&(STORYBOOK_REACT_CLASSES["../components/components/status/stories/index.stories.tsx#Initializing"]={docgenInfo:l.__docgenInfo,name:"Initializing",path:"../components/components/status/stories/index.stories.tsx#Initializing"})}catch(p){}try{s.displayName="Inactive",s.__docgenInfo={description:"",displayName:"Inactive",props:{}},typeof STORYBOOK_REACT_CLASSES!="undefined"&&(STORYBOOK_REACT_CLASSES["../components/components/status/stories/index.stories.tsx#Inactive"]={docgenInfo:s.__docgenInfo,name:"Inactive",path:"../components/components/status/stories/index.stories.tsx#Inactive"})}catch(p){}try{d.displayName="CustomLabel",d.__docgenInfo={description:"",displayName:"CustomLabel",props:{}},typeof STORYBOOK_REACT_CLASSES!="undefined"&&(STORYBOOK_REACT_CLASSES["../components/components/status/stories/index.stories.tsx#CustomLabel"]={docgenInfo:d.__docgenInfo,name:"CustomLabel",path:"../components/components/status/stories/index.stories.tsx#CustomLabel"})}catch(p){}try{r.displayName="NoLabel",r.__docgenInfo={description:"",displayName:"NoLabel",props:{}},typeof STORYBOOK_REACT_CLASSES!="undefined"&&(STORYBOOK_REACT_CLASSES["../components/components/status/stories/index.stories.tsx#NoLabel"]={docgenInfo:r.__docgenInfo,name:"NoLabel",path:"../components/components/status/stories/index.stories.tsx#NoLabel"})}catch(p){}},"../components/components/status/index.tsx":(m,_,t)=>{"use strict";t.d(_,{Z:()=>f});var a=t("../../../node_modules/.pnpm/@wordpress+i18n@4.50.0/node_modules/@wordpress/i18n/build-module/index.js"),i=t("../../../node_modules/.pnpm/classnames@2.3.2/node_modules/classnames/index.js"),S=t.n(i),u=t("../components/components/text/index.tsx"),c=t("../../../node_modules/.pnpm/style-loader@2.0.0_webpack@5.76.0/node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js"),n=t.n(c),e=t("../../../node_modules/.pnpm/css-loader@6.5.1_webpack@5.76.0/node_modules/css-loader/dist/cjs.js!../../../node_modules/.pnpm/postcss-loader@6.2.0_postcss@8.4.31_webpack@5.76.0/node_modules/postcss-loader/dist/cjs.js??ruleSet[1].rules[16].use[2]!../../../node_modules/.pnpm/sass-loader@12.4.0_sass@1.64.1_webpack@5.76.0/node_modules/sass-loader/dist/cjs.js!../components/components/status/style.module.scss"),o={};o.insert="head",o.singleton=!1;var l=n()(e.Z,o);const s=e.Z.locals||{};var d=t("../../../node_modules/.pnpm/react@18.2.0/node_modules/react/jsx-runtime.js");const r=a.__,x=({className:y,label:v,status:g="inactive"})=>{const A={active:r("Active","jetpack"),error:r("Error","jetpack"),action:r("Action needed","jetpack"),inactive:r("Inactive","jetpack"),initializing:r("Setting up","jetpack")};return(0,d.jsxs)(u.ZP,{variant:"body-extra-small",className:S()(s.status,{[s[`is-${g}`]]:g},y),children:[(0,d.jsx)("span",{className:s.status__indicator}),(0,d.jsx)("span",{className:s.status__label,children:v||v===""?v:A[g]})]})};x.displayName="Status";const f=x;try{status.displayName="status",status.__docgenInfo={description:"",displayName:"status",props:{status:{defaultValue:{value:"inactive"},description:"",name:"status",required:!1,type:{name:"enum",value:[{value:'"error"'},{value:'"active"'},{value:'"inactive"'},{value:'"action"'},{value:'"initializing"'}]}},label:{defaultValue:null,description:"",name:"label",required:!1,type:{name:"string"}},className:{defaultValue:null,description:"",name:"className",required:!1,type:{name:"string"}}}},typeof STORYBOOK_REACT_CLASSES!="undefined"&&(STORYBOOK_REACT_CLASSES["../components/components/status/index.tsx#status"]={docgenInfo:status.__docgenInfo,name:"status",path:"../components/components/status/index.tsx#status"})}catch(y){}},"../../../node_modules/.pnpm/classnames@2.3.2/node_modules/classnames/index.js":(m,_)=>{var t,a;/*!
+	Copyright (c) 2018 Jed Watson.
+	Licensed under the MIT License (MIT), see
+	http://jedwatson.github.io/classnames
+*/(function(){"use strict";var i={}.hasOwnProperty,S="[native code]";function u(){for(var c=[],n=0;n<arguments.length;n++){var e=arguments[n];if(e){var o=typeof e;if(o==="string"||o==="number")c.push(e);else if(Array.isArray(e)){if(e.length){var l=u.apply(null,e);l&&c.push(l)}}else if(o==="object"){if(e.toString!==Object.prototype.toString&&!e.toString.toString().includes("[native code]")){c.push(e.toString());continue}for(var s in e)i.call(e,s)&&e[s]&&c.push(s)}}}return c.join(" ")}m.exports?(u.default=u,m.exports=u):(t=[],a=function(){return u}.apply(_,t),a!==void 0&&(m.exports=a))})()},"../../../node_modules/.pnpm/css-loader@6.5.1_webpack@5.76.0/node_modules/css-loader/dist/cjs.js!../../../node_modules/.pnpm/postcss-loader@6.2.0_postcss@8.4.31_webpack@5.76.0/node_modules/postcss-loader/dist/cjs.js??ruleSet[1].rules[16].use[2]!../../../node_modules/.pnpm/sass-loader@12.4.0_sass@1.64.1_webpack@5.76.0/node_modules/sass-loader/dist/cjs.js!../components/components/status/style.module.scss":(m,_,t)=>{"use strict";t.d(_,{Z:()=>n});var a=t("../../../node_modules/.pnpm/css-loader@6.5.1_webpack@5.76.0/node_modules/css-loader/dist/runtime/noSourceMaps.js"),i=t.n(a),S=t("../../../node_modules/.pnpm/css-loader@6.5.1_webpack@5.76.0/node_modules/css-loader/dist/runtime/api.js"),u=t.n(S),c=u()(i());c.push([m.id,".ilNHXThBbZvdJnNGGBpo{--spacing-base: 8px;--wp-admin-border-width-focus: 1.5px;align-items:center;display:flex;white-space:nowrap}.sdrGGr0xAx6vgW7nrbDX{background-color:var(--jp-gray-50);border-radius:50%;flex-shrink:0;height:.666em;width:.666em}.QUL6A0kDB4u3GHJ4yqLr{font-weight:600;line-height:1.666;margin-left:calc(var(--spacing-base)/2)}.ilNHXThBbZvdJnNGGBpo.PoNAvAOrUwXGWlZ0swgL span,.ilNHXThBbZvdJnNGGBpo.PoNAvAOrUwXGWlZ0swgL a{color:var(--jp-green-50)}.ilNHXThBbZvdJnNGGBpo.PoNAvAOrUwXGWlZ0swgL .sdrGGr0xAx6vgW7nrbDX{background-color:var(--jp-green-50)}.ilNHXThBbZvdJnNGGBpo.SyUVUUZnplK7gFjn3W8K span,.ilNHXThBbZvdJnNGGBpo.SyUVUUZnplK7gFjn3W8K a{color:var(--jp-gray-50)}.ilNHXThBbZvdJnNGGBpo.SyUVUUZnplK7gFjn3W8K .sdrGGr0xAx6vgW7nrbDX{background-color:var(--jp-gray-50)}.ilNHXThBbZvdJnNGGBpo.mix4_vTri6naehq6m2Fg span,.ilNHXThBbZvdJnNGGBpo.mix4_vTri6naehq6m2Fg a{color:var(--jp-red-50)}.ilNHXThBbZvdJnNGGBpo.mix4_vTri6naehq6m2Fg .sdrGGr0xAx6vgW7nrbDX{background-color:var(--jp-red-50)}.ilNHXThBbZvdJnNGGBpo.fWMIo4Pn8EPL4NDbRY1p span,.ilNHXThBbZvdJnNGGBpo.fWMIo4Pn8EPL4NDbRY1p a{color:var(--jp-yellow-30)}.ilNHXThBbZvdJnNGGBpo.fWMIo4Pn8EPL4NDbRY1p .sdrGGr0xAx6vgW7nrbDX{background-color:var(--jp-yellow-30)}.ilNHXThBbZvdJnNGGBpo.f48fCJvs_HWBIR0twAbg span,.ilNHXThBbZvdJnNGGBpo.f48fCJvs_HWBIR0twAbg a{color:var(--jp-blue-40)}.ilNHXThBbZvdJnNGGBpo.f48fCJvs_HWBIR0twAbg .sdrGGr0xAx6vgW7nrbDX{background-color:var(--jp-blue-40)}",""]),c.locals={status:"ilNHXThBbZvdJnNGGBpo",status__indicator:"sdrGGr0xAx6vgW7nrbDX",status__label:"QUL6A0kDB4u3GHJ4yqLr","is-active":"PoNAvAOrUwXGWlZ0swgL","is-inactive":"SyUVUUZnplK7gFjn3W8K","is-error":"mix4_vTri6naehq6m2Fg","is-action":"fWMIo4Pn8EPL4NDbRY1p","is-initializing":"f48fCJvs_HWBIR0twAbg"};const n=c}}]);})();
