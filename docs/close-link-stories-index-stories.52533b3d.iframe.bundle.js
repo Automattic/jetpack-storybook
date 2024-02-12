9e939dec2db189e3f98d78c9ec42d54d593af2ab
anomiex <anomiex@users.noreply.github.com>
2024-02-12 20:38:51 +0000

    Update wordpress monorepo (#35608)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/7877463033

---
+++ docs/close-link-stories-index-stories.52533b3d.iframe.bundle.js
@@ -0,0 +1,53 @@
+(()=>{(self.webpackChunk_automattic_jetpack_storybook=self.webpackChunk_automattic_jetpack_storybook||[]).push([[7297],{"../../packages/my-jetpack/_inc/components/close-link/stories/index.stories.jsx":(_,r,e)=>{var h,O,D;"use strict";e.r(r),e.d(r,{__namedExportsOrder:()=>C,_default:()=>p,default:()=>y});var a=e("../../../node_modules/.pnpm/react@18.2.0/node_modules/react/index.js"),l=e("../../../node_modules/.pnpm/react-router-dom@6.6.2_react-dom@18.2.0_react@18.2.0/node_modules/react-router-dom/dist/index.js"),n=e("../../../node_modules/.pnpm/react-router@6.6.2_react@18.2.0/node_modules/react-router/dist/index.js"),d=e("../../../node_modules/.pnpm/@wordpress+icons@9.42.0_react@18.2.0/node_modules/@wordpress/icons/build-module/icon/index.js"),t=e("../../../node_modules/.pnpm/@wordpress+icons@9.42.0_react@18.2.0/node_modules/@wordpress/icons/build-module/library/close.js"),c=e("../../../node_modules/.pnpm/classnames@2.3.2/node_modules/classnames/index.js"),o=e.n(c),i=e("../../../node_modules/.pnpm/style-loader@2.0.0_webpack@5.76.0/node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js"),E=e.n(i),u=e("../../../node_modules/.pnpm/css-loader@6.5.1_webpack@5.76.0/node_modules/css-loader/dist/cjs.js!../../../node_modules/.pnpm/postcss-loader@6.2.0_postcss@8.4.31_webpack@5.76.0/node_modules/postcss-loader/dist/cjs.js??ruleSet[1].rules[16].use[2]!../../../node_modules/.pnpm/sass-loader@12.4.0_sass@1.64.1_webpack@5.76.0/node_modules/sass-loader/dist/cjs.js!../../packages/my-jetpack/_inc/components/close-link/styles.module.scss"),f={};f.insert="head",f.singleton=!1;var L=E()(u.Z,f);const v=u.Z.locals||{};var m=e("../../../node_modules/.pnpm/react@18.2.0/node_modules/react/jsx-runtime.js");const s=({className:R})=>(0,m.jsx)(l.rU,{to:"/",className:o()(v.link,R),children:(0,m.jsx)(d.Z,{icon:t.Z,className:v.icon})});s.displayName="CloseLink";const j=s;s.__docgenInfo={description:"",methods:[],displayName:"CloseLink"};var x=`import React from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import CloseLink from '..';
+
+export default {
+	title: 'Packages/My Jetpack/CloseLink',
+	component: CloseLink,
+};
+
+const DefaultArgs = {};
+
+const Template = args => (
+	<HashRouter>
+		<Routes>
+			<Route path="/" element={ <CloseLink { ...args } /> } />
+		</Routes>
+	</HashRouter>
+);
+
+export const _default = Template.bind( {} );
+_default.parameters = {};
+_default.args = DefaultArgs;
+`,g={_default:{startLoc:{col:17,line:12},endLoc:{col:1,line:18},startBody:{col:17,line:12},endBody:{col:1,line:18}}};const y={parameters:{storySource:{source:`import React from 'react';
+import { HashRouter, Routes, Route } from 'react-router-dom';
+import CloseLink from '..';
+
+export default {
+	title: 'Packages/My Jetpack/CloseLink',
+	component: CloseLink,
+};
+
+const DefaultArgs = {};
+
+const Template = args => (
+	<HashRouter>
+		<Routes>
+			<Route path="/" element={ <CloseLink { ...args } /> } />
+		</Routes>
+	</HashRouter>
+);
+
+export const _default = Template.bind( {} );
+_default.parameters = {};
+_default.args = DefaultArgs;
+`,locationsMap:{default:{startLoc:{col:17,line:12},endLoc:{col:1,line:18},startBody:{col:17,line:12},endBody:{col:1,line:18}}}}},title:"Packages/My Jetpack/CloseLink",component:j},A={},P=R=>(0,m.jsx)(l.UT,{children:(0,m.jsx)(n.Z5,{children:(0,m.jsx)(n.AW,{path:"/",element:(0,m.jsx)(j,{...R})})})});P.displayName="Template";const p=P.bind({});p.parameters={},p.args=A,p.parameters={...p.parameters,docs:{...(h=p.parameters)==null?void 0:h.docs,source:{originalSource:`args => <HashRouter>
+        <Routes>
+            <Route path="/" element={<CloseLink {...args} />} />
+        </Routes>
+    </HashRouter>`,...(D=(O=p.parameters)==null?void 0:O.docs)==null?void 0:D.source}}};const C=["_default"]},"../../../node_modules/.pnpm/@wordpress+icons@9.42.0_react@18.2.0/node_modules/@wordpress/icons/build-module/icon/index.js":(_,r,e)=>{"use strict";e.d(r,{Z:()=>n});var a=e("../../../node_modules/.pnpm/react@18.2.0/node_modules/react/index.js");function l({icon:d,size:t=24,...c},o){return(0,a.cloneElement)(d,{width:t,height:t,...c,ref:o})}const n=(0,a.forwardRef)(l)},"../../../node_modules/.pnpm/@wordpress+icons@9.42.0_react@18.2.0/node_modules/@wordpress/icons/build-module/library/close.js":(_,r,e)=>{"use strict";e.d(r,{Z:()=>d});var a=e("../../../node_modules/.pnpm/react@18.2.0/node_modules/react/index.js"),l=e("../../../node_modules/.pnpm/@wordpress+primitives@3.49.0/node_modules/@wordpress/primitives/build-module/svg/index.js");const d=(0,a.createElement)(l.Wj,{xmlns:"http://www.w3.org/2000/svg",viewBox:"0 0 24 24"},(0,a.createElement)(l.y$,{d:"M13 11.8l6.1-6.3-1-1-6.1 6.2-6.1-6.2-1 1 6.1 6.3-6.5 6.7 1 1 6.5-6.6 6.5 6.6 1-1z"}))},"../../../node_modules/.pnpm/@wordpress+primitives@3.49.0/node_modules/@wordpress/primitives/build-module/svg/index.js":(_,r,e)=>{"use strict";e.d(r,{Cd:()=>d,G:()=>t,UL:()=>E,Wj:()=>m,mg:()=>i,x1:()=>c,y$:()=>o});var a=e("../../../node_modules/.pnpm/classnames@2.3.2/node_modules/classnames/index.js"),l=e.n(a),n=e("../../../node_modules/.pnpm/react@18.2.0/node_modules/react/index.js");const d=s=>(0,n.createElement)("circle",s),t=s=>(0,n.createElement)("g",s),c=s=>(0,n.createElement)("line",s),o=s=>(0,n.createElement)("path",s),i=s=>(0,n.createElement)("polygon",s),E=s=>(0,n.createElement)("rect",s),u=s=>createElement("defs",s),f=s=>createElement("radialGradient",s),L=s=>createElement("linearGradient",s),v=s=>createElement("stop",s),m=(0,n.forwardRef)(({className:s,isPressed:j,...x},g)=>{const y={...x,className:l()(s,{"is-pressed":j})||void 0,"aria-hidden":!0,focusable:!1};return(0,n.createElement)("svg",{...y,ref:g})});m.displayName="SVG"},"../../../node_modules/.pnpm/classnames@2.3.2/node_modules/classnames/index.js":(_,r)=>{var e,a;/*!
+	Copyright (c) 2018 Jed Watson.
+	Licensed under the MIT License (MIT), see
+	http://jedwatson.github.io/classnames
+*/(function(){"use strict";var l={}.hasOwnProperty,n="[native code]";function d(){for(var t=[],c=0;c<arguments.length;c++){var o=arguments[c];if(o){var i=typeof o;if(i==="string"||i==="number")t.push(o);else if(Array.isArray(o)){if(o.length){var E=d.apply(null,o);E&&t.push(E)}}else if(i==="object"){if(o.toString!==Object.prototype.toString&&!o.toString.toString().includes("[native code]")){t.push(o.toString());continue}for(var u in o)l.call(o,u)&&o[u]&&t.push(u)}}}return t.join(" ")}_.exports?(d.default=d,_.exports=d):(e=[],a=function(){return d}.apply(r,e),a!==void 0&&(_.exports=a))})()},"../../../node_modules/.pnpm/css-loader@6.5.1_webpack@5.76.0/node_modules/css-loader/dist/cjs.js!../../../node_modules/.pnpm/postcss-loader@6.2.0_postcss@8.4.31_webpack@5.76.0/node_modules/postcss-loader/dist/cjs.js??ruleSet[1].rules[16].use[2]!../../../node_modules/.pnpm/sass-loader@12.4.0_sass@1.64.1_webpack@5.76.0/node_modules/sass-loader/dist/cjs.js!../../packages/my-jetpack/_inc/components/close-link/styles.module.scss":(_,r,e)=>{"use strict";e.d(r,{Z:()=>c});var a=e("../../../node_modules/.pnpm/css-loader@6.5.1_webpack@5.76.0/node_modules/css-loader/dist/runtime/noSourceMaps.js"),l=e.n(a),n=e("../../../node_modules/.pnpm/css-loader@6.5.1_webpack@5.76.0/node_modules/css-loader/dist/runtime/api.js"),d=e.n(n),t=d()(l());t.push([_.id,".ObIcQSmxDv91Nj5rgOpJ{--gray-90: #1E1E1E;fill:var(--gray-90);width:24px}.jJJPTXA5yQQ8yWmUdGxT{display:flex;text-decoration:none;align-items:center}",""]),t.locals={icon:"ObIcQSmxDv91Nj5rgOpJ",link:"jJJPTXA5yQQ8yWmUdGxT"};const c=t}}]);})();
---
+++ docs/features-image-cdn-quality-control-stories-quality-control-stories.ad146c67.iframe.bundle.js
@@ -0,0 +1,131 @@
+"use strict";(()=>{(self.webpackChunk_automattic_jetpack_storybook=self.webpackChunk_automattic_jetpack_storybook||[]).push([[3137],{"../../plugins/boost/app/assets/src/js/features/image-cdn/quality-control/stories/quality-control.stories.tsx":(x,u,t)=>{var S,h,E;t.r(u),t.d(u,{__namedExportsOrder:()=>O,_default:()=>n,default:()=>N});var d=t("../components/components/number-slider/index.tsx"),m=t("../../../node_modules/.pnpm/classnames@2.3.2/node_modules/classnames/index.js"),p=t.n(m),_=t("../../../node_modules/.pnpm/@wordpress+i18n@4.51.0/node_modules/@wordpress/i18n/build-module/index.js"),o=t("../../../node_modules/.pnpm/style-loader@2.0.0_webpack@5.76.0/node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js"),f=t.n(o),V=t("../../../node_modules/.pnpm/css-loader@6.5.1_webpack@5.76.0/node_modules/css-loader/dist/cjs.js!../../../node_modules/.pnpm/postcss-loader@6.2.0_postcss@8.4.31_webpack@5.76.0/node_modules/postcss-loader/dist/cjs.js??ruleSet[1].rules[16].use[2]!../../../node_modules/.pnpm/sass-loader@12.4.0_sass@1.64.1_webpack@5.76.0/node_modules/sass-loader/dist/cjs.js!../../plugins/boost/app/assets/src/js/features/image-cdn/quality-control/quality-control.module.scss"),g={};g.insert="head",g.singleton=!1;var Q=f()(V.Z,g);const l=V.Z.locals||{};var a=t("../../../node_modules/.pnpm/react@18.2.0/node_modules/react/index.js"),s=t("../../../node_modules/.pnpm/react@18.2.0/node_modules/react/jsx-runtime.js");const T=_.__,q=({label:e,quality:r,lossless:i,setQuality:y,setLossless:b,maxValue:w,minValue:L=20})=>{const v=(0,a.useId)(),[M,R]=(0,a.useState)(r);return(0,a.useEffect)(()=>{R(r)},[r]),(0,s.jsxs)("div",{className:l["quality-control"],children:[(0,s.jsx)("div",{className:l.label,children:e}),(0,s.jsx)("div",{className:p()(l.slider,{[l.disabled]:i}),children:(0,s.jsx)(d.Z,{value:M,onAfterChange:c=>{R(c),y(c)},minValue:L,maxValue:w})}),(0,s.jsxs)("label",{className:l.lossless,htmlFor:v,children:[(0,s.jsx)("input",{type:"checkbox",checked:i,id:v,onChange:c=>b(c.target.checked)}),T("Lossless","jetpack-boost")]})]})};q.displayName="QualityControl";const C=q;try{qualitycontrol.displayName="qualitycontrol",qualitycontrol.__docgenInfo={description:"",displayName:"qualitycontrol",props:{label:{defaultValue:null,description:"",name:"label",required:!0,type:{name:"string"}},quality:{defaultValue:null,description:"",name:"quality",required:!0,type:{name:"number"}},lossless:{defaultValue:null,description:"",name:"lossless",required:!0,type:{name:"boolean"}},setQuality:{defaultValue:null,description:"",name:"setQuality",required:!0,type:{name:"(newValue: number) => void"}},setLossless:{defaultValue:null,description:"",name:"setLossless",required:!0,type:{name:"(newValue: boolean) => void"}},maxValue:{defaultValue:null,description:"",name:"maxValue",required:!0,type:{name:"number"}},minValue:{defaultValue:{value:"20"},description:"",name:"minValue",required:!1,type:{name:"number"}}}},typeof STORYBOOK_REACT_CLASSES!="undefined"&&(STORYBOOK_REACT_CLASSES["../../plugins/boost/app/assets/src/js/features/image-cdn/quality-control/quality-control.tsx#qualitycontrol"]={docgenInfo:qualitycontrol.__docgenInfo,name:"qualitycontrol",path:"../../plugins/boost/app/assets/src/js/features/image-cdn/quality-control/quality-control.tsx#qualitycontrol"})}catch(e){}var k=`import type { Meta } from '@storybook/react';
+import QualityControl from '../quality-control';
+import React from 'react';
+
+const meta: Meta< typeof QualityControl > = {
+	title: 'Plugins/Boost/Image CDN/QualityControl',
+	component: QualityControl,
+	argTypes: {
+		label: {control: 'text'},
+		"config.lossless": {control: 'boolean'},
+		"config.quality": {control: 'number'},
+		maxValue: {control: 'number'},
+		minValue: {number: 'number'},
+	},
+	decorators: [
+		Story => (
+			<div style={ { maxWidth: '600px', margin: '200px auto', fontSize: '16px' } }>
+				<Story />
+			</div>
+		),
+	],
+};
+
+const defaultValues = {
+	label: 'JPEG',
+	"config.lossless": false,
+	"config.quality": 75,
+	maxValue: 80,
+	minValue: 20,
+};
+
+export default meta;
+
+const Template = args => {
+	const [config, setConfig] = React.useState( {
+		lossless: args["config.lossless"],
+		quality: args["config.quality"],
+	} );
+
+	React.useEffect( () => {
+		setConfig( {
+			lossless: args["config.lossless"],
+			quality: args["config.quality"],
+		} );
+	}, [args["config.lossless"], args["config.quality"]] );
+
+	const props = {
+		label: args.label,
+		config,
+		maxValue: args.maxValue,
+		minValue: args.minValue,
+	}
+	return <QualityControl { ...props } onChange={(newVal) => setConfig(newVal)} />
+};
+export const _default = Template.bind( {} );
+_default.args = defaultValues;
+`,A={_default:{startLoc:{col:17,line:34},endLoc:{col:1,line:54},startBody:{col:17,line:34},endBody:{col:1,line:54}}};const P={parameters:{storySource:{source:`import type { Meta } from '@storybook/react';
+import QualityControl from '../quality-control';
+import React from 'react';
+
+const meta: Meta< typeof QualityControl > = {
+	title: 'Plugins/Boost/Image CDN/QualityControl',
+	component: QualityControl,
+	argTypes: {
+		label: {control: 'text'},
+		"config.lossless": {control: 'boolean'},
+		"config.quality": {control: 'number'},
+		maxValue: {control: 'number'},
+		minValue: {number: 'number'},
+	},
+	decorators: [
+		Story => (
+			<div style={ { maxWidth: '600px', margin: '200px auto', fontSize: '16px' } }>
+				<Story />
+			</div>
+		),
+	],
+};
+
+const defaultValues = {
+	label: 'JPEG',
+	"config.lossless": false,
+	"config.quality": 75,
+	maxValue: 80,
+	minValue: 20,
+};
+
+export default meta;
+
+const Template = args => {
+	const [config, setConfig] = React.useState( {
+		lossless: args["config.lossless"],
+		quality: args["config.quality"],
+	} );
+
+	React.useEffect( () => {
+		setConfig( {
+			lossless: args["config.lossless"],
+			quality: args["config.quality"],
+		} );
+	}, [args["config.lossless"], args["config.quality"]] );
+
+	const props = {
+		label: args.label,
+		config,
+		maxValue: args.maxValue,
+		minValue: args.minValue,
+	}
+	return <QualityControl { ...props } onChange={(newVal) => setConfig(newVal)} />
+};
+export const _default = Template.bind( {} );
+_default.args = defaultValues;
+`,locationsMap:{default:{startLoc:{col:17,line:34},endLoc:{col:1,line:54},startBody:{col:17,line:34},endBody:{col:1,line:54}}}}},title:"Plugins/Boost/Image CDN/QualityControl",component:C,argTypes:{label:{control:"text"},"config.lossless":{control:"boolean"},"config.quality":{control:"number"},maxValue:{control:"number"},minValue:{number:"number"}},decorators:[e=>(0,s.jsx)("div",{style:{maxWidth:"600px",margin:"200px auto",fontSize:"16px"},children:(0,s.jsx)(e,{})})]},I={label:"JPEG","config.lossless":!1,"config.quality":75,maxValue:80,minValue:20},N=P,j=e=>{const[r,i]=a.useState({lossless:e["config.lossless"],quality:e["config.quality"]});a.useEffect(()=>{i({lossless:e["config.lossless"],quality:e["config.quality"]})},[e["config.lossless"],e["config.quality"]]);const y={label:e.label,config:r,maxValue:e.maxValue,minValue:e.minValue};return(0,s.jsx)(C,{...y,onChange:b=>i(b)})};j.displayName="Template";const n=j.bind({});n.args=I,n.parameters={...n.parameters,docs:{...(S=n.parameters)==null?void 0:S.docs,source:{originalSource:`args => {
+  const [config, setConfig] = React.useState({
+    lossless: args["config.lossless"],
+    quality: args["config.quality"]
+  });
+  React.useEffect(() => {
+    setConfig({
+      lossless: args["config.lossless"],
+      quality: args["config.quality"]
+    });
+  }, [args["config.lossless"], args["config.quality"]]);
+  const props = {
+    label: args.label,
+    config,
+    maxValue: args.maxValue,
+    minValue: args.minValue
+  };
+  return <QualityControl {...props} onChange={newVal => setConfig(newVal)} />;
+}`,...(E=(h=n.parameters)==null?void 0:h.docs)==null?void 0:E.source}}};const O=["_default"]},"../../../node_modules/.pnpm/css-loader@6.5.1_webpack@5.76.0/node_modules/css-loader/dist/cjs.js!../../../node_modules/.pnpm/postcss-loader@6.2.0_postcss@8.4.31_webpack@5.76.0/node_modules/postcss-loader/dist/cjs.js??ruleSet[1].rules[16].use[2]!../../../node_modules/.pnpm/sass-loader@12.4.0_sass@1.64.1_webpack@5.76.0/node_modules/sass-loader/dist/cjs.js!../../plugins/boost/app/assets/src/js/features/image-cdn/quality-control/quality-control.module.scss":(x,u,t)=>{t.d(u,{Z:()=>f});var d=t("../../../node_modules/.pnpm/css-loader@6.5.1_webpack@5.76.0/node_modules/css-loader/dist/runtime/noSourceMaps.js"),m=t.n(d),p=t("../../../node_modules/.pnpm/css-loader@6.5.1_webpack@5.76.0/node_modules/css-loader/dist/runtime/api.js"),_=t.n(p),o=_()(m());o.push([x.id,".skHPs2a8rLEzu399XlQb{display:flex;align-items:center;min-height:40px;margin-top:8px;gap:20px}.GuDwwHXl8ABNieRWxNRt{min-width:4rem}.abIPyZfa8Y3a0jR9mM9x{flex-shrink:0}.u5o8ZbblI7r7dNh8bgNi{width:100%}.z33KJTmJ5fZIHUrGeCqt{opacity:.3;filter:grayscale(1);pointer-events:none}@media screen and (max-width: 782px){.skHPs2a8rLEzu399XlQb{flex-direction:column;align-items:flex-start;gap:0;margin-bottom:30px}.GuDwwHXl8ABNieRWxNRt{margin:0;font-weight:500}}",""]),o.locals={"quality-control":"skHPs2a8rLEzu399XlQb",label:"GuDwwHXl8ABNieRWxNRt",lossless:"abIPyZfa8Y3a0jR9mM9x",slider:"u5o8ZbblI7r7dNh8bgNi",disabled:"z33KJTmJ5fZIHUrGeCqt"};const f=o}}]);})();
