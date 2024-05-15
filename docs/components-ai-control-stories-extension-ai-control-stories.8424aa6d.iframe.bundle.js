285e3c75bce9fa79fc53f4f11ac40fd3c123538d
anomiex <anomiex@users.noreply.github.com>
2024-05-15 16:35:48 +0000

    Update react monorepo to v18.3.1 (#37380)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/9099249530
    
    Upstream-Ref: Automattic/jetpack@7f3e5f718176948d5cf8e7352561f0bc49f784ee

---
+++ docs/components-ai-control-stories-extension-ai-control-stories.8424aa6d.iframe.bundle.js
@@ -0,0 +1,51 @@
+"use strict";(()=>{(self.webpackChunk_automattic_jetpack_storybook=self.webpackChunk_automattic_jetpack_storybook||[]).push([[9435],{"../ai-client/src/components/ai-control/stories/extension-ai-control.stories.tsx":(f,u,o)=>{var p,_,h;o.r(u),o.d(u,{Default:()=>s,__namedExportsOrder:()=>v,default:()=>A});var t=o("../../../node_modules/.pnpm/@storybook+addon-actions@8.0.9/node_modules/@storybook/addon-actions/dist/index.mjs"),c=o("@storybook/preview-api"),y=o.n(c),M=o("../../../node_modules/.pnpm/react@18.3.1/node_modules/react/index.js"),i=o("../ai-client/src/components/ai-control/index.tsx"),a=o("../../../node_modules/.pnpm/react@18.3.1/node_modules/react/jsx-runtime.js");const A={title:"JS Packages/AI Client/AI Control/Extension AI Control",component:i.C3,decorators:[n=>(0,a.jsx)("div",{style:{backgroundColor:"white"},children:(0,a.jsx)(n,{})})],argTypes:{state:{control:{type:"select"},options:["init","requesting","suggesting","done","error"]},error:{control:{type:"text"}},requestsRemaining:{control:{type:"number"}}},parameters:{controls:{exclude:/on[A-Z].*/}}},U=n=>{var C;const[{value:r},d,l]=(0,c.useArgs)(),D=e=>{var m;d({value:e}),(m=n==null?void 0:n.onChange)==null||m.call(n,e)},S=()=>{var e;d({state:"requesting",error:null,value:"",placeholder:r}),setTimeout(()=>{d({state:"suggesting"}),setTimeout(()=>{d({state:"done"})},3e3)},1e3),(e=n==null?void 0:n.onSend)==null||e.call(n,r)},x=()=>{var e;d({state:"done",error:null}),(e=n==null?void 0:n.onStop)==null||e.call(n)},P=()=>{var e;l(),(e=n==null?void 0:n.onClose)==null||e.call(n),l()},O=()=>{var e;l(),(e=n==null?void 0:n.onUndo)==null||e.call(n),l()},T=()=>{var e;(e=n==null?void 0:n.onUpgrade)==null||e.call(n)};return(0,a.jsx)(i.C3,{...n,onChange:D,onSend:S,onStop:x,onClose:P,onUndo:O,onUpgrade:T,value:(C=n==null?void 0:n.value)!=null?C:r})},E={value:"",placeholder:"Placeholder",showButtonLabels:!0,disabled:!1,isTransparent:!1,state:"init",showGuideLine:!1,error:null,requestsRemaining:null,showUpgradeMessage:!1,onChange:(0,t.aD)("onChange"),onSend:(0,t.aD)("onSend"),onStop:(0,t.aD)("onStop"),onClose:(0,t.aD)("onClose"),onUndo:(0,t.aD)("onUndo"),onUpgrade:(0,t.aD)("onUpgrade")},s=U.bind({});s.args=E,s.parameters={...s.parameters,docs:{...(p=s.parameters)==null?void 0:p.docs,source:{originalSource:`args => {
+  const [{
+    value
+  }, updateArgs, resetArgs] = useArgs();
+  const handleChange = (newValue: string) => {
+    updateArgs({
+      value: newValue
+    });
+    args?.onChange?.(newValue);
+  };
+  const handleSend = () => {
+    updateArgs({
+      state: 'requesting',
+      error: null,
+      value: '',
+      placeholder: value
+    });
+    setTimeout(() => {
+      updateArgs({
+        state: 'suggesting'
+      });
+      setTimeout(() => {
+        updateArgs({
+          state: 'done'
+        });
+      }, 3000);
+    }, 1000);
+    args?.onSend?.(value);
+  };
+  const handleStop = () => {
+    updateArgs({
+      state: 'done',
+      error: null
+    });
+    args?.onStop?.();
+  };
+  const handleClose = () => {
+    resetArgs();
+    args?.onClose?.();
+    resetArgs();
+  };
+  const handleUndo = () => {
+    resetArgs();
+    args?.onUndo?.();
+    resetArgs();
+  };
+  const handleUpgrade = () => {
+    args?.onUpgrade?.();
+  };
+  return <ExtensionAIControl {...args} onChange={handleChange} onSend={handleSend} onStop={handleStop} onClose={handleClose} onUndo={handleUndo} onUpgrade={handleUpgrade} value={args?.value ?? value} />;
+}`,...(h=(_=s.parameters)==null?void 0:_.docs)==null?void 0:h.source}}};const v=["Default"]}}]);})();
---
+++ docs/docs-howto-mdx.bd5ba4ba.iframe.bundle.js
@@ -0,0 +1,26 @@
+(()=>{(self.webpackChunk_automattic_jetpack_storybook=self.webpackChunk_automattic_jetpack_storybook||[]).push([[3450],{"./storybook/stories/docs/howto.mdx":(r,n,t)=>{"use strict";t.r(n),t.d(n,{default:()=>a});var d=t("../../../node_modules/.pnpm/react@18.3.1/node_modules/react/index.js"),e=t("../../../node_modules/.pnpm/react@18.3.1/node_modules/react/jsx-runtime.js"),c=t("../../../node_modules/.pnpm/@mdx-js+react@3.0.1_@types+react@18.3.1_react@18.3.1/node_modules/@mdx-js/react/lib/index.js"),i=t("../../../node_modules/.pnpm/@storybook+blocks@8.0.9_react-dom@18.3.1_react@18.3.1/node_modules/@storybook/blocks/dist/index.mjs"),m=t("../../../node_modules/.pnpm/ts-dedent@2.2.0/node_modules/ts-dedent/esm/index.js");function l(s){const o={a:"a",code:"code",h1:"h1",h2:"h2",p:"p",pre:"pre",...(0,c.a)(),...s.components};return(0,e.jsxs)(e.Fragment,{children:[(0,e.jsx)(i.h_,{title:"Docs/HowTo"}),`
+`,(0,e.jsx)(o.h1,{id:"how-to-use-this-tool",children:"How to use this tool"}),`
+`,(0,e.jsx)(o.p,{children:"The Jetpack Components Library allows you to discover, test and live-edit Jetpack visual component."}),`
+`,(0,e.jsx)(o.h2,{id:"publish-component-stories",children:"Publish component stories"}),`
+`,(0,e.jsxs)(o.p,{children:["Anywhere in a supported package, create one or more directories called ",(0,e.jsx)(o.code,{children:"stories"})," (e.g. src/components/my-component/stories)."]}),`
+`,(0,e.jsx)(o.p,{children:"In that directory, create an index.stories.js or index.stories.jsx file, like this:"}),`
+`,(0,e.jsx)(i.Hw,{language:"jsx",code:m.C`
+		/**
+		 * Internal dependencies
+		 */
+		import MyComponent from '../index.jsx';
+		// the default export is metadata about the component
+		export default {
+			title: 'Components/My Component',
+		};
+		// the export called __default is the default state of the component
+		export const _default = () => {
+			return <MyComponent foo="bar"/>;
+		};
+	`}),`
+`,(0,e.jsxs)(o.p,{children:["You can read more about writing stories in the ",(0,e.jsx)(o.a,{href:"https://storybook.js.org/docs/react/writing-stories/introduction",rel:"nofollow",children:"Storybook JS docs"})]}),`
+`,(0,e.jsx)(o.h2,{id:"edit-components-live",children:"Edit components live"}),`
+`,(0,e.jsx)(o.p,{children:"To run this locally, just run:"}),`
+`,(0,e.jsx)(o.pre,{children:(0,e.jsx)(o.code,{children:`pnpm run storybook:dev
+`})}),`
+`,(0,e.jsx)(o.p,{children:"Any edits to SCSS, HTML, JSX or other files should be hot-loaded so you can see changes to styles or components."})]})}function a(s={}){const{wrapper:o}={...(0,c.a)(),...s.components};return o?(0,e.jsx)(o,{...s,children:(0,e.jsx)(l,{...s})}):l(s)}},"../../../node_modules/.pnpm/memoizerific@1.11.3/node_modules/memoizerific sync recursive":r=>{function n(t){var d=new Error("Cannot find module '"+t+"'");throw d.code="MODULE_NOT_FOUND",d}n.keys=()=>[],n.resolve=n,n.id="../../../node_modules/.pnpm/memoizerific@1.11.3/node_modules/memoizerific sync recursive",r.exports=n}}]);})();
