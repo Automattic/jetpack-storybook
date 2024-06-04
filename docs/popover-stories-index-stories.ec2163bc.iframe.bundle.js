cf49958721723fda97bc7a49b9d9427562e61704
anomiex <anomiex@users.noreply.github.com>
2024-06-04 16:59:46 +0000

    Update wordpress monorepo (#37669)
    * Update wordpress monorepo
    * Pnpmfile hack for `@wordpress/primitives` missing `react` dep
    * jest: Mock `client-zip`, it doesn't even try to work with jest.
    * Update tests for wordpress/gutenberg#61366
    * Don't upgrade `@wordpress/dependency-extraction-webpack-plugin` past 5.9.0
    * Add pnpmfile hack to prevent accidentally re-updating `@wordpress/dependency-extraction-webpack-plugin`.
    * Update `@automattic/social-previews` pnpmfile hack to avoid duplicate `@wordpress/*` package versions.
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/9370984130
    
    Upstream-Ref: Automattic/jetpack@3273e03932bb8d55d574ae540493b536b1bdbf2b

--- docs/popover-stories-index-stories.ec2163bc.iframe.bundle.js
+++
@@ -1,9 +0,0 @@
-"use strict";(()=>{(self.webpackChunk_automattic_jetpack_storybook=self.webpackChunk_automattic_jetpack_storybook||[]).push([[6028],{"../components/components/popover/stories/index.stories.tsx":(l,t,e)=>{var a,c,i;e.r(t),e.d(t,{__namedExportsOrder:()=>_,_default:()=>n,default:()=>m});var p=e("../components/components/button/index.tsx"),d=e("../components/components/icons/index.tsx"),r=e("../components/components/popover/index.tsx"),o=e("../../../node_modules/.pnpm/react@18.3.1/node_modules/react/jsx-runtime.js");const m={title:"JS Packages/Components/Popover",component:r.Z,argTypes:{icon:{control:"object"},action:{control:"object"}},decorators:[s=>(0,o.jsx)("div",{style:{width:"600px",maxWidth:"90%",height:"600px",fontSize:"16px"},children:(0,o.jsx)(s,{})})]},n=(s=>(0,o.jsx)(r.Z,{...s,children:(0,o.jsxs)("p",{children:["This is line is just longer and in another paragraph.",(0,o.jsx)("br",{}),"This is the message."]})})).bind({});n.args={icon:(0,o.jsx)(d.CheckmarkIcon,{}),action:(0,o.jsx)(p.Z,{children:"Click me"})},n.parameters={...n.parameters,docs:{...(a=n.parameters)==null?void 0:a.docs,source:{originalSource:`args => {
-  return <Popover {...args}>
-            <p>
-                This is line is just longer and in another paragraph.
-                <br />
-                This is the message.
-            </p>
-        </Popover>;
-}`,...(i=(c=n.parameters)==null?void 0:c.docs)==null?void 0:i.source}}};const _=["_default"]}}]);})();
