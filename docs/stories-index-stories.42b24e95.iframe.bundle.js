7068dff49f4c2716f48bba078c5958e887a702b1
kraftbj <kraftbj@users.noreply.github.com>
2024-09-05 14:45:26 +0000

    ci: Check that React-using projects use react eslint rules (#39214)
    I noticed that several of our projects are using React but did not have
    our `jetpack-js-tools/eslintrc/react` eslint ruleset enabled. This adds
    a linting check for that, and enables it on most of the projects where
    it was not enabled.
    
    For a few I instead added a `@todo` comment about turning it on, due to
    a lot of cleanup being needed in order to successfully do so. I hope
    people more familiar with React and these projects will follow up on
    that.
    
    * js-packages/ai-client:
       32 × react/jsx-no-bind
       33 × react-hooks/exhaustive-deps
    * packages/videopress:
        8 × react-hooks/rules-of-hooks
       69 × react-hooks/exhaustive-deps
      157 × react/jsx-no-bind
    * plugins/boost:
        1 × react/no-danger
       81 × react/jsx-no-bind
    * plugins/crm:
        1 × react-hooks/rules-of-hooks
        2 × react/jsx-no-bind
        3 × react-hooks/exhaustive-deps
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/10722474820
    
    Upstream-Ref: Automattic/jetpack@d586800b8457f6106c1fecede9abf95d13ccfd24

---
+++ docs/stories-index-stories.42b24e95.iframe.bundle.js
@@ -0,0 +1,139 @@
+"use strict";(()=>{(self.webpackChunk_automattic_jetpack_storybook=self.webpackChunk_automattic_jetpack_storybook||[]).push([[6581],{"../social-logos/src/react/stories/index.stories.jsx":(i,n,s)=>{var T,C,L,M,N,P;s.r(n),s.d(n,{_All:()=>b,__namedExportsOrder:()=>W,_default:()=>S,default:()=>U});var l=s("../../../node_modules/.pnpm/react@18.3.1/node_modules/react/index.js"),a=s("../social-logos/src/react/social-logo.tsx"),e=s("../social-logos/src/react/social-logo-data.tsx"),o=s("../../../node_modules/.pnpm/style-loader@3.3.4_webpack@5.94.0_webpack-cli@4.9.1_/node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js"),t=s.n(o),u=s("../../../node_modules/.pnpm/style-loader@3.3.4_webpack@5.94.0_webpack-cli@4.9.1_/node_modules/style-loader/dist/runtime/styleDomAPI.js"),p=s.n(u),c=s("../../../node_modules/.pnpm/style-loader@3.3.4_webpack@5.94.0_webpack-cli@4.9.1_/node_modules/style-loader/dist/runtime/insertBySelector.js"),m=s.n(c),_=s("../../../node_modules/.pnpm/style-loader@3.3.4_webpack@5.94.0_webpack-cli@4.9.1_/node_modules/style-loader/dist/runtime/setAttributesWithoutAttributes.js"),r=s.n(_),f=s("../../../node_modules/.pnpm/style-loader@3.3.4_webpack@5.94.0_webpack-cli@4.9.1_/node_modules/style-loader/dist/runtime/insertStyleElement.js"),x=s.n(f),y=s("../../../node_modules/.pnpm/style-loader@3.3.4_webpack@5.94.0_webpack-cli@4.9.1_/node_modules/style-loader/dist/runtime/styleTagTransform.js"),O=s.n(y),w=s("../../../node_modules/.pnpm/css-loader@6.11.0_webpack@5.94.0_webpack-cli@4.9.1_/node_modules/css-loader/dist/cjs.js??ruleSet[1].rules[7].use[1]!../social-logos/src/css/example.css"),g={};g.styleTagTransform=O(),g.setAttributes=r(),g.insert=m().bind(null,"head"),g.domAPI=p(),g.insertStyleElement=x();var H=t()(w.A,g);const X=w.A&&w.A.locals?w.A.locals:void 0;var d=s("../../../node_modules/.pnpm/react@18.3.1/node_modules/react/jsx-runtime.js");function D({name:h,iconSize:v,showIconNames:j}){const E=(0,l.useCallback)(()=>{const I=`<SocialLogo icon="${h}" size="${v}" />`;window.prompt("Copy component code:",I)},[v,h]);return(0,d.jsxs)("div",{children:[(0,d.jsx)(a.d,{icon:h,size:v,onClick:E}),j&&(0,d.jsx)("p",{children:h})]},h)}function A(){const[h,v]=(0,l.useState)(!1),[j,E]=(0,l.useState)(!0),I=h?24:48,K=(0,l.useCallback)(k=>{v(k.target.checked)},[v]),z=(0,l.useCallback)(k=>{E(k.target.checked)},[E]),F=e.$.map(k=>(0,d.jsx)(D,{name:k.name,iconSize:I,showIconNames:j}));return(0,d.jsxs)("div",{className:"social-logos-example",children:[(0,d.jsx)("h1",{children:"Social Logos"}),(0,d.jsxs)("div",{className:"display-control-group",children:[(0,d.jsxs)("div",{className:"display-control",children:[(0,d.jsx)("h4",{children:"Small icons"}),(0,d.jsxs)("label",{className:"switch",children:[(0,d.jsx)("input",{type:"checkbox",onChange:K,checked:h}),(0,d.jsx)("span",{className:"handle"})]})]}),(0,d.jsxs)("div",{className:"display-control",children:[(0,d.jsx)("h4",{children:"Icon names"}),(0,d.jsxs)("label",{className:"switch",children:[(0,d.jsx)("input",{type:"checkbox",onChange:z,checked:j}),(0,d.jsx)("span",{className:"handle"}),(0,d.jsx)("span",{className:"switch-label","data-on":"On","data-off":"Off"})]})]})]}),(0,d.jsx)("div",{className:"icons",children:F}),(0,d.jsx)("p",{children:(0,d.jsx)("a",{href:"https://github.com/Automattic/social-logos",children:"GitHub"})})]})}const R=A;try{A.displayName="SocialLogosExample",A.__docgenInfo={description:"An example React component that displays all the social logos.",displayName:"SocialLogosExample",props:{}},typeof STORYBOOK_REACT_CLASSES!="undefined"&&(STORYBOOK_REACT_CLASSES["../social-logos/src/react/example.tsx#SocialLogosExample"]={docgenInfo:A.__docgenInfo,name:"SocialLogosExample",path:"../social-logos/src/react/example.tsx#SocialLogosExample"})}catch(h){}var B=s("../social-logos/src/react/index.ts");const U={title:"JS Packages/Social Logos/Icons"},S=()=>(0,d.jsx)(B.Ay,{icon:"wordpress"}),b=()=>(0,d.jsx)(R,{}),W=["_default","_All"];S.parameters={...S.parameters,docs:{...(T=S.parameters)==null?void 0:T.docs,source:{originalSource:`() => {
+  return <SocialLogo icon="wordpress" />;
+}`,...(L=(C=S.parameters)==null?void 0:C.docs)==null?void 0:L.source}}},b.parameters={...b.parameters,docs:{...(M=b.parameters)==null?void 0:M.docs,source:{originalSource:`() => {
+  return <SocialLogoExamples />;
+}`,...(P=(N=b.parameters)==null?void 0:N.docs)==null?void 0:P.source}}}},"../../../node_modules/.pnpm/css-loader@6.11.0_webpack@5.94.0_webpack-cli@4.9.1_/node_modules/css-loader/dist/cjs.js??ruleSet[1].rules[7].use[1]!../social-logos/src/css/example.css":(i,n,s)=>{s.d(n,{A:()=>u});var l=s("../../../node_modules/.pnpm/css-loader@6.11.0_webpack@5.94.0_webpack-cli@4.9.1_/node_modules/css-loader/dist/runtime/noSourceMaps.js"),a=s.n(l),e=s("../../../node_modules/.pnpm/css-loader@6.11.0_webpack@5.94.0_webpack-cli@4.9.1_/node_modules/css-loader/dist/runtime/api.js"),o=s.n(e),t=o()(a());t.push([i.id,`.social-logos-example {
+	max-width: 900px;
+	margin: 100px auto;
+	color: #767676;
+	font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen-Sans", "Ubuntu", "Cantarell", "Helvetica Neue", sans-serif;
+	line-height: 1.15;
+
+	a:link, a:visited {
+		color: #999;
+	}
+
+	a:active {
+		color: #1fc1ad;
+	}
+
+	h1 {
+		text-align: center;
+		font-size: 24pt;
+	}
+
+	[type=checkbox] {
+		margin: 0;
+	}
+
+	.icons {
+		padding: 0 20px;
+		overflow: hidden;
+		margin-bottom: 50px;
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: center;
+		
+		div {
+			width: 64px;
+			height: 64px;
+			float: left;
+			padding: 6px 2px;
+			position: relative;
+			font-size: 7pt;
+			cursor: pointer;
+			text-align: center;
+
+			p {
+				margin: 0;
+				color: #767676;
+				text-align: center;
+				overflow: hidden;
+				max-height: 2.2em;
+				word-break: break-word;
+
+				.is-hidden {
+					display: none;
+				}
+			}
+
+			div:hover svg {
+				fill: #1fc1ad;
+			}
+		}
+	}
+
+	.display-control-group {
+		display: flex;
+		justify-content: space-around;
+		margin-bottom: 20px;
+	}
+
+	.display-control {
+		display: flex;
+
+		h4 {
+			margin: 0 10px 0 0;
+		}
+	}
+
+	.switch {
+		position: relative;
+		display: inline-block;
+		width: 40px;
+		height: 20px;
+
+		input {
+			-webkit-appearance: none;
+			-moz-appearance: none;
+			appearance: none;
+		}
+	}
+
+	.handle {
+		position: absolute;
+		cursor: pointer;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		border: 2px solid gray;
+		border-radius: 10px;
+		transition: .4s;
+		box-sizing: border-box;
+	}
+
+	.handle:before {
+		position: absolute;
+		content: "";
+		height: 12px;
+		width: 12px;
+		left: 2px;
+		bottom: 2px;
+		background: gray;
+		border-radius: 50%;
+		transition: .4s;
+	}
+
+	input:checked + .handle {
+		border-color: #3AA662;
+	}
+
+	input:checked + .handle:before {
+		background: #3AA662;
+		transform: translateX(20px);
+	}
+
+	input:focus + .handle {
+		box-shadow: 0 0 3px #2196F3;
+	}
+
+
+	> p {
+		text-align: center;
+		margin-bottom: 2em;
+	}
+	
+}
+`,""]);const u=t},"../../../node_modules/.pnpm/css-loader@6.11.0_webpack@5.94.0_webpack-cli@4.9.1_/node_modules/css-loader/dist/runtime/api.js":i=>{i.exports=function(n){var s=[];return s.toString=function(){return this.map(function(a){var e="",o=typeof a[5]!="undefined";return a[4]&&(e+="@supports (".concat(a[4],") {")),a[2]&&(e+="@media ".concat(a[2]," {")),o&&(e+="@layer".concat(a[5].length>0?" ".concat(a[5]):""," {")),e+=n(a),o&&(e+="}"),a[2]&&(e+="}"),a[4]&&(e+="}"),e}).join("")},s.i=function(a,e,o,t,u){typeof a=="string"&&(a=[[null,a,void 0]]);var p={};if(o)for(var c=0;c<this.length;c++){var m=this[c][0];m!=null&&(p[m]=!0)}for(var _=0;_<a.length;_++){var r=[].concat(a[_]);o&&p[r[0]]||(typeof u!="undefined"&&(typeof r[5]=="undefined"||(r[1]="@layer".concat(r[5].length>0?" ".concat(r[5]):""," {").concat(r[1],"}")),r[5]=u),e&&(r[2]&&(r[1]="@media ".concat(r[2]," {").concat(r[1],"}")),r[2]=e),t&&(r[4]?(r[1]="@supports (".concat(r[4],") {").concat(r[1],"}"),r[4]=t):r[4]="".concat(t)),s.push(r))}},s}},"../../../node_modules/.pnpm/css-loader@6.11.0_webpack@5.94.0_webpack-cli@4.9.1_/node_modules/css-loader/dist/runtime/noSourceMaps.js":i=>{i.exports=function(n){return n[1]}},"../../../node_modules/.pnpm/style-loader@3.3.4_webpack@5.94.0_webpack-cli@4.9.1_/node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js":i=>{var n=[];function s(e){for(var o=-1,t=0;t<n.length;t++)if(n[t].identifier===e){o=t;break}return o}function l(e,o){for(var t={},u=[],p=0;p<e.length;p++){var c=e[p],m=o.base?c[0]+o.base:c[0],_=t[m]||0,r="".concat(m," ").concat(_);t[m]=_+1;var f=s(r),x={css:c[1],media:c[2],sourceMap:c[3],supports:c[4],layer:c[5]};if(f!==-1)n[f].references++,n[f].updater(x);else{var y=a(x,o);o.byIndex=p,n.splice(p,0,{identifier:r,updater:y,references:1})}u.push(r)}return u}function a(e,o){var t=o.domAPI(o);t.update(e);var u=function(c){if(c){if(c.css===e.css&&c.media===e.media&&c.sourceMap===e.sourceMap&&c.supports===e.supports&&c.layer===e.layer)return;t.update(e=c)}else t.remove()};return u}i.exports=function(e,o){o=o||{},e=e||[];var t=l(e,o);return function(p){p=p||[];for(var c=0;c<t.length;c++){var m=t[c],_=s(m);n[_].references--}for(var r=l(p,o),f=0;f<t.length;f++){var x=t[f],y=s(x);n[y].references===0&&(n[y].updater(),n.splice(y,1))}t=r}}},"../../../node_modules/.pnpm/style-loader@3.3.4_webpack@5.94.0_webpack-cli@4.9.1_/node_modules/style-loader/dist/runtime/insertBySelector.js":i=>{var n={};function s(a){if(typeof n[a]=="undefined"){var e=document.querySelector(a);if(window.HTMLIFrameElement&&e instanceof window.HTMLIFrameElement)try{e=e.contentDocument.head}catch(o){e=null}n[a]=e}return n[a]}function l(a,e){var o=s(a);if(!o)throw new Error("Couldn't find a style target. This probably means that the value for the 'insert' parameter is invalid.");o.appendChild(e)}i.exports=l},"../../../node_modules/.pnpm/style-loader@3.3.4_webpack@5.94.0_webpack-cli@4.9.1_/node_modules/style-loader/dist/runtime/insertStyleElement.js":i=>{function n(s){var l=document.createElement("style");return s.setAttributes(l,s.attributes),s.insert(l,s.options),l}i.exports=n},"../../../node_modules/.pnpm/style-loader@3.3.4_webpack@5.94.0_webpack-cli@4.9.1_/node_modules/style-loader/dist/runtime/setAttributesWithoutAttributes.js":(i,n,s)=>{function l(a){var e=s.nc;e&&a.setAttribute("nonce",e)}i.exports=l},"../../../node_modules/.pnpm/style-loader@3.3.4_webpack@5.94.0_webpack-cli@4.9.1_/node_modules/style-loader/dist/runtime/styleDomAPI.js":i=>{function n(a,e,o){var t="";o.supports&&(t+="@supports (".concat(o.supports,") {")),o.media&&(t+="@media ".concat(o.media," {"));var u=typeof o.layer!="undefined";u&&(t+="@layer".concat(o.layer.length>0?" ".concat(o.layer):""," {")),t+=o.css,u&&(t+="}"),o.media&&(t+="}"),o.supports&&(t+="}");var p=o.sourceMap;p&&typeof btoa!="undefined"&&(t+=`
+/*# sourceMappingURL=data:application/json;base64,`.concat(btoa(unescape(encodeURIComponent(JSON.stringify(p))))," */")),e.styleTagTransform(t,a,e.options)}function s(a){if(a.parentNode===null)return!1;a.parentNode.removeChild(a)}function l(a){if(typeof document=="undefined")return{update:function(){},remove:function(){}};var e=a.insertStyleElement(a);return{update:function(t){n(e,a,t)},remove:function(){s(e)}}}i.exports=l},"../../../node_modules/.pnpm/style-loader@3.3.4_webpack@5.94.0_webpack-cli@4.9.1_/node_modules/style-loader/dist/runtime/styleTagTransform.js":i=>{function n(s,l){if(l.styleSheet)l.styleSheet.cssText=s;else{for(;l.firstChild;)l.removeChild(l.firstChild);l.appendChild(document.createTextNode(s))}}i.exports=n}}]);})();
