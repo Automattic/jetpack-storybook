669844e0c52c4845bb3b566b860401bd413a9f2a
anomiex <anomiex@users.noreply.github.com>
2024-11-21 16:22:58 +0000

    Update wordpress monorepo (#40288)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/11957107408
    
    Upstream-Ref: Automattic/jetpack@88204ab00dc607b193511fea05c6d116e25c4d9b

---
+++ docs/icon-tooltip-stories-index-stories.9075e3b3.iframe.bundle.js
@@ -0,0 +1,54 @@
+"use strict";(()=>{(self.webpackChunk_automattic_jetpack_storybook=self.webpackChunk_automattic_jetpack_storybook||[]).push([[1945],{"../components/components/icon-tooltip/stories/index.stories.tsx":(u,a,e)=>{var w,y,E,S,A,T,k,I,P,O,C,M,D,R,W;e.r(a),e.d(a,{HasContent:()=>l,HoverShow:()=>c,Wide:()=>p,WrapperAnchor:()=>_,__namedExportsOrder:()=>b,_default:()=>i,default:()=>j});var s=e("../components/components/icon-tooltip/index.tsx"),t=e("../../../node_modules/.pnpm/style-loader@2.0.0_webpack@5.94.0_webpack-cli@4.9.1_/node_modules/style-loader/dist/runtime/injectStylesIntoStyleTag.js"),d=e.n(t),r=e("../../../node_modules/.pnpm/css-loader@6.5.1_webpack@5.94.0_webpack-cli@4.9.1_/node_modules/css-loader/dist/cjs.js!../../../node_modules/.pnpm/postcss-loader@6.2.0_postcss@8.4.47_webpack@5.94.0_webpack-cli@4.9.1_/node_modules/postcss-loader/dist/cjs.js??ruleSet[1].rules[15].use[2]!../../../node_modules/.pnpm/sass-loader@12.4.0_sass@1.64.1_webpack@5.94.0_webpack-cli@4.9.1_/node_modules/sass-loader/dist/cjs.js!../components/components/icon-tooltip/stories/style.scss"),n={};n.insert="head",n.singleton=!1;var h=d()(r.A,n);const v=r.A.locals||{};var o=e("../../../node_modules/.pnpm/react@18.3.1/node_modules/react/jsx-runtime.js");const x=["top","top-start","top-end","bottom","bottom-start","bottom-end"],j={title:"JS Packages/Components/IconTooltip",component:s.A,argTypes:{className:{control:{type:"text"}},iconClassName:{control:{type:"text"}},placement:{control:{type:"select"},options:x},animate:{control:{type:"boolean"}},iconCode:{control:{type:"text"}},iconSize:{control:{type:"number"}},offset:{control:{type:"number"}},title:{control:{type:"text"}},children:{control:{type:null}},wide:{control:{type:"boolean"}},hoverShow:{control:{type:"boolean"}}}},m=f=>(0,o.jsx)("div",{style:{position:"absolute",height:"1000px",left:"300px",top:"200px"},children:(0,o.jsx)(s.A,{...f})}),g=f=>(0,o.jsx)("div",{style:{position:"absolute",height:"1000px",left:"300px",top:"300px"},children:(0,o.jsx)("div",{className:"tooltip-wrapper",children:(0,o.jsx)("div",{className:"tooltip-wrapper-anchor",children:(0,o.jsx)(s.A,{...f,children:(0,o.jsxs)(o.Fragment,{children:[(0,o.jsx)("div",{children:"Thank you for upgrading! Now your visitors can search up to 500 records."}),(0,o.jsx)("div",{className:"tooltip-actions",children:(0,o.jsx)("span",{children:"1 of 2"})})]})})})})}),i=m.bind({}),l=m.bind({});l.args={title:"This is title!",children:(0,o.jsxs)("div",{children:["This is children block!",(0,o.jsx)("br",{}),(0,o.jsx)("br",{}),"Break Line!",(0,o.jsx)("br",{}),(0,o.jsxs)("ul",{children:[(0,o.jsx)("li",{children:"Item 1"}),(0,o.jsx)("li",{children:"Item 2"})]})]})};const _=g.bind({});_.args={popoverAnchorStyle:"wrapper",title:"Site records increased",placement:"top",forceShow:!0};const p=m.bind({});p.args={title:"This is title!",children:(0,o.jsx)("div",{children:"This is a wide tooltip!"}),wide:!0,placement:"bottom-start"};const c=m.bind({});c.args={title:"This is title!",children:(0,o.jsx)("div",{children:"This is a hover tooltip!"}),placement:"bottom-start",hoverShow:!0};const b=["_default","HasContent","WrapperAnchor","Wide","HoverShow"];i.parameters={...i.parameters,docs:{...(w=i.parameters)==null?void 0:w.docs,source:{originalSource:`args => <div style={{
+  position: 'absolute',
+  height: '1000px',
+  left: '300px',
+  top: '200px'
+}}>
+        <IconTooltip {...args} />
+    </div>`,...(E=(y=i.parameters)==null?void 0:y.docs)==null?void 0:E.source}}},l.parameters={...l.parameters,docs:{...(S=l.parameters)==null?void 0:S.docs,source:{originalSource:`args => <div style={{
+  position: 'absolute',
+  height: '1000px',
+  left: '300px',
+  top: '200px'
+}}>
+        <IconTooltip {...args} />
+    </div>`,...(T=(A=l.parameters)==null?void 0:A.docs)==null?void 0:T.source}}},_.parameters={..._.parameters,docs:{...(k=_.parameters)==null?void 0:k.docs,source:{originalSource:`args => <div style={{
+  position: 'absolute',
+  height: '1000px',
+  left: '300px',
+  top: '300px'
+}}>
+        <div className="tooltip-wrapper">
+            <div className="tooltip-wrapper-anchor">
+                <IconTooltip {...args}>
+                    <>
+                        <div>Thank you for upgrading! Now your visitors can search up to 500 records.</div>
+                        <div className="tooltip-actions">
+                            <span>1 of 2</span>
+                        </div>
+                    </>
+                </IconTooltip>
+            </div>
+        </div>
+    </div>`,...(P=(I=_.parameters)==null?void 0:I.docs)==null?void 0:P.source}}},p.parameters={...p.parameters,docs:{...(O=p.parameters)==null?void 0:O.docs,source:{originalSource:`args => <div style={{
+  position: 'absolute',
+  height: '1000px',
+  left: '300px',
+  top: '200px'
+}}>
+        <IconTooltip {...args} />
+    </div>`,...(M=(C=p.parameters)==null?void 0:C.docs)==null?void 0:M.source}}},c.parameters={...c.parameters,docs:{...(D=c.parameters)==null?void 0:D.docs,source:{originalSource:`args => <div style={{
+  position: 'absolute',
+  height: '1000px',
+  left: '300px',
+  top: '200px'
+}}>
+        <IconTooltip {...args} />
+    </div>`,...(W=(R=c.parameters)==null?void 0:R.docs)==null?void 0:W.source}}}},"../../../node_modules/.pnpm/@wordpress+components@28.12.0_@types+react@18.3.12_react-dom@18.3.1_react@18.3.1__react@18.3.1/node_modules/@wordpress/components/build-module/spinner/index.js":(u,a,e)=>{e.d(a,{Ay:()=>p});var s=e("../../../node_modules/.pnpm/clsx@2.1.1/node_modules/clsx/dist/clsx.mjs"),t=e("../../../node_modules/.pnpm/@emotion+styled@11.13.0_@emotion+react@11.13.3_@types+react@18.3.12_react@18.3.1__@types+react@18.3.12_react@18.3.1/node_modules/@emotion/styled/base/dist/emotion-styled-base.browser.esm.js"),d=e("../../../node_modules/.pnpm/@emotion+react@11.13.3_@types+react@18.3.12_react@18.3.1/node_modules/@emotion/react/dist/emotion-react.browser.esm.js"),r=e("../../../node_modules/.pnpm/@wordpress+components@28.12.0_@types+react@18.3.12_react-dom@18.3.1_react@18.3.1__react@18.3.1/node_modules/@wordpress/components/build-module/utils/config-values.js"),n=e("../../../node_modules/.pnpm/@wordpress+components@28.12.0_@types+react@18.3.12_react-dom@18.3.1_react@18.3.1__react@18.3.1/node_modules/@wordpress/components/build-module/utils/colors-values.js");function h(){return"You have tried to stringify object returned from `css` function. It isn't supposed to be used directly (e.g. as value of the `className` prop), but rather handed to emotion so it can handle it (e.g. as value of `css` prop)."}const v=(0,d.i7)`
+	from {
+		transform: rotate(0deg);
+	}
+	to {
+		transform: rotate(360deg);
+	}
+ `,o=(0,t.A)("svg",{target:"ea4tfvq2"})("width:",r.A.spinnerSize,"px;height:",r.A.spinnerSize,"px;display:inline-block;margin:5px 11px 0;position:relative;color:",n.l.theme.accent,";overflow:visible;opacity:1;background-color:transparent;"),x={name:"9s4963",styles:"fill:transparent;stroke-width:1.5px"},j=(0,t.A)("circle",{target:"ea4tfvq1"})(x,";stroke:",n.l.gray[300],";"),m=(0,t.A)("path",{target:"ea4tfvq0"})(x,";stroke:currentColor;stroke-linecap:round;transform-origin:50% 50%;animation:1.4s linear infinite both ",v,";");var g=e("../../../node_modules/.pnpm/react@18.3.1/node_modules/react/index.js"),i=e("../../../node_modules/.pnpm/react@18.3.1/node_modules/react/jsx-runtime.js");function l({className:c,...b},w){return(0,i.jsxs)(o,{className:(0,s.A)("components-spinner",c),viewBox:"0 0 100 100",width:"16",height:"16",xmlns:"http://www.w3.org/2000/svg",role:"presentation",focusable:"false",...b,ref:w,children:[(0,i.jsx)(j,{cx:"50",cy:"50",r:"50",vectorEffect:"non-scaling-stroke"}),(0,i.jsx)(m,{d:"m 50 0 a 50 50 0 0 1 50 50",vectorEffect:"non-scaling-stroke"})]})}const p=(0,g.forwardRef)(l)},"../../../node_modules/.pnpm/@wordpress+icons@10.12.0_react@18.3.1/node_modules/@wordpress/icons/build-module/icon/index.js":(u,a,e)=>{e.d(a,{A:()=>d});var s=e("../../../node_modules/.pnpm/react@18.3.1/node_modules/react/index.js");function t({icon:r,size:n=24,...h},v){return(0,s.cloneElement)(r,{width:n,height:n,...h,ref:v})}const d=(0,s.forwardRef)(t)},"../../../node_modules/.pnpm/@wordpress+icons@10.12.0_react@18.3.1/node_modules/@wordpress/icons/build-module/library/external.js":(u,a,e)=>{e.d(a,{A:()=>r});var s=e("../../../node_modules/.pnpm/@wordpress+primitives@4.12.0_react@18.3.1/node_modules/@wordpress/primitives/build-module/svg/index.js"),t=e("../../../node_modules/.pnpm/react@18.3.1/node_modules/react/jsx-runtime.js");const r=(0,t.jsx)(s.t4,{xmlns:"http://www.w3.org/2000/svg",viewBox:"0 0 24 24",children:(0,t.jsx)(s.wA,{d:"M19.5 4.5h-7V6h4.44l-5.97 5.97 1.06 1.06L18 7.06v4.44h1.5v-7Zm-13 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-3H17v3a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h3V5.5h-3Z"})})},"../../../node_modules/.pnpm/css-loader@6.5.1_webpack@5.94.0_webpack-cli@4.9.1_/node_modules/css-loader/dist/cjs.js!../../../node_modules/.pnpm/postcss-loader@6.2.0_postcss@8.4.47_webpack@5.94.0_webpack-cli@4.9.1_/node_modules/postcss-loader/dist/cjs.js??ruleSet[1].rules[15].use[2]!../../../node_modules/.pnpm/sass-loader@12.4.0_sass@1.64.1_webpack@5.94.0_webpack-cli@4.9.1_/node_modules/sass-loader/dist/cjs.js!../components/components/icon-tooltip/stories/style.scss":(u,a,e)=>{e.d(a,{A:()=>h});var s=e("../../../node_modules/.pnpm/css-loader@6.5.1_webpack@5.94.0_webpack-cli@4.9.1_/node_modules/css-loader/dist/runtime/noSourceMaps.js"),t=e.n(s),d=e("../../../node_modules/.pnpm/css-loader@6.5.1_webpack@5.94.0_webpack-cli@4.9.1_/node_modules/css-loader/dist/runtime/api.js"),r=e.n(d),n=r()(t());n.push([u.id,".tooltip-wrapper-anchor{position:absolute;top:0;left:0;width:64px;height:64px}.tooltip-wrapper-anchor .icon-tooltip-wrapper{width:100%;height:100%}.tooltip-wrapper-anchor .icon-tooltip-wrapper .icon-tooltip-title{font-size:var(--font-title-small);font-weight:500;margin-bottom:16px}.tooltip-wrapper-anchor .icon-tooltip-helper{width:100%;height:100%}.tooltip-wrapper-anchor .icon-tooltip-content{font-size:var(--font-body)}.tooltip-wrapper-anchor .tooltip-actions{display:flex;align-items:center;justify-content:space-between;color:var(--jp-gray-60);margin-top:24px}",""]);const h=n}}]);})();
