e356a36ebd217ea2ff577506d97c94b3b2a55b01
kraftbj <kraftbj@users.noreply.github.com>
2025-07-07 20:57:21 +0000

    Boost: Mock Unrelated E2E Boost Prerequisites (#44208)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/16127395006
    
    Upstream-Ref: Automattic/jetpack@f55b48dbef41036d9275e1a654f7e402cb1e8c1e

---
+++ docs/text-stories-index-stories.feea46e9.iframe.bundle.js
@@ -0,0 +1,153 @@
+"use strict";(self.webpackChunk_automattic_jetpack_storybook=self.webpackChunk_automattic_jetpack_storybook||[]).push([[2593],{"../components/components/text/stories/index.stories.tsx":(y,m,a)=>{a.r(m),a.d(m,{BoxModel:()=>r,CustomComponent:()=>s,CustomTag:()=>o,Default:()=>l,__namedExportsOrder:()=>_,default:()=>v});var n=a("../components/components/text/index.tsx"),c=a("../components/components/text/constants.ts"),u=a("../components/components/text/stories/style.module.scss"),t=a("../../../node_modules/.pnpm/react@18.3.1/node_modules/react/jsx-runtime.js"),g=`import Text, { BOX_MODEL_VALUES, H2, H3, Title, VARIANTS_MAPPING } from '../index.tsx';
+import styles from './style.module.scss';
+import type { SpacingValue } from '../types.ts';
+
+const SPACING_VALUES: Array< SpacingValue > = [ 0, 1, 2, 3, 4, 5, 6, 7, 8 ];
+
+export default {
+	title: 'JS Packages/Components/Text',
+	component: Text,
+	argTypes: {
+		...BOX_MODEL_VALUES.reduce(
+			( obj, cur ) => ( {
+				...obj,
+				[ cur ]: {
+					control: { type: 'select' },
+					options: SPACING_VALUES,
+				},
+			} ),
+			{}
+		),
+		variant: {
+			control: {
+				type: 'select',
+			},
+			options: Object.keys( VARIANTS_MAPPING ),
+		},
+	},
+	subcomponents: { H2, H3, Title },
+};
+
+const Template = args => <Text { ...args }>{ args.variant ?? 'body' }</Text>;
+
+export const Default = Template.bind( {} );
+
+Default.args = {
+	variant: 'headline-medium',
+};
+
+export const CustomTag = Template.bind( {} );
+
+CustomTag.args = {
+	variant: 'title-small',
+	component: 'div',
+};
+
+const Custom = ( { className, children } ) => (
+	<span className={ className }>{ children } Composition</span>
+);
+
+export const CustomComponent = Template.bind( {} );
+
+CustomComponent.args = {
+	variant: 'headline-small',
+	component: Custom,
+};
+
+const BoxModelTemplate = args => (
+	<div className={ styles[ 'box-model-wrapper' ] }>
+		<div className={ styles[ 'box-model-side-left' ] } />
+		<div className={ styles[ 'box-model-side-center' ] }>
+			<Text { ...args }>
+				<div className={ styles[ 'box-model-inner' ] }>Box Model</div>
+			</Text>
+		</div>
+		<div className={ styles[ 'box-model-side-right' ] } />
+	</div>
+);
+
+export const BoxModel = BoxModelTemplate.bind( {} );
+`,T={Default:{startLoc:{col:17,line:34},endLoc:{col:2,line:37},startBody:{col:17,line:34},endBody:{col:2,line:37}},CustomTag:{startLoc:{col:17,line:34},endLoc:{col:2,line:37},startBody:{col:17,line:34},endBody:{col:2,line:37}},CustomComponent:{startLoc:{col:17,line:34},endLoc:{col:2,line:37},startBody:{col:17,line:34},endBody:{col:2,line:37}},BoxModel:{startLoc:{col:25,line:59},endLoc:{col:2,line:75},startBody:{col:25,line:59},endBody:{col:2,line:75}}};const p=[0,1,2,3,4,5,6,7,8],v={parameters:{storySource:{source:`/* wp:polyfill */
+/* wp:polyfill esnext.iterator.constructor */
+/* wp:polyfill esnext.iterator.reduce */
+import Text, { BOX_MODEL_VALUES, H2, H3, Title, VARIANTS_MAPPING } from '../index.tsx';
+import styles from './style.module.scss';
+import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
+const SPACING_VALUES = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+export default {
+  title: 'JS Packages/Components/Text',
+  component: Text,
+  argTypes: {
+    ...BOX_MODEL_VALUES.reduce((obj, cur) => ({
+      ...obj,
+      [cur]: {
+        control: {
+          type: 'select'
+        },
+        options: SPACING_VALUES
+      }
+    }), {}),
+    variant: {
+      control: {
+        type: 'select'
+      },
+      options: Object.keys(VARIANTS_MAPPING)
+    }
+  },
+  subcomponents: {
+    H2,
+    H3,
+    Title
+  }
+};
+const Template = args => /*#__PURE__*/_jsx(Text, {
+  ...args,
+  children: args.variant ?? 'body'
+});
+export const Default = Template.bind({});
+Default.args = {
+  variant: 'headline-medium'
+};
+export const CustomTag = Template.bind({});
+CustomTag.args = {
+  variant: 'title-small',
+  component: 'div'
+};
+const Custom = ({
+  className,
+  children
+}) => /*#__PURE__*/_jsxs("span", {
+  className: className,
+  children: [children, " Composition"]
+});
+export const CustomComponent = Template.bind({});
+CustomComponent.args = {
+  variant: 'headline-small',
+  component: Custom
+};
+const BoxModelTemplate = args => /*#__PURE__*/_jsxs("div", {
+  className: styles['box-model-wrapper'],
+  children: [/*#__PURE__*/_jsx("div", {
+    className: styles['box-model-side-left']
+  }), /*#__PURE__*/_jsx("div", {
+    className: styles['box-model-side-center'],
+    children: /*#__PURE__*/_jsx(Text, {
+      ...args,
+      children: /*#__PURE__*/_jsx("div", {
+        className: styles['box-model-inner'],
+        children: "Box Model"
+      })
+    })
+  }), /*#__PURE__*/_jsx("div", {
+    className: styles['box-model-side-right']
+  })]
+});
+export const BoxModel = BoxModelTemplate.bind({});`,locationsMap:{default:{startLoc:{col:17,line:34},endLoc:{col:2,line:37},startBody:{col:17,line:34},endBody:{col:2,line:37}},"custom-tag":{startLoc:{col:17,line:34},endLoc:{col:2,line:37},startBody:{col:17,line:34},endBody:{col:2,line:37}},"custom-component":{startLoc:{col:17,line:34},endLoc:{col:2,line:37},startBody:{col:17,line:34},endBody:{col:2,line:37}},"box-model":{startLoc:{col:25,line:59},endLoc:{col:2,line:75},startBody:{col:25,line:59},endBody:{col:2,line:75}}}}},title:"JS Packages/Components/Text",component:n.Ay,argTypes:{...c.Z.reduce((e,d)=>({...e,[d]:{control:{type:"select"},options:p}}),{}),variant:{control:{type:"select"},options:Object.keys(c.Q)}},subcomponents:{H2:n.H2,H3:n.H3,Title:n.hE}},i=e=>(0,t.jsx)(n.Ay,{...e,children:e.variant??"body"}),l=i.bind({});l.args={variant:"headline-medium"};const o=i.bind({});o.args={variant:"title-small",component:"div"};const x=({className:e,children:d})=>(0,t.jsxs)("span",{className:e,children:[d," Composition"]}),s=i.bind({});s.args={variant:"headline-small",component:x};const r=(e=>(0,t.jsxs)("div",{className:u.A["box-model-wrapper"],children:[(0,t.jsx)("div",{className:u.A["box-model-side-left"]}),(0,t.jsx)("div",{className:u.A["box-model-side-center"],children:(0,t.jsx)(n.Ay,{...e,children:(0,t.jsx)("div",{className:u.A["box-model-inner"],children:"Box Model"})})}),(0,t.jsx)("div",{className:u.A["box-model-side-right"]})]})).bind({}),_=["Default","CustomTag","CustomComponent","BoxModel"];l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:"args => <Text {...args}>{args.variant ?? 'body'}</Text>",...l.parameters?.docs?.source}}},o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:"args => <Text {...args}>{args.variant ?? 'body'}</Text>",...o.parameters?.docs?.source}}},s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:"args => <Text {...args}>{args.variant ?? 'body'}</Text>",...s.parameters?.docs?.source}}},r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`args => <div className={styles['box-model-wrapper']}>
+        <div className={styles['box-model-side-left']} />
+        <div className={styles['box-model-side-center']}>
+            <Text {...args}>
+                <div className={styles['box-model-inner']}>Box Model</div>
+            </Text>
+        </div>
+        <div className={styles['box-model-side-right']} />
+    </div>`,...r.parameters?.docs?.source}}};try{Text.displayName="Text",Text.__docgenInfo={description:"Text component.",displayName:"Text",props:{variant:{defaultValue:{value:"body"},description:"",name:"variant",required:!1,type:{name:"enum",value:[{value:'"label"'},{value:'"headline-medium"'},{value:'"headline-small"'},{value:'"headline-small-regular"'},{value:'"title-medium"'},{value:'"title-medium-semi-bold"'},{value:'"title-small"'},{value:'"body"'},{value:'"body-small"'},{value:'"body-extra-small"'},{value:'"body-extra-small-bold"'}]}},m:{defaultValue:null,description:"margin",name:"m",required:!1,type:{name:"enum",value:[{value:"0"},{value:"1"},{value:"5"},{value:"2"},{value:"3"},{value:"4"},{value:"6"},{value:"7"},{value:"8"}]}},mt:{defaultValue:null,description:"margin-top",name:"mt",required:!1,type:{name:"enum",value:[{value:"0"},{value:"1"},{value:"5"},{value:"2"},{value:"3"},{value:"4"},{value:"6"},{value:"7"},{value:"8"}]}},mr:{defaultValue:null,description:"margin-right",name:"mr",required:!1,type:{name:"enum",value:[{value:"0"},{value:"1"},{value:"5"},{value:"2"},{value:"3"},{value:"4"},{value:"6"},{value:"7"},{value:"8"}]}},mb:{defaultValue:null,description:"margin-bottom",name:"mb",required:!1,type:{name:"enum",value:[{value:"0"},{value:"1"},{value:"5"},{value:"2"},{value:"3"},{value:"4"},{value:"6"},{value:"7"},{value:"8"}]}},ml:{defaultValue:null,description:"margin-left",name:"ml",required:!1,type:{name:"enum",value:[{value:"0"},{value:"1"},{value:"5"},{value:"2"},{value:"3"},{value:"4"},{value:"6"},{value:"7"},{value:"8"}]}},mx:{defaultValue:null,description:"margin left and right",name:"mx",required:!1,type:{name:"enum",value:[{value:"0"},{value:"1"},{value:"5"},{value:"2"},{value:"3"},{value:"4"},{value:"6"},{value:"7"},{value:"8"}]}},my:{defaultValue:null,description:"margin top and bottom",name:"my",required:!1,type:{name:"enum",value:[{value:"0"},{value:"1"},{value:"5"},{value:"2"},{value:"3"},{value:"4"},{value:"6"},{value:"7"},{value:"8"}]}},p:{defaultValue:null,description:"padding",name:"p",required:!1,type:{name:"enum",value:[{value:"0"},{value:"1"},{value:"5"},{value:"2"},{value:"3"},{value:"4"},{value:"6"},{value:"7"},{value:"8"}]}},pt:{defaultValue:null,description:"padding-top",name:"pt",required:!1,type:{name:"enum",value:[{value:"0"},{value:"1"},{value:"5"},{value:"2"},{value:"3"},{value:"4"},{value:"6"},{value:"7"},{value:"8"}]}},pr:{defaultValue:null,description:"padding-right",name:"pr",required:!1,type:{name:"enum",value:[{value:"0"},{value:"1"},{value:"5"},{value:"2"},{value:"3"},{value:"4"},{value:"6"},{value:"7"},{value:"8"}]}},pb:{defaultValue:null,description:"padding-bottom",name:"pb",required:!1,type:{name:"enum",value:[{value:"0"},{value:"1"},{value:"5"},{value:"2"},{value:"3"},{value:"4"},{value:"6"},{value:"7"},{value:"8"}]}},pl:{defaultValue:null,description:"padding-left",name:"pl",required:!1,type:{name:"enum",value:[{value:"0"},{value:"1"},{value:"5"},{value:"2"},{value:"3"},{value:"4"},{value:"6"},{value:"7"},{value:"8"}]}},px:{defaultValue:null,description:"padding left and right",name:"px",required:!1,type:{name:"enum",value:[{value:"0"},{value:"1"},{value:"5"},{value:"2"},{value:"3"},{value:"4"},{value:"6"},{value:"7"},{value:"8"}]}},py:{defaultValue:null,description:"padding top and bottom",name:"py",required:!1,type:{name:"enum",value:[{value:"0"},{value:"1"},{value:"5"},{value:"2"},{value:"3"},{value:"4"},{value:"6"},{value:"7"},{value:"8"}]}},className:{defaultValue:null,description:"HTML Class",name:"className",required:!1,type:{name:"string"}},children:{defaultValue:null,description:"The text itself that will be rendered.",name:"children",required:!0,type:{name:"ReactNode"}},component:{defaultValue:null,description:"Force an specific tag (span, div) or use a custom component that will receive className and children",name:"component",required:!1,type:{name:"FC<{ [prop: string]: unknown; }> | ElementType"}},style:{defaultValue:null,description:"",name:"style",required:!1,type:{name:"CSSProperties"}}}},typeof STORYBOOK_REACT_CLASSES<"u"&&(STORYBOOK_REACT_CLASSES["../components/components/text/stories/index.stories.tsx#Text"]={docgenInfo:Text.__docgenInfo,name:"Text",path:"../components/components/text/stories/index.stories.tsx#Text"})}catch{}}}]);
