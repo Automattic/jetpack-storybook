21b2c20d5a587b06caac1aeeb891d23307075446
tbradsha <tbradsha@users.noreply.github.com>
2025-10-20 13:28:10 +0000

    Prevent PHP fatals when directly accessing files, round 9 (#45537)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/18653099953
    
    Upstream-Ref: Automattic/jetpack@f37710674f98488e2ed6e1f52bf048052a366265

--- docs/components-message-stories-index-stories.63bcfacf.iframe.bundle.js
+++ docs/components-message-stories-index-stories.63bcfacf.iframe.bundle.js
@@ -6,7 +6,7 @@
   return <UpgradeMessage requestsRemaining={args.requestsRemaining} severity={args.severity} onUpgradeClick={action('onUpgradeClick')} />;
 }`,...E.parameters?.docs?.source}}},l.parameters={...l.parameters,docs:{...l.parameters?.docs,source:{originalSource:`args => {
   return <ErrorMessage error={args.error} code={args.code} onTryAgainClick={action('onTryAgainClick')} onUpgradeClick={action('onUpgradeClick')} />;
-}`,...l.parameters?.docs?.source}}};try{stories.displayName="stories",stories.__docgenInfo={description:"React component to render a block message.",displayName:"stories",props:{icon:{defaultValue:{value:"null"},description:"",name:"icon",required:!1,type:{name:"ReactNode"}},severity:{defaultValue:{value:"info"},description:"",name:"severity",required:!1,type:{name:"enum",value:[{value:'"success"'},{value:'"info"'},{value:'"warning"'},{value:'"error"'}]}},aiFeedbackThumbsOptions:{defaultValue:{value:`{
+}`,...l.parameters?.docs?.source}}};try{stories.displayName="stories",stories.__docgenInfo={description:"React component to render a block message.",displayName:"stories",props:{icon:{defaultValue:{value:"null"},description:"",name:"icon",required:!1,type:{name:"ReactNode"}},severity:{defaultValue:{value:"info"},description:"",name:"severity",required:!1,type:{name:"enum",value:[{value:'"warning"'},{value:'"error"'},{value:'"success"'},{value:'"info"'}]}},aiFeedbackThumbsOptions:{defaultValue:{value:`{
 		showAIFeedbackThumbs: false,
 		ratedItem: '',
 		prompt: '',
