f099fd2080bd568b205c12b38d6f1d69d562a9e9
obenland <obenland@users.noreply.github.com>
2026-04-14 19:58:51 +0000

    VideoPress: Fix poster image not saving when selected during upload (#47725)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/24419385854
    
    Upstream-Ref: Automattic/jetpack@761d512be40c3bc4fb037ae398eb8fce3a799327

---
+++ docs/assets/index.stories-CONKPgcb.js
@@ -0,0 +1,7 @@
+import{n as e}from"./chunk-zsgVPwQN.js";import{zh as t}from"./iframe-xA2xf8-k.js";import{n,t as r}from"./checkbox-5BHi_8Zw.js";var i,a,o,s,c,l;e((()=>{n(),i=t(),{useArgs:a}=__STORYBOOK_MODULE_PREVIEW_API__,o={title:`Packages/VideoPress/Checkbox`,component:r,parameters:{layout:`centered`}},s=e=>{let[,t]=a(),n=e=>t({checked:e});return(0,i.jsx)(r,{...e,onChange:n})},c=s.bind({}),c.args={checked:!0},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`args => {
+  const [, updateArgs] = useArgs();
+  const onChange = current => updateArgs({
+    checked: current
+  });
+  return <Checkbox {...args} onChange={onChange} />;
+}`,...c.parameters?.docs?.source}}},l=[`_default`]}))();export{l as __namedExportsOrder,c as _default,o as default};
--- docs/assets/index.stories-wnH-kGXE.js
+++
@@ -1,7 +0,0 @@
-import{n as e}from"./chunk-zsgVPwQN.js";import{zh as t}from"./iframe-D3aolNI7.js";import{n,t as r}from"./checkbox-CFG0bI2g.js";var i,a,o,s,c,l;e((()=>{n(),i=t(),{useArgs:a}=__STORYBOOK_MODULE_PREVIEW_API__,o={title:`Packages/VideoPress/Checkbox`,component:r,parameters:{layout:`centered`}},s=e=>{let[,t]=a(),n=e=>t({checked:e});return(0,i.jsx)(r,{...e,onChange:n})},c=s.bind({}),c.args={checked:!0},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`args => {
-  const [, updateArgs] = useArgs();
-  const onChange = current => updateArgs({
-    checked: current
-  });
-  return <Checkbox {...args} onChange={onChange} />;
-}`,...c.parameters?.docs?.source}}},l=[`_default`]}))();export{l as __namedExportsOrder,c as _default,o as default};
