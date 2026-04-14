47ac88896662738273de39e0d57889e5c0898729
obenland <obenland@users.noreply.github.com>
2026-04-14 19:01:16 +0000

    VideoPress: Clean up postMessage bridge code (#47436)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/24416776806
    
    Upstream-Ref: Automattic/jetpack@385788d3220442237d08102557769dbd463da9c7

--- docs/assets/index.stories-ZyNkCJrt.js
+++
@@ -1,5 +0,0 @@
-import{n as e}from"./chunk-zsgVPwQN.js";import{$r as t,Bh as n,Vh as r,pi as i,pr as a,zh as o}from"./iframe-CCINO4Kw.js";import{a as s,n as c,o as l,r as u,t as d}from"./development-DArceeHP.js";var f,p,m,h=e((()=>{f=`_icon_1jdhu_1`,p=`_link_1jdhu_7`,m={icon:f,link:p}})),g,_,v=e((()=>{a(),r(),d(),h(),g=o(),_=({className:e,accessibleName:r})=>(0,g.jsx)(u,{to:`/`,className:n(m.link,e),"aria-label":r||null,children:(0,g.jsx)(i,{icon:t,className:m.icon})}),_.__docgenInfo={description:``,methods:[],displayName:`CloseLink`}})),y,b,x,S,C,w;e((()=>{d(),v(),y=o(),b={title:`Packages/My Jetpack/CloseLink`,component:_},x={},S=e=>(0,y.jsx)(c,{children:(0,y.jsx)(l,{children:(0,y.jsx)(s,{path:`/`,element:(0,y.jsx)(_,{...e})})})}),C=S.bind({}),C.parameters={},C.args=x,C.parameters={...C.parameters,docs:{...C.parameters?.docs,source:{originalSource:`args => <HashRouter>
-        <Routes>
-            <Route path="/" element={<CloseLink {...args} />} />
-        </Routes>
-    </HashRouter>`,...C.parameters?.docs?.source}}},w=[`_default`]}))();export{w as __namedExportsOrder,C as _default,b as default};
--- docs/assets/stories-MDTD3Kuh.js
+++
@@ -1,3 +0,0 @@
-import{n as e}from"./chunk-zsgVPwQN.js";import{zh as t}from"./iframe-CCINO4Kw.js";import{r as n}from"./react-DJwyV_HB.js";import{t as r}from"./mdx-react-shim-Dwgl8Mnl.js";import{a as i,i as a,n as o,t as s}from"./blocks-3pgMi5Mb.js";import{n as c,r as l,t as u}from"./index2.stories-CkMSPjKh.js";function d(e){let t={h1:`h1`,...n(),...e.components};return(0,p.jsxs)(p.Fragment,{children:[(0,p.jsx)(o,{of:u}),`
-`,(0,p.jsx)(t.h1,{id:`posterpanel`,children:`PosterPanel`}),`
-`,(0,p.jsx)(s,{withSource:`open`,children:(0,p.jsx)(a,{id:`packages-videopress-block-editor-poster-panel--default`})})]})}function f(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,p.jsx)(t,{...e,children:(0,p.jsx)(d,{...e})}):d(e)}var p;e((()=>{p=t(),r(),i(),l(),c()}))();export{f as default};
