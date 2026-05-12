0c967ef3dbd5d9547d17af5a00e139342ad27d7f
robfelty <robfelty@users.noreply.github.com>
2026-05-12 14:29:46 +0000

    Jetpack Search AI Answers: add dashboard and front end ui. (#48592)
    * Jetpack Search AI Answers - add dashboard and front end ui.
    
    This is a squashed commit from developing the feature and multiple experiments with the interface.
    
    It also includes the original plan for building the feature.
    
    This was merged with the new experience selection logic from https://github.com/Automattic/jetpack/pull/48500
    
    * Add search tests to improve test coverage.
    
    * Further improve test coverage and clean up the changelog file.
    
    * Clean up old planning and design docs from the branch. Update the roadmap.
    
    * Address PR feedback: clean up css, some minor oversights, and punt on a few things.
    
    * Fix mistake with the changelogs.
    
    * Updated REST API calls to use new combined workflow
    
    * Addressed possible security concern
    
    * No longer requiring Gutenberg
    Rebased from trunk
    
    * Fixed tests
    
    * fixed tests
    
    * static analysis fix
    
    * Getting rid of unnecessary token
    
    * Fixed issue with saving personality
    
    * fixed personality settings
    
    * another attempt at fixing saving personality
    
    ---------
    
    Co-authored-by: Robert Felty <robfelty@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/25739823696
    
    Upstream-Ref: Automattic/jetpack@396bb85cadd0b991ae3783b079093e5b4600a58d

---
+++ docs/assets/index2.stories-Bxp91Wt6.js
@@ -0,0 +1,9 @@
+import{n as e}from"./chunk-zsgVPwQN.js";import{M as t,Ot as n,_l as r,j as i,sx as a,vl as o,wt as s}from"./iframe-CDMyhBVo.js";var c,l,u,d,f;e((()=>{o(),n(),t(),c=a(),l={title:`JS Packages/Components/Popover`,component:i,argTypes:{icon:{control:`object`},action:{control:`object`}},decorators:[e=>(0,c.jsx)(`div`,{style:{width:`600px`,maxWidth:`90%`,height:`600px`,fontSize:`16px`},children:(0,c.jsx)(e,{})})]},u=e=>(0,c.jsx)(i,{...e,children:(0,c.jsxs)(`p`,{children:[`This is line is just longer and in another paragraph.`,(0,c.jsx)(`br`,{}),`This is the message.`]})}),d=u.bind({}),d.args={icon:(0,c.jsx)(s,{}),action:(0,c.jsx)(r,{children:`Click me`})},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`args => {
+  return <Popover {...args}>
+            <p>
+                This is line is just longer and in another paragraph.
+                <br />
+                This is the message.
+            </p>
+        </Popover>;
+}`,...d.parameters?.docs?.source}}},f=[`_default`]}))();export{f as __namedExportsOrder,d as _default,l as default};
