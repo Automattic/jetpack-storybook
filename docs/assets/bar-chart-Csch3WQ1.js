e4679396128cf6bfcb100065ec257cba8c4e2376
anomiex <anomiex@users.noreply.github.com>
2026-06-09 16:24:18 +0000

    Lock file maintenance (#49448)
    * Lock file maintenance
    * Remove trustPolicyExclude for reselect@5.1.1 after upgrade to 5.2.0.
    * stylelint: Remove unnecessary escapes in regex
    * Downgrade `@tanstack/react-router` to 1.167.1
      Avoid https://href.li/?https://github.com/TanStack/router/issues/7538
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/27219400812
    
    Upstream-Ref: Automattic/jetpack@fa034e91a7375f7ab0b46f171abe539e3cb47c30

--- docs/assets/bar-chart-Csch3WQ1.js
+++
@@ -1,1 +0,0 @@
-import{n as e}from"./chunk-zsgVPwQN.js";import{r as t}from"./bar-chart-CHt2nQ72.js";var n=e((()=>{t()}));export{n as t};
--- docs/assets/index2.stories-Cx-qexcz.js
+++
@@ -1,9 +0,0 @@
-import{n as e}from"./chunk-zsgVPwQN.js";import{Er as t,Et as n,M as r,St as i,Tr as a,fv as o,j as s}from"./iframe-33AOSa0J.js";var c,l,u,d,f;e((()=>{t(),n(),r(),c=o(),l={title:`JS Packages/Components/Popover`,component:s,argTypes:{icon:{control:`object`},action:{control:`object`}},decorators:[e=>(0,c.jsx)(`div`,{style:{width:`600px`,maxWidth:`90%`,height:`600px`,fontSize:`16px`},children:(0,c.jsx)(e,{})})]},u=e=>(0,c.jsx)(s,{...e,children:(0,c.jsxs)(`p`,{children:[`This is line is just longer and in another paragraph.`,(0,c.jsx)(`br`,{}),`This is the message.`]})}),d=u.bind({}),d.args={icon:(0,c.jsx)(i,{}),action:(0,c.jsx)(a,{children:`Click me`})},d.parameters={...d.parameters,docs:{...d.parameters?.docs,source:{originalSource:`args => {
-  return <Popover {...args}>
-            <p>
-                This is line is just longer and in another paragraph.
-                <br />
-                This is the message.
-            </p>
-        </Popover>;
-}`,...d.parameters?.docs?.source}}},f=[`_default`]}))();export{f as __namedExportsOrder,d as _default,l as default};
