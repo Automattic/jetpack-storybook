dfb43648dad2ac87c8a0e74bb90f14a38607a0bc
anomiex <anomiex@users.noreply.github.com>
2026-06-16 14:40:39 +0000

    Update dependency markdown-it to v14.2.0 [SECURITY] (#49639)
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/27624405796
    
    Upstream-Ref: Automattic/jetpack@a18138263b268b31b313d198f8cc53da37a7caa0

--- docs/assets/chart-tooltip-BXycxA-C.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{n as t}from"./chart-tooltip-CLhr8f36.js";import{n}from"./pie-chart-tooltip-CgIKwSPB.js";var r=e((()=>{t(),n()}));export{r as t};
