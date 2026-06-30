54eafb0ffd69e46078a6c43ad05fe6aeb28658d0
retrofox <retrofox@users.noreply.github.com>
2026-06-30 09:39:29 +0000

    Premium Analytics: add React Query Devtools dev-only widget (#50002)
    * add filterable widget availability layer
    
    gate widget types per request before REST list and import map
    
    * add React Query Devtools dev-only widget
    
    replace inline provider devtools with a non-production widget
    
    * fix Phan errors in widget availability test
    
    cover functions with ::func / CoversFunction, not CoversClass
    
    * tighten widget + availability docblocks
    
    shorten prose; document the wp_get_environment_type production default
    
    * fix react-query-devtools widget i18n and deps
    
    wrap title in __(), link: data dep, declare react
    
    * drop redundant presentation in locations widget
    
    widget.json is the canonical source read by the build
    
    * add registry-time widget filter for dev gating
    
    Expose registry + runtime widget-type filters in core; gate the dev widget at registration, not on read.
    
    * fix filter reference in dev widget docblock
    
    name the registry-time filter, not the runtime one
    
    * explain name-based dev widget gating
    
    wp-build does not expose widget category to PHP yet
    
    * fix widget registry hydration order
    
    hydrate via bootstrap_widget_types() once the filter is hooked
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/28434044416
    
    Upstream-Ref: Automattic/jetpack@b5c1968f391cdd190e5054956a1e50c98e2877f2

--- docs/assets/index.stories-BRkyzIgM.js
+++
@@ -1,9 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{t}from"./jsx-runtime-D2pHJD-r.js";import{Ki as n,t as r}from"./build-module-BJuBuYBF.js";import{t as i}from"./button-57Hv0j_n.js";import{t as a}from"./iframe-BdQTm_rE.js";var o,s=e((()=>{o={"button-group":`_button-group_1hcx0_1`}}));function c({children:e,...t}){return(0,l.jsx)(n,{gap:0,className:`components-button-group ${o[`button-group`]}`,...t,children:e})}var l,u=e((()=>{a(),r(),s(),l=t(),c.Button=({onClick:e,variant:t=`secondary`,children:n,...r})=>(0,l.jsx)(i,{onClick:e,variant:t,className:`components-button`,...r,children:(0,l.jsx)(`span`,{children:n})}),c.__docgenInfo={description:`Button Group
-
-@param {object}                      props          - Component props.
-@param { import('react').ReactNode } props.children - Component children.
-
-@return { import('react').ReactNode } The Button Group component.`,methods:[{name:`Button`,docblock:null,modifiers:[`static`],params:[{name:`{ onClick, variant = 'secondary', children, ...props }`,optional:!1,type:null}],returns:null}],displayName:`ButtonGroup`}})),d,f,p,m,h;e((()=>{a(),u(),d=t(),f={title:`Plugins/Protect/Button Group`,component:c,argTypes:{}},p=e=>(0,d.jsxs)(c,{...e,children:[(0,d.jsx)(i,{children:`Button 1`}),(0,d.jsx)(i,{children:`Button 2`})]}),m=p.bind({}),m.parameters={...m.parameters,docs:{...m.parameters?.docs,source:{originalSource:`args => <ButtonGroup {...args}>
-        <Button>Button 1</Button>
-        <Button>Button 2</Button>
-    </ButtonGroup>`,...m.parameters?.docs?.source}}},h=[`Default`]}))();export{m as Default,h as __namedExportsOrder,f as default};
