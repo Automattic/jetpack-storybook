017899aad790590be9f9503cf27ac7940497b242
kangzj <kangzj@users.noreply.github.com>
2026-07-01 23:43:05 +0000

    Premium Analytics: Align Emails widget header (#50141)
    * WOOA7S-1644: Align Emails widget header
    
    * Address review: use framed Emails widget presentation
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/28554430949
    
    Upstream-Ref: Automattic/jetpack@0c232d7acbc32565e57d4c59d0839650a0bdc6af

---
+++ docs/assets/with-chart-theme-DrrHFyst.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{t}from"./jsx-runtime-D2pHJD-r.js";import{J as n,t as r}from"./hooks-BPnV4fx1.js";import{A as i}from"./providers-D4rH48NF.js";import{t as a}from"./src--3Q6WzQo.js";var o,s,c,l=e((()=>{a(),r(),o=t(),s=({children:e})=>(0,o.jsx)(i,{theme:n(),children:e}),c=e=>(0,o.jsx)(s,{children:(0,o.jsx)(e,{})})}));export{c as n,l as t};
