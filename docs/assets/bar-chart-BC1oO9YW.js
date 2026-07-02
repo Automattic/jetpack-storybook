fedb92e7f8b6a1bcff7433ae778ec56c4a99a1f3
chihsuan <chihsuan@users.noreply.github.com>
2026-07-02 06:35:18 +0000

    Premium Analytics: port the Sales by coupon usage widget (#49708)
    * Premium Analytics: Add sales by coupon usage widget
    
    * Premium Analytics: Fix sales by coupon usage stories
    
    * fix: conform sales-by-coupon-usage port to widget render contract
    
    ---------
    
    Co-authored-by: Chi-Hsuan Huang <chihsuan.tw@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/28569768240
    
    Upstream-Ref: Automattic/jetpack@938d24fdf25b8a526f54b65228e1fc5a4e71eb09

---
+++ docs/assets/bar-chart-BC1oO9YW.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{r as t}from"./bar-chart-xiL_1NFt.js";var n=e((()=>{t()}));export{n as t};
--- docs/assets/mdx-react-shim-B7ncfl85.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{pt as t}from"./esm-hh_6m0F5.js";var n=e((()=>{t()}));export{n as t};
