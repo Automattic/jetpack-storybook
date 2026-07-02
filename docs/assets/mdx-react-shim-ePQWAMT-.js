afd2d6db2d40bd3a0c88592d9bb93a9c27f0eac3
chihsuan <chihsuan@users.noreply.github.com>
2026-07-02 02:21:25 +0000

    Widgets: conform Woo-family render props to WidgetRenderProps<T> contract (#50113)
    * refactor: conform premium-analytics Woo widgets to WidgetRenderProps contract
    
    * refactor: trim render doc comments to minimal house style
    
    * docs: clarify no-config-attributes docblocks and reword changelog
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/28560312800
    
    Upstream-Ref: Automattic/jetpack@7518444756992752af6cd94653095ad79ee8441c

---
+++ docs/assets/mdx-react-shim-ePQWAMT-.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{pt as t}from"./esm-D8474HNU.js";var n=e((()=>{t()}));export{n as t};
---
+++ docs/assets/with-chart-theme-BlqxJhr5.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{t}from"./jsx-runtime-D2pHJD-r.js";import{J as n,t as r}from"./hooks-CYICuBN5.js";import{A as i}from"./providers-BQ8Ha7b7.js";import{t as a}from"./src-CIZJ5Ju6.js";var o,s,c,l=e((()=>{a(),r(),o=t(),s=({children:e})=>(0,o.jsx)(i,{theme:n(),children:e}),c=e=>(0,o.jsx)(s,{children:(0,o.jsx)(e,{})})}));export{c as n,l as t};
