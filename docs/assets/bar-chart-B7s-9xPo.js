ee91ce03c564fe81f13c7b9704025147535c9144
chihsuan <chihsuan@users.noreply.github.com>
2026-07-02 10:11:51 +0000

    Premium Analytics: port the Revenue by customer type widget (#49717)
    * Premium Analytics: port the Revenue by customer type widget
    
    * fix: conform revenue-by-customer-type port to widget contract
    
    ---------
    
    Co-authored-by: Chi-Hsuan Huang <chihsuan.tw@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/28581254007
    
    Upstream-Ref: Automattic/jetpack@52a07e2e71e8467b239ac11695a300155927b75d

--- docs/assets/bar-chart-B7s-9xPo.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{r as t}from"./bar-chart-C4MF1YJA.js";var n=e((()=>{t()}));export{n as t};
--- docs/assets/metric-with-comparison-u4x7liKF.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{n as t}from"./metric-with-comparison-Cf5EcYx6.js";var n=e((()=>{t()}));export{n as t};
