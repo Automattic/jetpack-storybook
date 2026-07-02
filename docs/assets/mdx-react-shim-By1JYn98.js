2c4950f0faf61575c441a28a3adee636ca4e8479
chihsuan <chihsuan@users.noreply.github.com>
2026-07-02 08:14:31 +0000

    Premium Analytics: port the Net sales over time widget (#49711)
    * Premium Analytics: port the Net sales over time widget
    
    * fix: conform net-sales-over-time port to widget contract
    
    ---------
    
    Co-authored-by: Chi-Hsuan Huang <chihsuan.tw@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/28574584678
    
    Upstream-Ref: Automattic/jetpack@ea011f30e397f2d750faea06a6371f7cd49676b1

---
+++ docs/assets/mdx-react-shim-By1JYn98.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{pt as t}from"./esm-DPMLKPpO.js";var n=e((()=>{t()}));export{n as t};
---
+++ docs/assets/metric-delta-CDPWvcJk.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{n as t}from"./metric-delta-Dz2G1lGW.js";var n=e((()=>{t()}));export{n as t};
