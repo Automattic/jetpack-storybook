a13c7a5e9d81d51b87162b51bb673121f1eb286a
chihsuan <chihsuan@users.noreply.github.com>
2026-07-02 04:07:10 +0000

    Widgets: normalize Premium Analytics widget file and style conventions (#50112)
    * refactor: normalize premium-analytics widget CSS modules to style.module.css
    
    * refactor: drop React.ReactNode return type from react-query-dev-tool widget
    
    * docs: require Record<never,never> for zero-attribute widgets in the widget contract
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/28563987720
    
    Upstream-Ref: Automattic/jetpack@438e23a71cb16a10c6a6113cbfd56af4e8004cec

---
+++ docs/assets/stories-BOlDAYa6.js
@@ -0,0 +1,3 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{t}from"./jsx-runtime-D2pHJD-r.js";import{gt as n}from"./esm-ucfRQTjh.js";import{a as r,i,n as a,t as o}from"./blocks-CKXS7ymV.js";import{t as s}from"./mdx-react-shim-YKiTaSC0.js";import{n as c,r as l,t as u}from"./index.stories-C9faE7aH.js";function d(e){let t={h1:`h1`,...n(),...e.components};return(0,p.jsxs)(p.Fragment,{children:[(0,p.jsx)(a,{of:u}),`
+`,(0,p.jsx)(t.h1,{id:`posterpanel`,children:`PosterPanel`}),`
+`,(0,p.jsx)(o,{withSource:`open`,children:(0,p.jsx)(i,{id:`packages-videopress-block-editor-poster-panel--default`})})]})}function f(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,p.jsx)(t,{...e,children:(0,p.jsx)(d,{...e})}):d(e)}var p;e((()=>{p=t(),s(),r(),l(),c()}))();export{f as default};
--- docs/assets/with-widget-root-DFR79mvK.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{t}from"./jsx-runtime-D2pHJD-r.js";import{B as n,V as r,a as i,o as a,u as o}from"./hooks-C5v9axHT.js";import{n as s,t as c}from"./register-report-mocks-DcmARGdJ.js";var l,u,d=e((()=>{o(),a(),c(),l=t(),s(),u=(e=r())=>t=>(0,l.jsx)(i,{attributes:{reportParams:n(e)},children:(0,l.jsx)(t,{})})}));export{u as n,d as t};
