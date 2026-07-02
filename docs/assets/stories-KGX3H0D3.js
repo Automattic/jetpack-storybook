11e9daeac5c851138c292c99bd3705288f408372
chihsuan <chihsuan@users.noreply.github.com>
2026-07-02 08:09:31 +0000

    Premium Analytics: port the Gross sales over time widget (#49709)
    * Premium Analytics: Add gross sales over time widget
    
    * Premium Analytics: Align gross sales widget with trunk
    
    * Premium Analytics: refine gross sales widget story
    
    * fix: conform gross-sales-over-time port to widget contract
    
    ---------
    
    Co-authored-by: Chi-Hsuan Huang <chihsuan.tw@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/28574340485
    
    Upstream-Ref: Automattic/jetpack@682fb235d39b1031624251dfb01835231942c99a

--- docs/assets/stories-KGX3H0D3.js
+++
@@ -1,3 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{t}from"./jsx-runtime-D2pHJD-r.js";import{gt as n}from"./esm-BXUQ3AgK.js";import{a as r,i,n as a,t as o}from"./blocks-JM6qb7KC.js";import{t as s}from"./mdx-react-shim-Dyh6M7yx.js";import{n as c,r as l,t as u}from"./index.stories-CR9ERG-3.js";function d(e){let t={h1:`h1`,...n(),...e.components};return(0,p.jsxs)(p.Fragment,{children:[(0,p.jsx)(a,{of:u}),`
-`,(0,p.jsx)(t.h1,{id:`posterpanel`,children:`PosterPanel`}),`
-`,(0,p.jsx)(o,{withSource:`open`,children:(0,p.jsx)(i,{id:`packages-videopress-block-editor-poster-panel--default`})})]})}function f(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,p.jsx)(t,{...e,children:(0,p.jsx)(d,{...e})}):d(e)}var p;e((()=>{p=t(),s(),r(),l(),c()}))();export{f as default};
