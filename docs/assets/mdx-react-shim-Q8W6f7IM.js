1ac67b233fc001c0ea8b3f091315a3cfb0934706
chihsuan <chihsuan@users.noreply.github.com>
2026-07-02 08:53:02 +0000

    Premium Analytics: prepare semi-circle chart options (#50153)
    * Prepare Premium Analytics semi-circle chart options
    
    * Premium Analytics: expose semi-circle chart aspectRatio as a prop
    
    ---------
    
    Co-authored-by: Chi-Hsuan Huang <chihsuan.tw@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/28576842582
    
    Upstream-Ref: Automattic/jetpack@80b8a2e6aff675cdd9443aa093167da05915a8d4

--- docs/assets/mdx-react-shim-Q8W6f7IM.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{pt as t}from"./esm-CWqK-8DQ.js";var n=e((()=>{t()}));export{n as t};
--- docs/assets/metric-delta-DEUls63z.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{n as t}from"./metric-delta-Dkn52pYn.js";var n=e((()=>{t()}));export{n as t};
