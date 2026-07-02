344b15b37304489512760e9ea7b879bf7552130e
chihsuan <chihsuan@users.noreply.github.com>
2026-07-02 10:10:06 +0000

    Premium Analytics: port the Orders fulfillment widget (#49713)
    * Premium Analytics: port the Orders fulfillment widget
    
    * fix: conform orders-fulfillment port to widget contract
    
    ---------
    
    Co-authored-by: Chi-Hsuan Huang <chihsuan.tw@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/28581190410
    
    Upstream-Ref: Automattic/jetpack@362e9c0a21313a0b8e2132197b6faba23d139aa2

---
+++ docs/assets/formatter-EIJCOSYU-DxPwVd-U.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{vo as t,yo as n}from"./iframe-B2Rpno1y.js";import{at as r,it as i,ot as a,st as o}from"./esm-uhAZE1J7.js";var s;e((()=>{a(),r(),n(),s=(0,t(o(),1).default)(2)(async(e,t)=>e===!1?t:i(t))}))();export{s as formatter};
---
+++ docs/assets/line-chart-CxxkXD1F.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{r as t}from"./line-chart-DRTy5f2j.js";var n=e((()=>{t()}));export{n as t};
