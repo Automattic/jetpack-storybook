4fe49c92a69cf491af896fff89b54c39bd01c1a2
chihsuan <chihsuan@users.noreply.github.com>
2026-07-02 05:03:44 +0000

    Premium Analytics: port the Total returns widget (#49727)
    * Premium Analytics: port the Total returns widget
    
    * fix: conform total-returns port to widget contract and fix bar-chart legend
    
    ---------
    
    Co-authored-by: Chi-Hsuan Huang <chihsuan.tw@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/28566097816
    
    Upstream-Ref: Automattic/jetpack@2d7f4b2e80db91a8e7d5754cf3267b819b2f0e68

--- docs/assets/with-chart-theme-ClS8NMQP.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{t}from"./jsx-runtime-D2pHJD-r.js";import{Q as n,t as r}from"./hooks-CS0BRRqn.js";import{A as i}from"./providers-BQ8Ha7b7.js";import{t as a}from"./src-tc0mkFRD.js";var o,s,c,l=e((()=>{a(),r(),o=t(),s=({children:e})=>(0,o.jsx)(i,{theme:n(),children:e}),c=e=>(0,o.jsx)(s,{children:(0,o.jsx)(e,{})})}));export{c as n,l as t};
