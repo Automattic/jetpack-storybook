639ad262844b1fed8c330326265070275314ecdb
dognose24 <dognose24@users.noreply.github.com>
2026-07-02 05:55:22 +0000

    Premium Analytics: port Stats Clicks widget (#50052)
    * Premium Analytics: add Clicks widget
    
    * Premium Analytics: handle nested click rows
    
    * Premium Analytics: address clicks widget feedback
    
    * Premium Analytics: Add clicks drill-down support
    
    * Fix Clicks lint issues
    
    * Add Clicks widget to default dashboard
    
    * Fix Clicks leaf link rendering
    
    * Fix Clicks PR CI checks
    
    * Align Clicks widget header across states
    
    * Align Clicks dashboard story presentation
    
    * Keep Clicks header visible in close-up stories
    
    * Add icon to Clicks widget header
    
    * Stabilize Clicks header height
    
    * Fix Clicks breadcrumb title cursor
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/28568037158
    
    Upstream-Ref: Automattic/jetpack@c36baa98d8f5f41abbaf07762867fa47f10409f0

---
+++ docs/assets/metric-delta-CrGcaFaD.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{n as t}from"./metric-delta-B4-Tozsk.js";var n=e((()=>{t()}));export{n as t};
---
+++ docs/assets/use-videopress-settings-cLUzOvuJ.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{t}from"./state-4M-9r40T.js";var n,r=e((()=>{n=`atomic`})),i=e((()=>{t()}));export{n,r,i as t};
