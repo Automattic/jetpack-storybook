c066599d1a8f5db31d5a8b58920bf994247c3610
chihsuan <chihsuan@users.noreply.github.com>
2026-07-02 08:12:36 +0000

    Premium Analytics: port the Coupon usage over time widget (#49707)
    * Premium Analytics: Add coupon usage over time widget
    
    * Premium Analytics: Fix coupon widget story formatting
    
    * fix: conform coupon-usage-over-time port to widget contract
    
    * fix: let coupon usage donut fill its container via maxSize=null
    
    ---------
    
    Co-authored-by: Chi-Hsuan Huang <chihsuan.tw@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/28574469210
    
    Upstream-Ref: Automattic/jetpack@6d8af6ef9eb61f875cf429e2621657caba426e09

---
+++ docs/assets/total-sales-over-time-widget.stories-CZrflTN9.js
@@ -0,0 +1,83 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{n as t,t as n}from"./build-module-Da_5_6n_.js";import{t as r}from"./jsx-runtime-D2pHJD-r.js";import{dn as i,n as a}from"./build-module-h3iDQ_Ze.js";import{$ as o,a as s,rt as c,u as l}from"./hooks-CH2BoqR7.js";import{n as u,r as d}from"./line-chart-CFz-L0Au.js";import{l as f,t as p}from"./src-BlU2m1VV.js";import{n as m,t as h}from"./register-report-mocks-DU4Acim2.js";import{o as g,t as _}from"./src-CjnOx3HZ.js";import{i as v,n as y,r as b,t as x}from"./widget-dashboard-with-widget-DvrANZ1X.js";function S({attributes:e={},setError:t}){return(0,C.jsx)(s,{attributes:e,setError:t,options:{from:`/`},children:(0,C.jsx)(g,{metricKey:`total_sales`})})}var C,w=e((()=>{_(),C=r()})),T,E=e((()=>{n(),a(),T={name:`jpa/total-sales-over-time`,title:t(`Total sales over time`,`jetpack-premium-analytics`),description:t(`Track total sales including all orders and transactions.`,`jetpack-premium-analytics`),icon:i}}));function D(e=!1,t=F){return{reportParams:o(e,t)}}function O({withComparison:e,preset:t}){let n=!!e,r=t??F;return!n&&r===F?`getDefaultQueryParams()`:n&&r===F?`getDefaultQueryParams( true )`:`getDefaultQueryParams( ${n?`true`:`false`}, '${r}' )`}function k(e){return`import { getDefaultQueryParams } from '@jetpack-premium-analytics/data';
+
+<TotalSalesOverTimeRender
+\tattributes={ {
+\t\treportParams: ${O(e)},
+\t} }
+/>`}function A({withComparison:e,preset:t}){return P(),(0,M.jsx)(c,{children:(0,M.jsx)(S,{attributes:D(e,t)})})}function j({withComparison:e,preset:t,...n}){return P(),(0,M.jsx)(c,{children:(0,M.jsx)(y,{...n,widgetType:T,renderModule:N,renderComponent:S,attributes:{reportParams:o(e,t)}})})}var M,N,P,F,I,L,R,z,B,V,H;e((()=>{l(),p(),b(),h(),d(),w(),E(),M=r(),m(),N=`storybook/total-sales-over-time`,P=()=>u.Legend,F=`last-30-days`,I=f,L=e=>(0,M.jsx)(`div`,{style:{width:`100%`,height:`300px`},children:(0,M.jsx)(e,{})}),R={title:`Packages/Premium Analytics/Widgets/TotalSalesOverTime`,component:S,tags:[`autodocs`],argTypes:{preset:{control:`select`,options:I,description:`Date-range preset used to generate the widget report params.`},withComparison:{control:`boolean`,description:`Include previous-period comparison report params.`}},parameters:{docs:{description:{component:`Dashboard widget that displays total sales over time for the selected period.`}}}},z={render:A,args:{preset:F,withComparison:!1},decorators:[L],parameters:{docs:{source:{transform:(e,t)=>k(t.args)}}}},B={render:A,args:{preset:F,withComparison:!0},decorators:[L],parameters:{docs:{source:{transform:(e,t)=>k(t.args)}}}},V={render:e=>(0,M.jsx)(j,{...e}),args:{...x,preset:F,withComparison:!0},argTypes:{...v,preset:{control:`select`,options:I,description:`Date-range preset used to generate the widget report params.`},withComparison:{control:`boolean`,description:`Include previous-period comparison report params.`}},parameters:{docs:{source:{code:`import { getDefaultQueryParams } from '@jetpack-premium-analytics/data';
+
+<WidgetDashboardWithWidget
+	widgetType={ widgetDefinition }
+	renderModule="storybook/total-sales-over-time"
+	renderComponent={ TotalSalesOverTimeRender }
+	attributes={ {
+		reportParams: getDefaultQueryParams( true ),
+	} }
+/>`}}}},z.parameters={...z.parameters,docs:{...z.parameters?.docs,source:{originalSource:`{
+  render: renderTotalSalesOverTime,
+  args: {
+    preset: DEFAULT_PRESET,
+    withComparison: false
+  },
+  decorators: [withWidgetCanvas],
+  parameters: {
+    docs: {
+      source: {
+        transform: (_source: string, storyContext: {
+          args: Partial<TotalSalesOverTimeStoryControls>;
+        }) => getTotalSalesOverTimeSource(storyContext.args)
+      }
+    }
+  }
+}`,...z.parameters?.docs?.source},description:{story:`Default state for the current report period.`,...z.parameters?.docs?.description}}},B.parameters={...B.parameters,docs:{...B.parameters?.docs,source:{originalSource:`{
+  render: renderTotalSalesOverTime,
+  args: {
+    preset: DEFAULT_PRESET,
+    withComparison: true
+  },
+  decorators: [withWidgetCanvas],
+  parameters: {
+    docs: {
+      source: {
+        transform: (_source: string, storyContext: {
+          args: Partial<TotalSalesOverTimeStoryControls>;
+        }) => getTotalSalesOverTimeSource(storyContext.args)
+      }
+    }
+  }
+}`,...B.parameters?.docs?.source},description:{story:`Comparison period enabled, showing period-over-period change and line chart data.`,...B.parameters?.docs?.description}}},V.parameters={...V.parameters,docs:{...V.parameters?.docs,source:{originalSource:`{
+  render: args => <TotalSalesOverTimeDashboardStory {...args} />,
+  args: {
+    ...DEFAULT_WIDGET_DASHBOARD_STORY_ARGS,
+    preset: DEFAULT_PRESET,
+    withComparison: true
+  },
+  argTypes: {
+    ...widgetDashboardWithWidgetArgTypes,
+    preset: {
+      control: 'select',
+      options: PRESET_OPTIONS,
+      description: 'Date-range preset used to generate the widget report params.'
+    },
+    withComparison: {
+      control: 'boolean',
+      description: 'Include previous-period comparison report params.'
+    }
+  },
+  parameters: {
+    docs: {
+      source: {
+        code: \`import { getDefaultQueryParams } from '@jetpack-premium-analytics/data';
+
+<WidgetDashboardWithWidget
+\\twidgetType={ widgetDefinition }
+\\trenderModule="storybook/total-sales-over-time"
+\\trenderComponent={ TotalSalesOverTimeRender }
+\\tattributes={ {
+\\t\\treportParams: getDefaultQueryParams( true ),
+\\t} }
+/>\`
+      }
+    }
+  }
+}`,...V.parameters?.docs?.source},description:{story:`Renders the widget through the shared dashboard harness.`,...V.parameters?.docs?.description}}},H=[`Default`,`WithComparison`,`WidgetDashboardWithWidget`]}))();export{z as Default,V as WidgetDashboardWithWidget,B as WithComparison,H as __namedExportsOrder,R as default};
---
+++ docs/assets/with-widget-root-BvA_6W9b.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{t}from"./jsx-runtime-D2pHJD-r.js";import{$ as n,Q as r,a as i,o as a,u as o}from"./hooks-CH2BoqR7.js";import{n as s,t as c}from"./register-report-mocks-DU4Acim2.js";var l,u,d=e((()=>{o(),a(),c(),l=t(),s(),u=(e=n())=>t=>(0,l.jsx)(i,{attributes:{reportParams:r(e)},children:(0,l.jsx)(t,{})})}));export{u as n,d as t};
