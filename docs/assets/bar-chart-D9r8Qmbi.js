580e57592db5239682e22d33c78347fe3c31d06f
chihsuan <chihsuan@users.noreply.github.com>
2026-07-08 03:45:22 +0000

    Revert "Premium Analytics: update widget packages and promote in-body control…" (#50292)
    This reverts commit e52d5402c88dfd482715f0333166fcdaf4cf9e67.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/28915421611
    
    Upstream-Ref: Automattic/jetpack@946029c45c64c06e9197c0da5d494a550819f1a0

--- docs/assets/bar-chart-D9r8Qmbi.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{r as t}from"./bar-chart-DfTG0uOq.js";var n=e((()=>{t()}));export{n as t};
--- docs/assets/with-chart-theme-Bo_FcLdR.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{t}from"./jsx-runtime-D2pHJD-r.js";import{Vt as n,t as r}from"./hooks-CELVii9l.js";import{A as i}from"./providers-C1AedUiy.js";import{t as a}from"./src-23zNjit4.js";var o,s,c,l=e((()=>{a(),r(),o=t(),s=({children:e})=>(0,o.jsx)(i,{theme:n(),children:e}),c=e=>(0,o.jsx)(s,{children:(0,o.jsx)(e,{})})}));export{c as n,l as t};
