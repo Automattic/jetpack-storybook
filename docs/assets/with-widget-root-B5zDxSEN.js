3903c650b4989fb75e6c11713d5900b2c7f379d4
chihsuan <chihsuan@users.noreply.github.com>
2026-07-13 08:22:19 +0000

    WOOA7S-1521: Port Stats WordAds chart tabs widget (#50314)
    * feat: add WordAds chart tabs stats widget to Premium Analytics
    
    * fix: derive WordAds window from range, clamp to yesterday, align tabs with upstream, WidgetState
    
    * fix: anchor WordAds window to range start when clamping end to yesterday
    
    * feat: add year granularity to WordAds chart tabs and drop unused setError
    
    * feat: select WordAds chart tab metrics via array attribute
    
    * chore: simplify WordAds chart tabs changelog and comments
    
    * fix: align WordAds comparison window to the clamped primary bucket count
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/29234231371
    
    Upstream-Ref: Automattic/jetpack@8c78e38ddbf289a5a5b8f5abc78551f742ee57f4

--- docs/assets/with-widget-root-B5zDxSEN.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{t}from"./jsx-runtime-D2pHJD-r.js";import{Nt as n,Pt as r,c as i,p as a,s as o}from"./hooks-Dboa4Xl3.js";import{n as s,t as c}from"./register-report-mocks-BXjXbLk-.js";var l,u,d=e((()=>{a(),i(),c(),l=t(),s(),u=(e=r())=>t=>(0,l.jsx)(o,{attributes:{reportParams:n(e)},children:(0,l.jsx)(t,{})})}));export{u as n,d as t};
