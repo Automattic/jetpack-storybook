21effc62e1bb48c320ee6ad4f0ad19116429ffe2
retrofox <retrofox@users.noreply.github.com>
2026-07-17 07:15:00 +0000

    fix select-field write-path value type coercion (#50584)
    map the selected option back to its original element value
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/29561640689
    
    Upstream-Ref: Automattic/jetpack@4b9466b8705e101787cb95293f52c1f060b9206c

--- docs/assets/bar-chart-BkiXi8kg.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{r as t}from"./bar-chart-OcS_XdKc.js";var n=e((()=>{t()}));export{n as t};
--- docs/assets/line-chart-DPtGmsuI.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{r as t}from"./line-chart-Bq_f6sfX.js";var n=e((()=>{t()}));export{n as t};
