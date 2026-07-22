4fc6c4e14431a010eb1cccbf70f5c3f2368534f1
gmjuhasz <gmjuhasz@users.noreply.github.com>
2026-07-22 10:32:03 +0000

    Social: Add admin UI v2 feature flag (#50733)
    * Social: add admin UI v2 feature flag
    
    * Address feature flag review feedback
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/29911180685
    
    Upstream-Ref: Automattic/jetpack@50fe4c5f1bbd9f9fc9093efc9d6ddca6001840de

--- docs/assets/formatter-L2YTDE3Q-Biv30fqr.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{ba as t,ya as n}from"./iframe-ZSTl3Plf.js";import{ct as r,dt as i,lt as a,ut as o}from"./esm--x-x_3O-.js";var s;e((()=>{o(),a(),t(),s=(0,n(i(),1).default)(2)(async(e,t)=>e===!1?t:r(t))}))();export{s as formatter};
