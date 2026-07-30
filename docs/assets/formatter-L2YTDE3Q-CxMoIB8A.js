3a48f7b5d8bb01ec0382aa5d50cf3010f3de5665
Nikschavan <Nikschavan@users.noreply.github.com>
2026-07-30 07:38:24 +0000

    Premium Analytics: link Most viewed post titles through the router (#50891)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/30522334223
    
    Upstream-Ref: Automattic/jetpack@8523e90bbab3cb07f8ec1fc1a5c00c13a92f3e0d

---
+++ docs/assets/formatter-L2YTDE3Q-CxMoIB8A.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{Sa as t,xa as n}from"./iframe-DM6ftJDn.js";import{ct as r,dt as i,lt as a,ut as o}from"./esm-9MLtSNrV.js";var s;e((()=>{o(),a(),t(),s=(0,n(i(),1).default)(2)(async(e,t)=>e===!1?t:r(t))}))();export{s as formatter};
