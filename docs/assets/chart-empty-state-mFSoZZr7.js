243847b7cd12c0edc003321f501d0cfc432dca33
chihsuan <chihsuan@users.noreply.github.com>
2026-08-11 09:55:44 +0000

    fix: remove duplicate padding in Premium Analytics chart-tab widgets (#51175)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/31478549443
    
    Upstream-Ref: Automattic/jetpack@1da805b23d9448f8e235ad2b719b161673fc17b3

--- docs/assets/chart-empty-state-mFSoZZr7.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{n as t}from"./chart-empty-state-B91I8is7.js";var n=e((()=>{t()}));export{n as t};
---
+++ docs/assets/formatter-L2YTDE3Q-PaR3eiae.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{Xl as t,Zl as n}from"./iframe-BK6QZFTP.js";import{ct as r,dt as i,lt as a,ut as o}from"./esm-Dj6zcqzB.js";var s;e((()=>{o(),a(),n(),s=(0,t(i(),1).default)(2)(async(e,t)=>e===!1?t:r(t))}))();export{s as formatter};
