ee5d6ce92900eafe42e06be0cd2466d858601959
retrofox <retrofox@users.noreply.github.com>
2026-08-20 23:08:46 +0000

    Premium Analytics: fix the sub-day previous-period comparison range (#51374)
    * fix sub-day previous-period comparison range
    
    shift both ends by the inclusive window duration
    
    * add changelog for comparison range fix
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/32426112407
    
    Upstream-Ref: Automattic/jetpack@56777bddb674f27f32472af67b51872034bd1d6a

--- docs/assets/with-widget-root-ccvzw2vi.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{t}from"./jsx-runtime-D2pHJD-r.js";import{$ as n,G as r,K as i,et as a}from"./report-metric-DRvIAU-G.js";import{Gt as o,Ut as s,t as c}from"./src-DxSjl9JX.js";var l,u,d=e((()=>{c(),a(),r(),l=t(),i(),u=(e=o())=>t=>(0,l.jsx)(n,{attributes:{reportParams:s(e)},children:(0,l.jsx)(t,{})})}));export{u as n,d as t};
