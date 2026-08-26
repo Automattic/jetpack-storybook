9983b579fcb6c763427ffa47bc3fc767bbd1d935
chihsuan <chihsuan@users.noreply.github.com>
2026-08-26 09:51:21 +0000

    fix: decode HTML entities in Premium Analytics email subject lines (#51596)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/32953608660
    
    Upstream-Ref: Automattic/jetpack@699db1bd12b59ce817c2be1877bb9c7556af6715

--- docs/assets/metric-delta-Cxv8N27A.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{n as t}from"./metric-delta-B1bFcH0M.js";var n=e((()=>{t()}));export{n as t};
---
+++ docs/assets/metric-with-comparison-C8XDdUHe.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{n as t}from"./metric-with-comparison-uGQ7D5xX.js";var n=e((()=>{t()}));export{n as t};
