cbe289a2882a2649925c59683e308284b0fc9eaa
chihsuan <chihsuan@users.noreply.github.com>
2026-08-27 08:22:55 +0000

    Premium Analytics: decode HTML entities in Stats titles and names (#51632)
    * fix: decode HTML entities in Premium Analytics Stats titles and names
    
    * fix: safely decode Premium Analytics HTML entities
    
    * refactor: simplify Premium Analytics entity decoding
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/33052362774
    
    Upstream-Ref: Automattic/jetpack@a8939ada53047c11bdb40dee9d5eebbcb3610bac

---
+++ docs/assets/index.stories-BMtAdVhs.js
@@ -0,0 +1,108 @@
+import{a as e,i as t}from"./preload-helper-usAeo7Bx.js";import{t as n}from"./jsx-runtime-D2pHJD-r.js";import{n as r,t as i}from"./build-module-CDRs4YxF.js";import{n as a,t as o}from"./button-DdL66lrb.js";import{a as s,n as c,r as l}from"./text-DNyawDL-.js";import{Bt as u,Lt as d,Rt as f,zt as p}from"./iframe-Buds_oZQ.js";var m,h,g,_=t((()=>{m=`_container_10o4l_1`,h=`_header_10o4l_15`,g={container:m,header:h}})),v=e({ButtonExternalLink:()=>R,ButtonLink:()=>L,ButtonPrimary:()=>F,ButtonSecondary:()=>I,Destructive:()=>V,Disabled:()=>B,FullWidth:()=>U,Icon:()=>z,Loading:()=>H,VariantsAndProps:()=>W,__namedExportsOrder:()=>G,_default:()=>N,default:()=>j}),y,b,x,S,C,w,T,E,D,O,k,A,j,M,N,P,F,I,L,R,z,B,V,H,U,W,G,K=t((()=>{r(),u(),f(),s(),a(),_(),y=n(),{Icon:b,...x}=i,{check:S,cloud:C}=x,w={variant:{table:{disable:!0}}},T={disabled:{table:{disable:!0}}},E={isDestructive:{table:{disable:!0}}},D={isLoading:{table:{disable:!0}}},O={icon:{table:{disable:!0}}},k={className:{table:{disable:!0}}},A={fullWidth:{table:{disable:!0}}},j={title:`JS Packages/Components/Button`,component:o,argTypes:{variant:{control:{type:`select`},options:[`primary`,`secondary`,`link`,`tertiary`]},size:{control:{type:`select`},options:[`normal`,`small`]},weight:{control:{type:`select`},options:[`bold`,`regular`]},icon:{control:{type:`select`},options:[`none`,...Object.keys(x)]}}},M=e=>{let t=e?.icon&&e?.icon!==`none`?(0,y.jsx)(b,{icon:x[e.icon]}):null;return(0,y.jsx)(o,{...e,icon:t})},N=M.bind({}),N.args={variant:`primary`,size:`normal`,weight:`bold`,icon:`cloud`,isExternalLink:!1,isLoading:!1,disabled:!1,isDestructive:!1,fullWidth:!1,children:`Once upon a time… a button story`},P=e=>(0,y.jsx)(o,{...e}),F=P.bind({}),F.argTypes={...w,...T,...O,...D,...E,...A},F.args={size:`normal`,children:`Jetpack Button`,variant:`primary`},I=P.bind({}),I.argTypes={...w,...T,...O,...D,...E,...k,...A},I.args={size:`normal`,children:`Jetpack Button`,variant:`secondary`},L=P.bind({}),L.argTypes={...w,...T,...O,...D,...E,...k,...A},L.args={size:`normal`,children:`Jetpack Button`,variant:`link`},R=P.bind({}),R.argTypes={...w,...T,...O,...D,...E,...k,...A},R.args={size:`normal`,children:`Jetpack Button`,variant:`external-link`},z=P.bind({}),z.argTypes={...O,...T,...D,...E,...k,...A},z.args={size:`normal`,children:`Jetpack Button`,icon:(0,y.jsx)(b,{icon:S}),variant:`primary`},B=P.bind({}),B.argTypes={...T,...E,...D,...k,...A,...O},B.args={size:`normal`,children:`Jetpack Button`,variant:`primary`,disabled:!0},V=P.bind({}),V.argTypes={...E,...D,...T,...k,...A,...O},V.args={size:`normal`,children:`Jetpack Button`,variant:`primary`,isDestructive:!0},H=P.bind({}),H.argTypes={...E,...D,...T,...k,...A,...O},H.args={size:`normal`,children:`Jetpack Button`,variant:`primary`,isLoading:!0},U=P.bind({}),U.argTypes={...E,...D,...T,...k,...O},U.args={size:`normal`,children:`Jetpack Button`,variant:`primary`,fullWidth:!0},W=()=>{let e=[`primary`,`secondary`,`link`];return(0,y.jsxs)(y.Fragment,{children:[(0,y.jsxs)(d,{children:[(0,y.jsx)(p,{children:(0,y.jsx)(c,{children:`Variants & Props`})}),(0,y.jsx)(p,{children:(0,y.jsxs)(l,{mb:3,children:[`The following shows how the properties modify the appearance and/or behavior of the button, in the different variants. Keep in mind that you cannot combine the variants but you can combine the props. Use the `,``,(0,y.jsx)(`a`,{href:`./?path=/story/js-packages-components-button--default`,children:`default story`}),` to play with the combinations.`]})})]}),(0,y.jsxs)(d,{className:g.container,horizontalGap:0,children:[(0,y.jsx)(p,{className:`${g[`row-instance`]} ${g.header}`,sm:4,md:2,lg:3,children:(0,y.jsx)(l,{variant:`body-extra-small`,children:`props / variants`})}),(0,y.jsx)(p,{sm:4,md:2,lg:3,children:(0,y.jsx)(l,{variant:`body-extra-small`,className:g.header,children:`Primary`})}),(0,y.jsx)(p,{sm:4,md:2,lg:3,children:(0,y.jsx)(l,{variant:`body-extra-small`,className:g.header,children:`Secondary`})}),(0,y.jsx)(p,{sm:4,md:2,lg:3,children:(0,y.jsx)(l,{variant:`body-extra-small`,className:g.header,children:`Link`})}),(0,y.jsx)(p,{className:g[`row-instance`],sm:4,md:2,lg:3,children:(0,y.jsx)(l,{variant:`body-extra-small`,children:`no props`})}),e.map(e=>(0,y.jsx)(p,{sm:4,md:2,lg:3,children:(0,y.jsx)(o,{...F.args,variant:e})},`normal-`+e)),(0,y.jsx)(p,{className:g[`row-instance`],sm:4,md:2,lg:3,children:(0,y.jsx)(l,{variant:`body-extra-small`,children:`size: small`})}),e.map(e=>(0,y.jsx)(p,{sm:4,md:2,lg:3,children:(0,y.jsx)(o,{...F.args,variant:e})},`small-`+e)),(0,y.jsx)(p,{className:g[`row-instance`],sm:4,md:2,lg:3,children:(0,y.jsx)(l,{variant:`body-extra-small`,children:`weight: regular`})}),e.map(e=>(0,y.jsx)(p,{sm:4,md:2,lg:3,children:(0,y.jsx)(o,{...F.args,variant:e,weight:`regular`})},`regular-`+e)),(0,y.jsx)(p,{className:g[`row-instance`],sm:4,md:2,lg:3,children:(0,y.jsx)(l,{variant:`body-extra-small`,children:`icon (cloud)`})}),e.map(e=>(0,y.jsx)(p,{sm:4,md:2,lg:3,children:(0,y.jsx)(o,{...F.args,variant:e,icon:(0,y.jsx)(b,{icon:C})})},`icon-`+e)),(0,y.jsx)(p,{className:g[`row-instance`],sm:4,md:2,lg:3,children:(0,y.jsx)(l,{variant:`body-extra-small`,children:`disabled`})}),e.map(e=>(0,y.jsx)(p,{sm:4,md:2,lg:3,children:(0,y.jsx)(o,{...F.args,variant:e,disabled:!0})},`disabled-`+e)),(0,y.jsx)(p,{className:g[`row-instance`],sm:4,md:2,lg:3,children:(0,y.jsx)(l,{variant:`body-extra-small`,children:`isDestructive`})}),e.map(e=>(0,y.jsx)(p,{sm:4,md:2,lg:3,children:(0,y.jsx)(o,{...F.args,variant:e,isDestructive:!0})},`destructive-`+e)),(0,y.jsx)(p,{className:g[`row-instance`],sm:4,md:2,lg:3,children:(0,y.jsx)(l,{variant:`body-extra-small`,children:`isExternalLink`})}),e.map(e=>(0,y.jsx)(p,{sm:4,md:2,lg:3,children:(0,y.jsx)(o,{...F.args,variant:e,isExternalLink:!0})},`external-`+e)),(0,y.jsx)(p,{className:g[`row-instance`],sm:4,md:2,lg:3,children:(0,y.jsx)(l,{variant:`body-extra-small`,children:`isLoading`})}),e.map(e=>(0,y.jsx)(p,{sm:4,md:2,lg:3,children:(0,y.jsx)(o,{...F.args,variant:e,isLoading:!0})},`loading-`+e)),(0,y.jsx)(p,{className:g[`row-instance`],sm:4,md:2,lg:3,children:(0,y.jsx)(l,{variant:`body-extra-small`,children:`fullWidth`})}),e.map(e=>(0,y.jsx)(p,{sm:4,md:2,lg:3,children:(0,y.jsx)(o,{...F.args,variant:e,fullWidth:!0})},`fullwidth-`+e))]})]})},W.storyName=`Variants & Props`,N.parameters={...N.parameters,docs:{...N.parameters?.docs,source:{originalSource:`args => {
+  const icon = args?.icon && args?.icon !== 'none' ? <WPIcon icon={icons[args.icon]} /> : null;
+  return <Button {...args} icon={icon} />;
+}`,...N.parameters?.docs?.source}}},F.parameters={...F.parameters,docs:{...F.parameters?.docs,source:{originalSource:`args => <Button {...args} />`,...F.parameters?.docs?.source}}},I.parameters={...I.parameters,docs:{...I.parameters?.docs,source:{originalSource:`args => <Button {...args} />`,...I.parameters?.docs?.source}}},L.parameters={...L.parameters,docs:{...L.parameters?.docs,source:{originalSource:`args => <Button {...args} />`,...L.parameters?.docs?.source}}},R.parameters={...R.parameters,docs:{...R.parameters?.docs,source:{originalSource:`args => <Button {...args} />`,...R.parameters?.docs?.source}}},z.parameters={...z.parameters,docs:{...z.parameters?.docs,source:{originalSource:`args => <Button {...args} />`,...z.parameters?.docs?.source}}},B.parameters={...B.parameters,docs:{...B.parameters?.docs,source:{originalSource:`args => <Button {...args} />`,...B.parameters?.docs?.source}}},V.parameters={...V.parameters,docs:{...V.parameters?.docs,source:{originalSource:`args => <Button {...args} />`,...V.parameters?.docs?.source}}},H.parameters={...H.parameters,docs:{...H.parameters?.docs,source:{originalSource:`args => <Button {...args} />`,...H.parameters?.docs?.source}}},U.parameters={...U.parameters,docs:{...U.parameters?.docs,source:{originalSource:`args => <Button {...args} />`,...U.parameters?.docs?.source}}},W.parameters={...W.parameters,docs:{...W.parameters?.docs,source:{originalSource:`() => {
+  const variants = ['primary', 'secondary', 'link'];
+  return <>
+            <Container>
+                <Col>
+                    <H3>Variants & Props</H3>
+                </Col>
+                <Col>
+                    <Text mb={3}>
+                        The following shows how the properties modify the appearance and/or behavior of the
+                        button, in the different variants. Keep in mind that you cannot combine the variants but
+                        you can combine the props. Use the {''}
+                        <a href="./?path=/story/js-packages-components-button--default">default story</a> to
+                        play with the combinations.
+                    </Text>
+                </Col>
+            </Container>
+            <Container className={styles.container} horizontalGap={0}>
+                <Col className={\`\${styles['row-instance']} \${styles.header}\`} sm={4} md={2} lg={3}>
+                    <Text variant="body-extra-small">props / variants</Text>
+                </Col>
+
+                <Col sm={4} md={2} lg={3}>
+                    <Text variant="body-extra-small" className={styles.header}>
+                        Primary
+                    </Text>
+                </Col>
+
+                <Col sm={4} md={2} lg={3}>
+                    <Text variant="body-extra-small" className={styles.header}>
+                        Secondary
+                    </Text>
+                </Col>
+
+                <Col sm={4} md={2} lg={3}>
+                    <Text variant="body-extra-small" className={styles.header}>
+                        Link
+                    </Text>
+                </Col>
+
+                <Col className={styles['row-instance']} sm={4} md={2} lg={3}>
+                    <Text variant="body-extra-small">no props</Text>
+                </Col>
+                {variants.map(variant => <Col key={'normal-' + variant} sm={4} md={2} lg={3}>
+                        <Button {...ButtonPrimary.args} variant={variant} />
+                    </Col>)}
+
+                <Col className={styles['row-instance']} sm={4} md={2} lg={3}>
+                    <Text variant="body-extra-small">size: small</Text>
+                </Col>
+                {variants.map(variant => <Col key={'small-' + variant} sm={4} md={2} lg={3}>
+                        <Button {...ButtonPrimary.args} variant={variant} />
+                    </Col>)}
+
+                <Col className={styles['row-instance']} sm={4} md={2} lg={3}>
+                    <Text variant="body-extra-small">weight: regular</Text>
+                </Col>
+                {variants.map(variant => <Col key={'regular-' + variant} sm={4} md={2} lg={3}>
+                        <Button {...ButtonPrimary.args} variant={variant} weight="regular" />
+                    </Col>)}
+
+                <Col className={styles['row-instance']} sm={4} md={2} lg={3}>
+                    <Text variant="body-extra-small">icon (cloud)</Text>
+                </Col>
+                {variants.map(variant => <Col key={'icon-' + variant} sm={4} md={2} lg={3}>
+                        <Button {...ButtonPrimary.args} variant={variant} icon={<WPIcon icon={cloud} />} />
+                    </Col>)}
+
+                <Col className={styles['row-instance']} sm={4} md={2} lg={3}>
+                    <Text variant="body-extra-small">disabled</Text>
+                </Col>
+                {variants.map(variant => <Col key={'disabled-' + variant} sm={4} md={2} lg={3}>
+                        <Button {...ButtonPrimary.args} variant={variant} disabled />
+                    </Col>)}
+
+                <Col className={styles['row-instance']} sm={4} md={2} lg={3}>
+                    <Text variant="body-extra-small">isDestructive</Text>
+                </Col>
+                {variants.map(variant => <Col key={'destructive-' + variant} sm={4} md={2} lg={3}>
+                        <Button {...ButtonPrimary.args} variant={variant} isDestructive />
+                    </Col>)}
+
+                <Col className={styles['row-instance']} sm={4} md={2} lg={3}>
+                    <Text variant="body-extra-small">isExternalLink</Text>
+                </Col>
+                {variants.map(variant => <Col key={'external-' + variant} sm={4} md={2} lg={3}>
+                        <Button {...ButtonPrimary.args} variant={variant} isExternalLink />
+                    </Col>)}
+
+                <Col className={styles['row-instance']} sm={4} md={2} lg={3}>
+                    <Text variant="body-extra-small">isLoading</Text>
+                </Col>
+                {variants.map(variant => <Col key={'loading-' + variant} sm={4} md={2} lg={3}>
+                        <Button {...ButtonPrimary.args} variant={variant} isLoading />
+                    </Col>)}
+
+                <Col className={styles['row-instance']} sm={4} md={2} lg={3}>
+                    <Text variant="body-extra-small">fullWidth</Text>
+                </Col>
+                {variants.map(variant => <Col key={'fullwidth-' + variant} sm={4} md={2} lg={3}>
+                        <Button {...ButtonPrimary.args} variant={variant} fullWidth />
+                    </Col>)}
+            </Container>
+        </>;
+}`,...W.parameters?.docs?.source}}},G=[`_default`,`ButtonPrimary`,`ButtonSecondary`,`ButtonLink`,`ButtonExternalLink`,`Icon`,`Disabled`,`Destructive`,`Loading`,`FullWidth`,`VariantsAndProps`]}));K();export{R as ButtonExternalLink,L as ButtonLink,F as ButtonPrimary,I as ButtonSecondary,V as Destructive,B as Disabled,U as FullWidth,z as Icon,H as Loading,W as VariantsAndProps,G as __namedExportsOrder,N as _default,j as default,K as n,v as t};
---
+++ docs/assets/total-visitors-widget.stories-C_UhL-na.js
@@ -0,0 +1,48 @@
+import{c as e,i as t}from"./preload-helper-usAeo7Bx.js";import{t as n}from"./react-DVCOKQW8.js";import{n as r,t as i}from"./build-module-DyKOxfM8.js";import{t as a}from"./jsx-runtime-D2pHJD-r.js";import{n as o,ui as s}from"./build-module-CDRs4YxF.js";import{dt as ee,kn as te,t as c}from"./src-DxWOn7KZ.js";import{b as l,g as u}from"./hooks-xxVzjUoJ.js";import{n as ne}from"./sparkline-fhGaXXPf.js";import{j as re,qt as d,t as f,vn as ie}from"./src-SOjeTdvv.js";import{n as p,t as m}from"./src-DBrm_zrj.js";import{M as ae}from"./chart-tooltip-BG6HpJ96.js";import{t as oe}from"./metric-sparkline-skeleton-vclqCRxE.js";import{G as h,K as se,Y as g}from"./report-metric-6rVF2bW5.js";import{t as ce}from"./widget-state-CLdoxDOw.js";import{C as _,D as le,E as ue,S as v,T as y,b as de,t as fe,w as pe,x as b}from"./src-CPMt0aCw.js";var x,S,C,w,T=t((()=>{x=`_root_lau5r_1`,S=`_body_lau5r_9`,C=`_chart_lau5r_21`,w={root:x,body:S,chart:C}}));function me(){let{reportParams:e}=l(),{primary:t,isLoading:n,isFetching:i,isError:a,error:o,refetch:c}=re((0,D.useMemo)(()=>ie({...e,stat_fields:`views,visitors`,period:k}),[e])),u=t.data,d=Number(u?.summary?.visitors??0),f=p(d,`number`,j),m=p(d,`number`,d>=1e3?A:j),h=(0,D.useMemo)(()=>(u?.data??[]).map(e=>Number(e.visitors??0)),[u]);return(0,O.jsx)(`div`,{className:w.root,children:(0,O.jsx)(ce,{isLoading:n,isFetching:i,isError:a&&h.length===0,isEmpty:h.length===0,error:ae(o,{retryDescription:r(`We couldn't load your visitors. Please try again in a moment.`,`jetpack-premium-analytics-pkg`),onRetry:c}),empty:{icon:s,description:r(`No visitors in this period.`,`jetpack-premium-analytics-pkg`)},renderLoading:(0,O.jsx)(oe,{}),children:(0,O.jsxs)(`div`,{className:w.body,children:[(0,O.jsx)(te,{variant:`heading-2xl`,title:f,children:m===f?m:(0,O.jsxs)(O.Fragment,{children:[(0,O.jsx)(`span`,{"aria-hidden":`true`,children:m}),(0,O.jsx)(ee,{children:f})]})}),(0,O.jsx)(`div`,{className:w.chart,children:(0,O.jsx)(ne,{data:h,maxWidth:1/0})})]})})})}function E({attributes:e={},setError:t}){return(0,O.jsx)(u,{attributes:e,setError:t,children:(0,O.jsx)(me,{})})}var D,O,k,A,j,he=t((()=>{f(),c(),m(),fe(),i(),o(),D=e(n(),1),T(),O=a(),k=`day`,A={useMultipliers:!0,decimals:1},j={decimals:0}})),M,N=t((()=>{o(),M={icon:s}})),P,F,I,L,R,z,B,ge=t((()=>{P=`jpa/total-visitors`,F=`Total visitors`,I=`Total visitors for the selected period, with the trend over time.`,L={content:`The number of visitors to your site. A returning visitor is counted once per day.`,links:[{label:`Learn more`,href:`https://wordpress.com/support/stats/#visitors`}]},R=`stats`,z=`framed`,B={name:P,title:F,description:I,help:L,category:R,presentation:z}}));function _e(){return(0,U.jsx)(E,{attributes:{reportParams:d(!1)}})}function V(e){return(0,U.jsx)(E,{attributes:{reportParams:d(!1,e)}})}function H(e){return(0,U.jsx)(y,{...e,widgetType:G,renderModule:W,renderComponent:E,attributes:{reportParams:d(!1)}})}var U,W,G,K,q,J,Y,X,Z,Q,$;t((()=>{f(),h(),ue(),_(),de(),he(),N(),ge(),U=a(),se(),W=`storybook/total-visitors`,G=v(B,M),K={title:`Packages/Premium Analytics/Widgets/TotalVisitors`,component:E,tags:[`autodocs`],parameters:{docs:{description:{component:`The "Total visitors" card: the selected period's visitor total as a large figure over an area sparkline of the trend. The total sums each day's visitors, so a returning visitor counts once per day — the card carries that caveat in its info popover. There is no WithComparison story: the widget strips comparison from its request and renders no delta.`}}}},q={render:_e,decorators:[b]},J={render:()=>V(`last-90-days`),tags:[`!autodocs`],decorators:[b],beforeEach:()=>(g(`stats/visits`,`loading`),()=>g(`stats/visits`,null))},Y={render:()=>V(`last-7-days`),tags:[`!autodocs`],decorators:[b],beforeEach:()=>(g(`stats/visits`,`error`),()=>g(`stats/visits`,null))},X={render:()=>V(`last-12-months`),tags:[`!autodocs`],decorators:[b],beforeEach:()=>(g(`stats/visits`,`error-retryable`),()=>g(`stats/visits`,null))},Z={render:()=>V(`last-year`),tags:[`!autodocs`],decorators:[b],beforeEach:()=>(g(`stats/visits`,`empty`),()=>g(`stats/visits`,null))},Q={render:e=>(0,U.jsx)(H,{...e}),args:{...pe},argTypes:{...le}},q.parameters={...q.parameters,docs:{...q.parameters?.docs,source:{originalSource:`{
+  render: renderTotalVisitors,
+  decorators: [withWidgetCanvas]
+}`,...q.parameters?.docs?.source},description:{story:`Default state — the period total over its trend sparkline.`,...q.parameters?.docs?.description}}},J.parameters={...J.parameters,docs:{...J.parameters?.docs,source:{originalSource:`{
+  render: () => renderTotalVisitorsOnPreset('last-90-days'),
+  tags: ['!autodocs'],
+  decorators: [withWidgetCanvas],
+  beforeEach: () => {
+    setReportMockState('stats/visits', 'loading');
+    return () => setReportMockState('stats/visits', null);
+  }
+}`,...J.parameters?.docs?.source},description:{story:`First load: the fetch is in flight, so the widget shows its loading state. The
+mock is forced to never resolve for the duration of this story.`,...J.parameters?.docs?.description}}},Y.parameters={...Y.parameters,docs:{...Y.parameters?.docs,source:{originalSource:`{
+  render: () => renderTotalVisitorsOnPreset('last-7-days'),
+  tags: ['!autodocs'],
+  decorators: [withWidgetCanvas],
+  beforeEach: () => {
+    setReportMockState('stats/visits', 'error');
+    return () => setReportMockState('stats/visits', null);
+  }
+}`,...Y.parameters?.docs?.source},description:{story:`The fetch failed with a permission-gated 403: neutral copy and no Retry
+action, since retrying cannot clear a permission gate.`,...Y.parameters?.docs?.description}}},X.parameters={...X.parameters,docs:{...X.parameters?.docs,source:{originalSource:`{
+  render: () => renderTotalVisitorsOnPreset('last-12-months'),
+  tags: ['!autodocs'],
+  decorators: [withWidgetCanvas],
+  beforeEach: () => {
+    setReportMockState('stats/visits', 'error-retryable');
+    return () => setReportMockState('stats/visits', null);
+  }
+}`,...X.parameters?.docs?.source},description:{story:`The fetch failed in a way that can heal — the proxy's \`no_connection\` 403: the
+retryable copy with a Retry action, which re-runs the query (still mocked as
+failing while this story is active).`,...X.parameters?.docs?.description}}},Z.parameters={...Z.parameters,docs:{...Z.parameters?.docs,source:{originalSource:`{
+  render: () => renderTotalVisitorsOnPreset('last-year'),
+  tags: ['!autodocs'],
+  decorators: [withWidgetCanvas],
+  beforeEach: () => {
+    setReportMockState('stats/visits', 'empty');
+    return () => setReportMockState('stats/visits', null);
+  }
+}`,...Z.parameters?.docs?.source},description:{story:`Resolved with no buckets: the widget shows its empty state.`,...Z.parameters?.docs?.description}}},Q.parameters={...Q.parameters,docs:{...Q.parameters?.docs,source:{originalSource:`{
+  render: args => <TotalVisitorsDashboardStory {...args} />,
+  args: {
+    ...DEFAULT_WIDGET_DASHBOARD_STORY_ARGS
+  },
+  argTypes: {
+    ...widgetDashboardWithWidgetArgTypes
+  }
+}`,...Q.parameters?.docs?.source}}},$=[`Default`,`Loading`,`Error`,`ErrorRetryable`,`Empty`,`WidgetDashboardWithWidget`]}))();export{q as Default,Z as Empty,Y as Error,X as ErrorRetryable,J as Loading,Q as WidgetDashboardWithWidget,$ as __namedExportsOrder,K as default};
