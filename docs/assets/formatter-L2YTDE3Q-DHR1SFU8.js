0afa55d4a2c0ac9a1c0decc0d086bef32f11303f
chihsuan <chihsuan@users.noreply.github.com>
2026-08-31 05:58:35 +0000

    Premium Analytics: treat an empty referrer results list as a leaf row (#51757)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/33361351276
    
    Upstream-Ref: Automattic/jetpack@9d96397fe0abb2c6895b9f4742a775afd8649741

---
+++ docs/assets/formatter-L2YTDE3Q-DHR1SFU8.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{nl as t,tl as n}from"./iframe-9TfebIvY.js";import{ct as r,dt as i,lt as a,ut as o}from"./esm-3wFNRta_.js";var s;e((()=>{o(),a(),t(),s=(0,n(i(),1).default)(2)(async(e,t)=>e===!1?t:r(t))}))();export{s as formatter};
