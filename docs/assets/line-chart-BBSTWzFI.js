36bf6af031a0a11e44a496df79c43ee3aa6df6c2
chihsuan <chihsuan@users.noreply.github.com>
2026-08-31 09:11:42 +0000

    Premium Analytics: rename and reorder the Top pages CSV comparison column (#51705)
    * feat: add previous-period columns to CSV exports with an active comparison
    
    * fix: leave the CSV previous-period cell blank when the row is outside the comparison
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/33374953181
    
    Upstream-Ref: Automattic/jetpack@764a5762209513153d50a761f1bf8cc6c07f0ff3

---
+++ docs/assets/line-chart-BBSTWzFI.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{r as t}from"./line-chart-toKCseq-.js";var n=e((()=>{t()}));export{n as t};
--- docs/assets/metric-with-comparison-CXAvjX-m.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{n as t}from"./metric-with-comparison-RR70z74a.js";var n=e((()=>{t()}));export{n as t};
