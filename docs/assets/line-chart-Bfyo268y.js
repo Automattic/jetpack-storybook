a22f71d45510bc6222fbf76add080b4de725838a
xavier-lc <xavier-lc@users.noreply.github.com>
2026-09-01 10:50:43 +0000

    Boost: align the upgrade modal copy with Jetpack.com (#51777)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/33497787973
    
    Upstream-Ref: Automattic/jetpack@c40f89ba93908b60c74fe40ef8289c7f2e23eca8

--- docs/assets/line-chart-Bfyo268y.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{r as t}from"./line-chart-DyN2S9_r.js";var n=e((()=>{t()}));export{n as t};
---
+++ docs/assets/with-chart-theme-BqGHeKvr.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{t}from"./jsx-runtime-D2pHJD-r.js";import{t as n}from"./src-BYuqoXB0.js";import{L as r,t as i}from"./hooks-CaRU2z5t.js";import{s as a}from"./chart-scope-B1Tkx_Sm.js";var o,s,c,l=e((()=>{n(),i(),o=t(),s=({children:e})=>(0,o.jsx)(a,{theme:r(),children:e}),c=e=>(0,o.jsx)(s,{children:(0,o.jsx)(e,{})})}));export{c as n,l as t};
