5cd386e41684457bfd95c970c726ce2affe21003
retrofox <retrofox@users.noreply.github.com>
2026-09-01 09:33:45 +0000

    Premium Analytics: turn date comparison off by default (#51771)
    * turn date comparison off by default
    fresh loads no longer seed comp=1; comparison starts when the user picks one
    
    * drop stray comparison params at the dashboard seed
    a hand-edited bare comp=1 no longer outlives the seed
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/33491134705
    
    Upstream-Ref: Automattic/jetpack@3511af2a7f7c9e9575441e2221e2e2e883110524

---
+++ docs/assets/formatter-L2YTDE3Q-DAhWhvbq.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{nl as t,tl as n}from"./iframe-DqJaThyQ.js";import{ct as r,dt as i,lt as a,ut as o}from"./esm-BpYyhtbr.js";var s;e((()=>{o(),a(),t(),s=(0,n(i(),1).default)(2)(async(e,t)=>e===!1?t:r(t))}))();export{s as formatter};
