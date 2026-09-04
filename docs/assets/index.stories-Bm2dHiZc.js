159211da2d4af0244efff0c0fd6eb8e8e4fa2252
adamwoodnz <adamwoodnz@users.noreply.github.com>
2026-09-04 05:53:08 +0000

    Premium Analytics: keep the date picker on the range a chart drill-down applies (#51924)
    * fix: keep the Premium Analytics date picker on the drilled range
    
    * test: cover useStagedValue's no-op revert and fix the ref comments
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/33831664259
    
    Upstream-Ref: Automattic/jetpack@62352d0156dbc95871d8118c07ca834a90425321

---
+++ docs/assets/index.stories-Bm2dHiZc.js
@@ -0,0 +1,14 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{t}from"./jsx-runtime-D2pHJD-r.js";import{Fr as n,Ir as r}from"./iframe-CLmsfEX5.js";var i,a,o,s,c,l,u,d,f,p,m;e((()=>{r(),i=t(),a={title:`JS Packages/Components/DonutMeter`,component:n,parameters:{layout:`centered`}},o=e=>(0,i.jsx)(n,{...e}),s={segmentCount:16,totalCount:100,thickness:`3.5`,donutWidth:`64px`,title:`Meter title goes here`,description:`Meter description goes here`},c=o.bind({}),c.args=s,l={...s,type:`warning`},u=o.bind({}),u.args=l,d={...s,type:`danger`},f=o.bind({}),f.args=d,p=e=>(0,i.jsxs)(`div`,{children:[(0,i.jsxs)(`div`,{style:{display:`flex`,flexFlow:`row`},children:[(0,i.jsx)(n,{...e,segmentCount:0,useAdaptiveColors:!0}),(0,i.jsx)(n,{...e,segmentCount:20,useAdaptiveColors:!0}),(0,i.jsx)(n,{...e,segmentCount:40,useAdaptiveColors:!0}),(0,i.jsx)(n,{...e,segmentCount:60,useAdaptiveColors:!0}),(0,i.jsx)(n,{...e,segmentCount:80,useAdaptiveColors:!0}),(0,i.jsx)(n,{...e,segmentCount:100,useAdaptiveColors:!0})]}),(0,i.jsx)(`p`,{children:`Color changing according to the fullness of the meter.`})]}),p.args={...s},c.parameters={...c.parameters,docs:{...c.parameters?.docs,source:{originalSource:`args => <DonutMeter {...args} />`,...c.parameters?.docs?.source}}},u.parameters={...u.parameters,docs:{...u.parameters?.docs,source:{originalSource:`args => <DonutMeter {...args} />`,...u.parameters?.docs?.source}}},f.parameters={...f.parameters,docs:{...f.parameters?.docs,source:{originalSource:`args => <DonutMeter {...args} />`,...f.parameters?.docs?.source}}},p.parameters={...p.parameters,docs:{...p.parameters?.docs,source:{originalSource:`args => <div>
+        <div style={{
+    display: 'flex',
+    flexFlow: 'row'
+  }}>
+            <DonutMeter {...args} segmentCount={0} useAdaptiveColors />
+            <DonutMeter {...args} segmentCount={20} useAdaptiveColors />
+            <DonutMeter {...args} segmentCount={40} useAdaptiveColors />
+            <DonutMeter {...args} segmentCount={60} useAdaptiveColors />
+            <DonutMeter {...args} segmentCount={80} useAdaptiveColors />
+            <DonutMeter {...args} segmentCount={100} useAdaptiveColors />
+        </div>
+        <p>Color changing according to the fullness of the meter.</p>
+    </div>`,...p.parameters?.docs?.source}}},m=[`_Default`,`Warning`,`Danger`,`AdaptiveColors`]}))();export{p as AdaptiveColors,f as Danger,u as Warning,c as _Default,m as __namedExportsOrder,a as default};
---
+++ docs/assets/metric-delta-CzgVpvdN.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{n as t}from"./metric-delta-DQr4udYx.js";var n=e((()=>{t()}));export{n as t};
