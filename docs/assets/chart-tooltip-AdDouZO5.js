ef3655551207c4db0e260a346ae2d6e61fce0e40
retrofox <retrofox@users.noreply.github.com>
2026-09-07 08:01:23 +0000

    Premium Analytics: disable the dashboard date controls while customizing (#52024)
    * add disabled prop to the date controls
    greys them out but keeps them focusable, with the menus shut
    
    * disable dashboard date controls while customizing
    the layout has to be saved or dropped before the page is used again
    
    * remove emptu jsdoc comment
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/34096664327
    
    Upstream-Ref: Automattic/jetpack@5d4fd5e080de918be5ae97fe6e93311fb2b643cf

---
+++ docs/assets/chart-tooltip-AdDouZO5.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{n as t}from"./chart-tooltip-QjE3AjQb.js";import{n}from"./pie-chart-tooltip-BONvMHUW.js";var r=e((()=>{t(),n()}));export{r as t};
