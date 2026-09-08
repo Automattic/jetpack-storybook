183ec07a498e165866d232d74d49938c8465ae1e
Nikschavan <Nikschavan@users.noreply.github.com>
2026-09-08 05:22:03 +0000

    WOOA7S-2032: Premium Analytics: display the dashboard on a three-column grid and rearrange the Traffic defaults (#51873)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/34189110404
    
    Upstream-Ref: Automattic/jetpack@1e146d29d81ccc5c56a5eaf7be6d02b96f598622

---
+++ docs/assets/chart-tooltip-DtciCPez.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{n as t}from"./chart-tooltip-B8FgBrx2.js";import{n}from"./pie-chart-tooltip-dgW3SJDe.js";var r=e((()=>{t(),n()}));export{r as t};
---
+++ docs/assets/mdx-react-shim-95ZeVZJA.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{_t as t}from"./esm-8d3Lsb-o.js";var n=e((()=>{t()}));export{n as t};
