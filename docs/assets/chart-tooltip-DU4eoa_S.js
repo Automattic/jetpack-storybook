db56794357237b3393513c662797bad80d4b61ef
anomiex <anomiex@users.noreply.github.com>
2026-09-08 19:38:24 +0000

    Lock file maintenance (#52028)
    Then fix test failures due to ariakit microtasks.
    
    The new version of ariakit switched some internal operations (e.g.
    tabindex updates) from animation frames (which don't get a chance to run
    in synchronous tests) to microtasks (which do), leading to a bunch of
    "something wasn't wrapped in `act()`" errors.
    
    Since it's internal ariakit stuff, there's not really anything to wait
    for. Introduce a dummy `await act( async () => {} )` into the relevant
    code paths to let those settle.
    
    It looks like ariakit's position on this is
    ["not our problem, react-testing-library should wait for microtasks"][1],
    while react-testing-library has had [a PR for that pending since 2023][2].
    
    [1]: https://href.li?https://github.com/ariakit/ariakit/issues/3939
    [2]: https://href.li?https://github.com/testing-library/react-testing-library/pull/1214
    
    ---------
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: Brad Jorsch <brad.jorsch@automattic.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/34268016102
    
    Upstream-Ref: Automattic/jetpack@1b31d196a77df3cc7a2d8e21a6196e69bc9b55d8

---
+++ docs/assets/chart-tooltip-DU4eoa_S.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{n as t}from"./chart-tooltip-DG5EU5jm.js";import{n}from"./pie-chart-tooltip-Bk_NLWhn.js";var r=e((()=>{t(),n()}));export{r as t};
