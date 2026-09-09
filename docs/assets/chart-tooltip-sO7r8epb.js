ab48085664f0b814f1ba2fa5300d892be9bdec4a
chrisbliss18 <chrisbliss18@users.noreply.github.com>
2026-09-09 23:39:40 +0000

    Update Jetpack 16.2 release readme
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/34416704472
    
    Upstream-Ref: Automattic/jetpack@527c8a04eade3a5d609bf371858ec90d2067200e

--- docs/assets/chart-tooltip-sO7r8epb.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{n as t}from"./chart-tooltip-01Bun2Yk.js";import{n}from"./pie-chart-tooltip-BiQ6FMhP.js";var r=e((()=>{t(),n()}));export{r as t};
--- docs/assets/metric-value-DjjU4vOd.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{n as t}from"./metric-value-DAZym56l.js";var n=e((()=>{t()}));export{n as t};
