e029a8f5361838b1ece6fb0b0006dca85f7e0187
enejb <enejb@users.noreply.github.com>
2026-09-16 19:58:53 +0000

    My Jetpack: add a flag that replaces the Products tab with a Features tab (#52346)
    * My Jetpack: add a flag that replaces the Products tab with a Features tab
    
    * My Jetpack: publish the products tab slug and label once for the tab, links and footers
    
    * My Jetpack: keep trunk's Products strings while the Features tab is off, and cover the review gaps
    
    * My Jetpack: address review on the Features tab flag
    
    * Jetpack: move the footer products link helper out of Jetpack_Admin_Page
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/35141565627
    
    Upstream-Ref: Automattic/jetpack@34903b0292964820f57d1bbe844f9a5873d29883

--- docs/assets/chart-tooltip-_ysh7CGU.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{n as t}from"./chart-tooltip-DP6V1c0V.js";import{n}from"./pie-chart-tooltip-CsSHo8a0.js";var r=e((()=>{t(),n()}));export{r as t};
