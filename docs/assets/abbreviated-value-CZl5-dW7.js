7ac3e9d1c03032871f4d39da9eadcede18879d8b
LiamSarsfield <LiamSarsfield@users.noreply.github.com>
2026-09-17 14:49:45 +0000

    Boost: stop My Jetpack product requests on sites that are not public (#52432)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/35233806268
    
    Upstream-Ref: Automattic/jetpack@5ca0c149f4a41394060972e0e28614c76396576f

--- docs/assets/abbreviated-value-CZl5-dW7.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{r as t}from"./abbreviated-value-Bwp-1ZvU.js";var n=e((()=>{t()}));export{n as t};
---
+++ docs/assets/chart-tooltip-C808O6_R.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{n as t}from"./chart-tooltip-D9gx2-cb.js";import{n}from"./pie-chart-tooltip-BC_IqmX4.js";var r=e((()=>{t(),n()}));export{r as t};
