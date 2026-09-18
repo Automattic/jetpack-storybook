14bf5e91e5d88b6186bd6f7781c5682fba5656d0
retrofox <retrofox@users.noreply.github.com>
2026-09-18 21:04:53 +0000

    Premium Analytics: drop the grouping control from the WordAds chart (#52483)
    * update WordAds chart header to match Subscribers
    
    drop the grouping control and add a line or bar toggle
    
    * minor jsdoc improvements
    
    * test the saved year case on a three-year window
    
    the one-year and day-long cases passed on trunk too
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/35393052808
    
    Upstream-Ref: Automattic/jetpack@84ec3ec5a89d087a8a359c85818381e65b935c23

--- docs/assets/chart-tooltip-CFBvd3rm.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{n as t}from"./chart-tooltip-BhNi_KKf.js";import{n}from"./pie-chart-tooltip-BaoO_PhK.js";var r=e((()=>{t(),n()}));export{r as t};
---
+++ docs/assets/formatter-L2YTDE3Q-TUTXfT_y.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{Os as t,ks as n}from"./iframe-6o7L_qBH.js";import{ct as r,dt as i,lt as a,ut as o}from"./esm-DaoOxsFc.js";var s;e((()=>{o(),a(),n(),s=(0,t(i(),1).default)(2)(async(e,t)=>e===!1?t:r(t))}))();export{s as formatter};
