bfe8ebd637448e248c4971f9286e09b205ad4943
LiamSarsfield <LiamSarsfield@users.noreply.github.com>
2026-09-18 10:39:50 +0000

    Boost: show Settings tooltips in full instead of clipping them at the card edge (#52465)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/35333846960
    
    Upstream-Ref: Automattic/jetpack@8191f4942f0e91229476f8dd740114d40f7fcf0b

---
+++ docs/assets/chart-tooltip-B0XI9hCs.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{n as t}from"./chart-tooltip-Bw5V7JSh.js";import{n}from"./pie-chart-tooltip-DL--2tqm.js";var r=e((()=>{t(),n()}));export{r as t};
