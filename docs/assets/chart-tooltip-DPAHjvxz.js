1265088e36d788e42f3ca8f8d433606419586ba8
retrofox <retrofox@users.noreply.github.com>
2026-09-21 13:19:00 +0000

    Premium Analytics: expose the Insights tab in the customer preview (#52525)
    * feat: expose the Insights tab in the Premium Analytics customer preview
    
    * feat: name both preview tabs in the welcome, feedback and switch-off copy
    
    * feat: stand the preview feedback banner on the Insights tab too
    
    * revert: keep the preview feedback banner on the Traffic tab only
    
    ---------
    
    Co-authored-by: Damián Suárez <rdsuarez@gmail.com>
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/35602738769
    
    Upstream-Ref: Automattic/jetpack@ad692d834d75e2d5b5964cd45025d54b0441223c

---
+++ docs/assets/chart-tooltip-DPAHjvxz.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{n as t}from"./chart-tooltip-CI_B93g_.js";import{n}from"./pie-chart-tooltip-C9IYf-XK.js";var r=e((()=>{t(),n()}));export{r as t};
