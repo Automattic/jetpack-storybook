f923a1bdd7a416e40e9a2f55b38d351cc52c4411
LiamSarsfield <LiamSarsfield@users.noreply.github.com>
2026-09-22 08:16:57 +0000

    Boost: Hide footer links when My Jetpack is unavailable (#52557)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/35701844866
    
    Upstream-Ref: Automattic/jetpack@0ea5f95bfe601eb0135c14391160b8684fc2a54f

---
+++ docs/assets/chart-comparative-line-Dwe1Z2b1.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{n as t}from"./comparative-line-chart-DMO9ewXz.js";var n=e((()=>{t()}));export{n as t};
---
+++ docs/assets/chart-tooltip-Bz8kqfMR.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{n as t}from"./chart-tooltip-aUB9erc-.js";import{n}from"./pie-chart-tooltip-Dox_fkkF.js";var r=e((()=>{t(),n()}));export{r as t};
