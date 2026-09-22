548919d26ffbada66bc80c27085010fef52cb23e
anomiex <anomiex@users.noreply.github.com>
2026-09-22 16:50:32 +0000

    Easy `@automattic/format-currency` replacements (#52236)
    `@automattic/format-currency` has been deprecated for a while (e.g.
    https://github.com/Automattic/jetpack/pull/48406#issuecomment-4354449194),
    but actually doing the work to replace it has been difficult.
    
    I did a quick look for easy replacements, and found two:
    
    * `js-packages/components` was only using `@automattic/format-currency`
      for a list of options in a Storybook story. Switch to
      `Intl.supportedValuesOf( 'currency' )` for that instead.
    * `packages/newsletter` was using only the `formatCurrency()` function,
      which has a drop-in replacement in `@automattic/number-formatters`
      (see #42796).
    
    The other uses seem potentially more difficult, so I didn't attempt
    them.
    
    * `packages/paypal-payments` seems to be trying to handle cases that
      `Intl` functions or `@automattic/number-formatters` don't handle.
    * Donations block in `plugins/jetpack` had #43418, which it's not clear
      why that stalled. I'm wary of trying it again without knowing that.
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/35754400463
    
    Upstream-Ref: Automattic/jetpack@8d1533e65c92fab7769d06b0bd1174c50fded6b5

---
+++ docs/assets/formatter-L2YTDE3Q-CqblsRTB.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{Os as t,ks as n}from"./iframe-7NNWv5XW.js";import{ct as r,dt as i,lt as a,ut as o}from"./esm-Bv34EhiF.js";var s;e((()=>{o(),a(),n(),s=(0,t(i(),1).default)(2)(async(e,t)=>e===!1?t:r(t))}))();export{s as formatter};
