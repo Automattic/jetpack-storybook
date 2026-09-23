2aaf0ad716b292851e27f8a520db9252f7fc1ad3
xavier-lc <xavier-lc@users.noreply.github.com>
2026-09-23 14:43:49 +0000

    IconTooltip: open, dismiss and tab out of info tooltips with the keyboard (#52661)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/35873885920
    
    Upstream-Ref: Automattic/jetpack@7620ba9c6efabfa6b706e43970650ec4404446c9

---
+++ docs/assets/chart-tooltip-CFhcqlZM.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{n as t}from"./chart-tooltip-Bj49FP_i.js";import{n}from"./pie-chart-tooltip-PS-a3fqH.js";var r=e((()=>{t(),n()}));export{r as t};
