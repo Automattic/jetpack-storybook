eae32194437c4939e47037362e5110ba4c794956
chihsuan <chihsuan@users.noreply.github.com>
2026-09-23 03:22:35 +0000

    Premium Analytics: Pick each line chart's value axis baseline by metric (#52600)
    * Premium Analytics: Pick each line chart's value axis baseline by metric
    
    * Premium Analytics: pad the line chart axis over the series the legend leaves visible
    
    * Premium Analytics: label a padded value axis in full when compact labels repeat
    
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/35812553794
    
    Upstream-Ref: Automattic/jetpack@45d7c7c5ba4e4b3331ef055e891193438e67d34d

---
+++ docs/assets/chart-tooltip-D0SIupPC.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{n as t}from"./chart-tooltip-HkbJC4BK.js";import{n}from"./pie-chart-tooltip-5mD6tYc3.js";var r=e((()=>{t(),n()}));export{r as t};
--- docs/assets/widget-grid-animation-BgCKZrQg.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./preload-helper-usAeo7Bx.js";import{n as t}from"./widget-grid-animation-6_7zwmsh.js";var n=e((()=>{t()}));export{n as t};
