b964eb634dde2c4f15fd3653eadf2fc730efb88d
xavier-lc <xavier-lc@users.noreply.github.com>
2026-09-25 15:18:16 +0000

    IconTooltip: Add a label prop and name Boost's info icons (#52800)
    Committed via a GitHub action: https://github.com/Automattic/jetpack/actions/runs/36150977507
    
    Upstream-Ref: Automattic/jetpack@f29ac8bcea7b5ed274543c4f4c0f3daf3e36e240

---
+++ docs/assets/use-permission-DowGYPbY.js
@@ -0,0 +1,1 @@
+import{i as e}from"./preload-helper-usAeo7Bx.js";import{n as t}from"./use-connection-sAQY4AQV.js";import{t as n}from"./connection-CUBC0QPF.js";var r,i=e((()=>{n(),r=()=>{let{isRegistered:e,hasConnectedOwner:n,isUserConnected:r}=t();return{isRegistered:e,hasConnectedOwner:n,isUserConnected:r,canPerformAction:e&&n&&r}}}));export{r as n,i as t};
